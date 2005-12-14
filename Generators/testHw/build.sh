c938d9aa4c3b67d72c2f2de27a7f86afd38df744
buckley <none@none>
2005-12-14 12:00:42 +0000

    Rewriting build script

--- Generators/testHw/build.sh
+++ Generators/testHw/build.sh
@@ -1,10 +1,9 @@
-#! /usr/bin/env bash
-
-LIBHERWIG=$HOME/local/lib/libherwig65.a
-INCHERWIG=$HOME/local/include/herwig6507
-LIBHEPMC=$HOME/local/lib/libHepMC.so
-INCHEPMC=$HOME/local/include/
+LIBHERWIG=$(HOME)/local/lib/libherwig65.a
+INCHERWIG=$(HOME)/local/include/herwig6507
+LIBHEPMC=$(HOME)/local/lib/libHepMC.so
+INCHEPMC=$(HOME)/local/include/
 LIBCLHEP=/usr/local/lib/libCLHEP.so
 INCCLHEP=/usr/local/include/
 
-g++ -I$INCHEPMC -I$INCCLHEP Herwig.cxx 2>&1 
+all:
+	g++ -I$(INCHEPMC) -I$(INCCLHEP) $(LIBHERWIG) $(LIBCLHEP) Herwig.cxx 2>&1
