2ddd49531326dcf2ad1b7e68305201723d5a5010
buckley <none@none>
2007-01-16 17:40:28 +0000

    A bit more playing with LWH

--- src/Test/testLWH.cc
+++ src/Test/testLWH.cc
@@ -2,6 +2,8 @@
 
 
 #include "LWH/AnalysisFactory.h"
+#include "LWH/TreeFactory.h"
+#include "LWH/Tree.h"
 #include "LWH/HistogramFactory.h"
 #include "LWH/Histogram1D.h"
 
@@ -10,24 +12,25 @@
 
 
 using namespace LWH;
+using namespace AIDA;
 
 int main(int argc, char* argv[]) {
   //IAnalysisFactory* af = new AnalysisFactory();
-  //IHistogramFactory* hf = af->createHistogramFactory();
-  //IHistogram1D* h = hf->createHistogram1D("test1", "Testing histo", 10, 0.0, 10.0);
-
-  Histogram1D h(20, 0.0, 5.0);
+  //ITree* tree = af->createTreeFactory()->create("/foo");
+  //IHistogramFactory* hf = af->createHistogramFactory(*tree);
+  //IHistogram1D* h = hf->createHistogram1D("test1", "Testing histo", 20, 0.0, 5.0);
+  Histogram1D* h = new Histogram1D(20, 0.0, 5.0);
 
   for (int i = 0 ; i < 10000 ; ++i) {
     double x = i/2000.0;
     double y = sin(x);
-    std::cout << "Filling: " << x << ", " << y << std::endl;
-    h.fill(x, y);
+    //std::cout << "Filling: " << x << ", " << y << std::endl;
+    h->fill(x, y);
   }
 
-  h.writeXML(std::cout, "/test1", "foooo");
+  h->writeXML(std::cout, "/test1", "foooo");
   std::ofstream filestr("test.data");
-  h.writeFLAT(filestr, "/test1", "baaaar");
+  h->writeFLAT(filestr, "/test1", "baaaar");
 
   return EXIT_SUCCESS;
 }
