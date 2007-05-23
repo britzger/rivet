2e650fecc2c3d31bccf6fa732f8c829576eba88c
buckley <none@none>
2007-05-23 12:59:11 +0000

    Removed manual histo booking and normalizing commented code from D0 jet analysis (and some other tidying)

--- src/Analysis/HepEx0409040.cc
+++ src/Analysis/HepEx0409040.cc
@@ -16,50 +16,6 @@ using namespace HepMC;
 
 // Book histograms
 void HepEx0409040::init() {
-
-  //set in constructor (default=x-axis range in rad)
-  //xscale = 1.;   // histo x-axis range in [rad*128/Pi]
-  //xscale = PI/128.;   // histo x-axis range in [rad]
-
-
-//   double bins_pTmax75_100_arr[] = {64., 80., 88., 96., 100., 102., 104., 106., 108., 110., 
-// 				   112., 114., 116., 118., 120., 122., 124., 126., 128.}; 
-//   vector<double> bins_pTmax75_100(19);
-//   for (int i=0; i<=18; ++i) 
-//     bins_pTmax75_100[i] = bins_pTmax75_100_arr[i]*xscale;
-
-    
-//   double bins_pTmax100_130_arr[] = {64., 72., 80., 84., 88., 92., 94., 96., 98., 
-// 				    100., 102., 104., 106., 108., 110., 112., 114., 
-// 				    116., 118., 120., 122., 124., 126., 128.}; 
-//   vector<double> bins_pTmax100_130(24);
-//   for (int i=0; i<=23; ++i)
-//     bins_pTmax100_130[i] = bins_pTmax100_130_arr[i]*xscale;
-  
-  
-//   double bins_pTmax130_180_arr[] = {64., 80., 88., 92., 96., 98., 100., 102., 104., 106., 
-// 				    108., 110., 112., 114., 115., 116., 117., 118., 119.,
-// 				    120., 121., 122., 123., 124., 125., 126., 127., 128.}; 
-//   vector<double> bins_pTmax130_180(28);
-//   for (int i=0; i<=27; ++i)
-//     bins_pTmax130_180[i] = bins_pTmax130_180_arr[i]*xscale;
-  
-  
-//   double bins_pTmax180_arr[] = {64., 80., 88., 92., 96., 98., 100., 102., 104., 106., 
-// 				108., 110., 112., 114., 115., 116., 117., 118., 119., 
-// 				120., 121., 122., 123., 124., 125., 126., 127.}; 
-//   vector<double> bins_pTmax180_(27);
-//   for (int i=0; i<=26; ++i) 
-//     bins_pTmax180_[i] = bins_pTmax180_arr[i]*xscale;
-
-  
-
-//   histJetAzimuth_pTmax75_100 = bookHistogram1D("JetAzimuth_pTmax75_100", "Jet Jet azimuthal angle, pTmax=75..100", bins_pTmax75_100);
-//   histJetAzimuth_pTmax100_130 = bookHistogram1D("JetAzimuth_pTmax100_130", "Jet Jet azimuthal angle, pTmax=100..130", bins_pTmax100_130);
-//   histJetAzimuth_pTmax130_180 = bookHistogram1D("JetAzimuth_pTmax130_180", "Jet Jet azimuthal angle, pTmax=130..180", bins_pTmax130_180);
-//   histJetAzimuth_pTmax180_ = bookHistogram1D("JetAzimuth_pTmax180_", "Jet Jet azimuthal angle, pTmax>180", bins_pTmax180_);
-
-
   // Use histogram auto-booking
   histJetAzimuth_pTmax75_100  = bookHistogram1D(1, 1, 1, "Jet Jet azimuthal angle, pTmax=75..100");
   histJetAzimuth_pTmax100_130 = bookHistogram1D(2, 1, 1, "Jet Jet azimuthal angle, pTmax=100..130");
@@ -72,22 +28,19 @@ void HepEx0409040::init() {
 void HepEx0409040::analyze(const Event & event) {
   Log& log = getLog();
   log << Log::DEBUG << "Starting analyzing" << endl;
-  //cout << "Start analyzing" << endl; //ls
 
-  // Analyse and print some info
-  
+  // Analyse and print some info  
   const D0RunIIconeJets& jetpro = event.applyProjection(conejets);
   
   //int nj = jetpro.getNJets(); //ls
   //log << Log::INFO << "Jet multiplicity before any pT cut = " << nj << endl; //ls
   //cout << "Jet multiplicity before any pT cut = " << nj << endl; //ls
    
-  //check fabs(z-vertex) < 50 cm 
-  const PVertex& PV = event.applyProjection(p_vertex); //segmentation violation
-  //either the HepMC event record is not filled properly or the F77-Wrapper functions are faulty
-  if (fabs(PV().position().z())< 500.) { //assummed to be in mm, PYTHIA convention
+  // Check z-component of vertex is < 50 cm from the nominal IP
+  const PVertex& PV = event.applyProjection(p_vertex);
+  /// @todo SEGV: either the HepMC event record is not filled properly or the F77-Wrapper functions are faulty
+  if (fabs(PV().position().z())< 500.0) { /// @todo z- value assumed to be in mm, PYTHIA convention: dangerous!
 
-    
     // Fill histograms
     list<HepEntity>::iterator jetpTmax = jetpro.jets->end();
     list<HepEntity>::iterator jet2ndpTmax = jetpro.jets->end();
@@ -136,16 +89,15 @@ void HepEx0409040::analyze(const Event & event) {
           double dphi = delta_phi(jetpTmax->phi(),jet2ndpTmax->phi());
           if (fabs(xscale-1.)<1.e-3) dphi /= xscale; //x-axis range [64,128]
           
-          //cout << "Filling histograms now: dphi=" << dphi << endl;
-          
-          if (jetpTmax->pT() > 75. && jetpTmax->pT() <= 100.) 
-            histJetAzimuth_pTmax75_100->fill(dphi, event.weight() );
-          else if (jetpTmax->pT() > 100. && jetpTmax->pT() <= 130.)
-            histJetAzimuth_pTmax100_130->fill(dphi, event.weight() );
-          else if (jetpTmax->pT() > 130. && jetpTmax->pT() <= 180.)
-            histJetAzimuth_pTmax130_180->fill(dphi, event.weight() );
-          else if (jetpTmax->pT() > 180.)
-            histJetAzimuth_pTmax180_->fill(dphi, event.weight() );
+          //cout << "Filling histograms now: dphi=" << dphi << endl;          
+          if (jetpTmax->pT() > 75.0 && jetpTmax->pT() <= 100.0)
+            histJetAzimuth_pTmax75_100->fill(dphi, event.weight());
+          else if (jetpTmax->pT() > 100.0 && jetpTmax->pT() <= 130.0)
+            histJetAzimuth_pTmax100_130->fill(dphi, event.weight());
+          else if (jetpTmax->pT() > 130.0 && jetpTmax->pT() <= 180.0)
+            histJetAzimuth_pTmax130_180->fill(dphi, event.weight());
+          else if (jetpTmax->pT() > 180.0)
+            histJetAzimuth_pTmax180_->fill(dphi, event.weight());
           
         } //CalMET
       } //jets N, pT
@@ -162,50 +114,6 @@ void HepEx0409040::analyze(const Event & event) {
 // Finalize
 void HepEx0409040::finalize() { 
   Log& log = getLog();
-
-//   double area75_100 = 0;
-//   for (int i=0; i < histJetAzimuth_pTmax75_100->axis().bins(); ++i) {
-//     area75_100 += histJetAzimuth_pTmax75_100->binHeight(i) * 
-//       histJetAzimuth_pTmax75_100->axis().binWidth(i);
-//   }
-//   log << Log::INFO << "Area under histJetAzimuth_pTmax75_100 histogram: " << area75_100 << endl;
-
-//   double area100_130 = 0;
-//   for (int i=0; i < histJetAzimuth_pTmax100_130->axis().bins(); ++i) {
-//     area100_130 += histJetAzimuth_pTmax100_130->binHeight(i) * 
-//       histJetAzimuth_pTmax100_130->axis().binWidth(i);
-//   }
-//   log << Log::INFO << "Area under histJetAzimuth_pTmax100_130 histogram: " << area100_130 << endl;
-
-//   double area130_180 = 0;
-//   for (int i=0; i < histJetAzimuth_pTmax130_180->axis().bins(); ++i) {
-//     area130_180 += histJetAzimuth_pTmax130_180->binHeight(i) * 
-//       histJetAzimuth_pTmax130_180->axis().binWidth(i);
-//   }
-//   log << Log::INFO << "Area under histJetAzimuth_pTmax130_180 histogram: " << area130_180 << endl;
-
-//   double area180_ = 0;
-//   for (int i=0; i < histJetAzimuth_pTmax180_->axis().bins(); ++i) {
-//     area180_ += histJetAzimuth_pTmax180_->binHeight(i) * 
-//       histJetAzimuth_pTmax180_->axis().binWidth(i);
-//   }
-//   log << Log::INFO << "Area under histJetAzimuth_pTmax180_ histogram: " << area180_ << endl;
-
-
-
-//   // //Normalize the histogram areas to 1
-//   if (histJetAzimuth_pTmax75_100->sumBinHeights()!=0){
-//     histJetAzimuth_pTmax75_100->scale(1./area75_100);
-//   }
-//   if (histJetAzimuth_pTmax100_130->sumBinHeights()!=0){
-//     histJetAzimuth_pTmax100_130->scale(1./area100_130);
-//   }
-//   if (histJetAzimuth_pTmax130_180->sumBinHeights()!=0){
-//     histJetAzimuth_pTmax130_180->scale(1./area130_180);
-//   }
-//   if (histJetAzimuth_pTmax180_->sumBinHeights()!=0){
-//     histJetAzimuth_pTmax180_->scale(1./area180_); 
-//   }
 
   // Normalize histograms to unit area
   normalize(histJetAzimuth_pTmax75_100);
