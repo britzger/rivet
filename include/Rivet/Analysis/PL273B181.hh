fdf33779e4ecb005f4482c63b1f26f8531deb8b5
buckley <none@none>
2007-05-30 12:43:04 +0000

    Moved the misplaced event shape plots out of the PLB analysis - they are currently just hanging around as unbuilt copies of the old PLB analysis, renamed as more appropriate ZPhys and EurPhys papers. Fix this post-release.

--- include/Rivet/Analysis/PL273B181.hh
+++ include/Rivet/Analysis/PL273B181.hh
@@ -9,19 +9,18 @@
 
 namespace Rivet {
 
-  /// This class just measures the charged multiplicity
+  /// This analysis just measures the charged multiplicity.
   class PL273B181 : public Analysis {
 
   public:
 
     /// Default constructor.
     inline PL273B181()
-      : mult(fsproj), spher(fsproj) 
+      : mult(fsproj)
     { 
       setBeams(ELECTRON, POSITRON); 
       addProjection(fsproj);
       addProjection(mult);
-      addProjection(spher);
     }
 
   public:
@@ -37,17 +36,14 @@ namespace Rivet {
 
     virtual void finalize();
 
-    /// Return the RivetInfo object of this analysis object.
-    //virtual RivetInfo getInfo() const;
-
   private:
 
-    /// The projectors used by this analysis.
+    /// @name The projectors used by this analysis.
+    //{
     FinalState fsproj;
 
     Multiplicity mult;
-
-    Sphericity spher;
+    //}
 
   private:
 
@@ -57,9 +53,6 @@ namespace Rivet {
     /// @name Histograms
     //@{
     AIDA::IHistogram1D* histChTot_;
-    AIDA::IHistogram1D* histSphericity_;
-    AIDA::IHistogram1D* histPlanarity_;
-    AIDA::IHistogram1D* histAplanarity_;
     //@}
 
   };
---
+++ src/Analysis/ZPhys73C11.cc
@@ -0,0 +1,60 @@
+// -*- C++ -*-
+
+#include "Rivet/Tools/Logging.hh"
+#include "Rivet/RivetAIDA.hh"
+#include "Rivet/Analysis/PL273B181.hh"
+#include "HepPDT/ParticleID.hh"
+
+using namespace AIDA;
+using namespace HepMC;
+
+namespace Rivet {
+
+  void PL273B181::init() {
+    // Book histograms
+    // histChTot_       = bookHistogram1D("TotalChMult","Total charged multiplicity", 25, 1.0, 51.0);
+    histChTot_       = bookHistogram1D(1, 1, 1, "Total charged multiplicity");
+    histSphericity_  = bookHistogram1D("Sphericity", "Event Shape: Sphericity", 8, 0.0, 0.70);
+    histAplanarity_  = bookHistogram1D("Aplanarity", "Event Shape: APlanarity", 10, 0.0, 0.09);
+    histPlanarity_   = bookHistogram1D("Planarity",  "Event Shape: Planarity", 16, 0.0, 0.70);
+
+  }
+
+
+  // Do the analysis
+  void PL273B181::analyze(const Event & event) {
+    Log& log = getLog();
+    log << Log::DEBUG << "Starting analyzing" << endl;
+
+    // Analyse and print some info
+    const Multiplicity& m = event.applyProjection(mult);
+    log << Log::INFO << "Total charged multiplicity    = " << m.totalChargedMultiplicity() << endl;
+
+    //Analyse the event shape info
+    const Sphericity& s = event.applyProjection(spher);
+    log << Log::INFO << "Sphericity    = " << s.sphericity() << endl;
+    log << Log::INFO << "Aplanarity    = " << s.aplanarity() << endl;
+    log << Log::INFO << "Planarity     = " << s.planarity() << endl;
+
+    // Fill histograms here, and scale them later
+    const double weight = event.weight();
+    histChTot_->fill(m.totalChargedMultiplicity(), weight);
+    histSphericity_->fill(s.sphericity(), weight);
+    histPlanarity_->fill(s.planarity(), weight);
+    histAplanarity_->fill(s.aplanarity(), weight);
+
+    // Finished...
+    log << Log::DEBUG << "Finished analyzing" << endl;
+  }
+
+
+  // Finalize
+  void PL273B181::finalize() { 
+    // Normalize the histogram areas to 1
+    normalize(histChTot_);
+    normalize(histSphericity_);
+    normalize(histPlanarity_); 
+    normalize(histAplanarity_);
+  }
+
+}
