b167b7d3beac0a408f66ef1ea47d1e46c3c4c265
jmb <none@none>
2007-07-02 18:32:01 +0000

    first version of fastJet interface

---
+++ include/Rivet/Projections/FastJets.hh
@@ -0,0 +1,114 @@
+// -*- C++ -*-
+#ifndef RIVET_FastJets_HH
+#define RIVET_FastJets_HH
+
+#include "Rivet/Projections/Projection.hh"
+#include "Rivet/Projections/FinalState.hh"
+#include "Rivet/Particle.hh"
+#include "fastjet/ClusterSequence.hh"
+
+namespace Rivet {
+  
+  /// Project out kT jets found using fastJet package.
+  class FastJets : public Projection {
+    
+  public:
+    
+    /// @name Standard constructors and destructors.
+    //@{
+    /// Default constructor. Must specify a FinalState projection which is
+    //  assumed to live throughout the run.
+    inline FastJets(FinalState& fsp)
+      : _cseq(0), _type(fastjet::kt_algorithm), _recom(fastjet::E_scheme), 
+        _rparameter(1.0), _fsproj(&fsp) 
+    { 
+      addProjection(fsp);
+      _jdef = fastjet::JetDefinition(_type,_rparameter,_recom); 
+    }
+
+    // @todo implement configurable constructor for fastJet
+    /// Argument constructor. Allows the to be run with different parameters.
+    /// Must specify a FinalState projection which is assumed to live throughout the run. 
+    //    inline FastJets(FinalState& fsp, int type, int angle, int recom, double rparameter)
+    //  : _pktev(0), _type(type), _angle(angle), _recom(recom),
+    //    _rparameter(rparameter), _fsproj(&fsp)
+    //{ 
+    //  addProjection(fsp);
+    //}
+    
+    /// Destructor.
+    inline virtual ~FastJets() { 
+      if (_cseq) delete _cseq; 
+    }
+    //@}
+
+  public:
+    /// Return the name of the projection
+    inline string getName() const {
+      return "FastJets";
+    }
+    
+  protected:   
+
+    /// Perform the projection on the Event.
+    void project(const Event& e);
+
+    /// Compare projections.
+    int compare(const Projection& p) const;  
+
+  public:
+    
+    // @todo for more efficiency, pass the pt cut.
+    inline int getNJets() const {
+      return _cseq->inclusive_jets().size();
+    }
+    inline vector<fastjet::PseudoJet> getJetsPt() const {
+      return sorted_by_pt(_cseq->inclusive_jets());
+    }
+
+    /// return the cluster sequence.
+    inline fastjet::ClusterSequence getCSeq() const {
+      return *_cseq;
+    }
+
+    /// return the jet definitions
+    inline fastjet::JetDefinition getJetDef() const {
+      return _jdef;
+    }
+
+    /// Get the subjet splitting variables for the given jet.
+    vector<double> getYSubJet(const fastjet::PseudoJet& jet) const; 
+
+  private:
+    
+
+    /// Internal KtEvent, rebuilt every time an event is projected, but not otherwise.
+    fastjet::ClusterSequence* _cseq;
+
+    fastjet::JetFinder _type;
+    fastjet::RecombinationScheme _recom;
+    double _rparameter;  
+
+    /// The FinalState projection used by this projection.
+    FinalState* _fsproj;
+
+    /// Jet definition
+    fastjet::JetDefinition  _jdef;
+
+    fastjet::Strategy _strat;
+
+
+
+    /// Map of vectors of y scales. This is mutable so we can use caching/lazy evaluation.
+    mutable map<int, vector<double> > _yscales;
+
+  private:
+    
+    /// Hiding the assignment operator.
+    FastJets& operator=(const FastJets&);
+  
+  };
+
+}
+
+#endif
---
+++ src/Projections/FastJets.cc
@@ -0,0 +1,60 @@
+// -*- C++ -*-
+
+#include "Rivet/Tools/Logging.hh"
+#include "Rivet/Projections/FastJets.hh"
+#include "Rivet/Projections/Cmp.hh"
+#include "Rivet/RivetCLHEP.hh"
+
+namespace Rivet {
+
+  int FastJets::compare(const Projection& p) const {
+    const FastJets& other = dynamic_cast<const FastJets&>(p);
+    return \
+      pcmp(*_fsproj, *other._fsproj) || 
+      cmp(_type, other._type) ||
+      cmp(_recom, other._recom) ||
+      cmp(_rparameter, other._rparameter);
+  }
+
+
+  void FastJets::project(const Event& e) {
+    // Project into final state
+
+    const FinalState& fs = e.applyProjection(*_fsproj);
+
+    // Store 4 vector data about each particle into vecs
+    vector<fastjet::PseudoJet> vecs;
+    for (ParticleVector::const_iterator p = fs.particles().begin(); p != fs.particles().end(); ++p) {
+      HepMC::FourVector fv = p->getMomentum();
+      // Store the FourVector in the KtLorentzVector form
+      fastjet::PseudoJet psj(fv.px(),fv.py(),fv.pz(),fv.e());
+      vecs.push_back(psj);
+    }
+    if (_cseq) delete _cseq;
+    _cseq = new fastjet::ClusterSequence(vecs, _jdef);
+  }
+
+
+  vector<double> FastJets::getYSubJet(const fastjet::PseudoJet& jet) const {
+    map<int,vector<double> >::iterator iter = _yscales.find(jet.cluster_hist_index());
+
+    if (iter == _yscales.end()) {
+      fastjet::ClusterSequence cseq(_cseq->constituents(jet),_jdef);
+      vector<double> yMergeVals;
+      for (int i=1; i<4; ++i) {
+	// multiple the dmerge value by R^2 so that it corresponds to a
+	// relative k_t (fastjet has 1/R^2 in the dij distance by default)
+        yMergeVals.push_back(cseq.exclusive_dmerge(i)*_jdef.R()*_jdef.R()/jet.perp2());
+        //yMergeVals.push_back(cseq.exclusive_dmerge(i)*_jdef.R()*_jdef.R());
+      }
+      _yscales.insert(make_pair( jet.cluster_hist_index(), yMergeVals ));
+      return yMergeVals;
+    } else {
+      // This was cached.
+      return iter->second;
+    }
+
+  }
+
+
+}
