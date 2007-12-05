7c75f8909e76a8e8027f6fd6cbef0329e966c64a
buckley <none@none>
2007-12-05 16:33:09 +0000

    Eliminated all mention of CLHEP and KtJet from (active) code.

--- src/Projections/FinalState.cc
+++ src/Projections/FinalState.cc
@@ -20,8 +20,8 @@ namespace Rivet {
           pi != e.genEvent().particles_end(); ++pi ) {
       // Only include particles which are final state (status = 1) and which
       // pass the eta and phi cuts. The eta cut is pre-tested by checking if the
-      // x and y components of the momentum are non-zero since CLHEP might throw
-      // an exception otherwise.
+      // x and y components of the momentum are non-zero since the vectors might
+      // throw an exception otherwise.
       if ( (*pi)->status() == 1 &&
            fabs((*pi)->momentum().x()) > 0.0 &&
            fabs((*pi)->momentum().y()) > 0.0 &&
