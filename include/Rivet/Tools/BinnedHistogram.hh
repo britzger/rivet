5380853466e2e507b48b1e400f904fd10108d099
jmonk <none@none>
2008-01-21 19:48:48 +0000

    Adding BinnedHistogram for storing a series of Histograms of the same quantity that are in different bins of a second quantity (e.g. Jet Pt in different eta regions)

---
+++ include/Rivet/Tools/BinnedHistogram.hh
@@ -0,0 +1,63 @@
+// -*- C++ -*-
+/**
+ *  BinnedHistogram.hh
+ *  Rivet
+ *
+ *  Created by James Monk on 13/12/2007.
+ *
+ */
+#ifndef BINNEDHISTOGRAM_HH
+#define BINNEDHISTOGRAM_HH
+#include "Rivet/Rivet.hh"
+
+namespace Rivet{
+
+  
+  /**
+   * BinnedHistogram contains a series of histograms of the same quantity
+   * each in a different region of a second quantity.  For example, a 
+   * BinnedHistogram may contain histograms of the cross section differential 
+   * in PT in different eta regions.
+   *
+   **/
+  
+  template<typename T> class BinnedHistogram{
+    
+  public:
+    
+    /**
+     * Create a new empty BinnedHistogram
+     */
+    
+    BinnedHistogram(){return;};
+    
+    /**
+     *  Add a histogram in the region between binMin and binMax to this set of 
+     *  BinnedHistograms.
+     */
+    
+    const BinnedHistogram<T> &addHistogram(const T &binMin, 
+                                        const T &binMax, 
+                                        AIDA::IHistogram1D *histo);
+    
+    /**
+     *  Fill the histogram that lies in the same region as bin with the 
+     *  value val of weight weight.
+     */
+    
+    AIDA::IHistogram1D* const fill(const T &bin,
+                                const T &val,
+                                const double &weight);
+    
+    const vector<AIDA::IHistogram1D*> &getHistograms()const{return _histos;};
+    
+  private:
+    
+    map<T, AIDA::IHistogram1D*> _histosByUpperBound;
+    map<T, AIDA::IHistogram1D*> _histosByLowerBound;
+    vector<AIDA::IHistogram1D*> _histos;
+    
+  };
+}
+
+#endif
---
+++ src/Tools/BinnedHistogram.cc
@@ -0,0 +1,79 @@
+// -*- C++ -*-
+/**
+ *  BinnedHistogram.cpp
+ *  Rivet
+ *
+ *  Created by James Monk on 13/12/2007.
+ *
+ */
+
+#include "Rivet/BinnedHistogram.hh"
+#include "Rivet/RivetAIDA.hh"
+
+namespace Rivet{
+
+  template<typename T> 
+  const BinnedHistogram<T> &BinnedHistogram<T>::addHistogram
+  (const T &binMin, const T &binMax, AIDA::IHistogram1D *histo){
+    
+    if(binMin > binMax) throw runtime_error
+      ("Cannot add a binned histogram where the lower bin edge is above the upper edge");
+    
+    _histosByUpperBound[binMax] = histo;
+    _histosByLowerBound[binMin] = histo;
+    bool found = false;
+    for(vector<AIDA::IHistogram1D*>::iterator histIt = _histos.begin();
+        histIt != _histos.end() && !found; ++histIt){
+      if((*histIt)==histo) found = true;
+    }
+    
+    if(!found){
+      _histos.push_back(histo);
+    }
+    
+    return *this;
+  }
+////////////////////////////////////////////////////////////////////////////////
+  template<typename T>
+  AIDA::IHistogram1D* const BinnedHistogram<T>::fill(const T &bin,
+                                                     const T &val,
+                                                     const double &weight){
+
+    typename map<T, AIDA::IHistogram1D*>::iterator histIt = 
+      _histosByUpperBound.upper_bound(bin);
+    //check that the bin is not out of range
+    if(histIt == _histosByUpperBound.end()){
+      return 0;
+    }
+    
+    AIDA::IHistogram1D* histo = histIt->second;
+    
+    histIt = _histosByLowerBound.lower_bound(bin);
+    //no need to check going beyond the upper bound if we already passed above
+    //(given that upper bound > lower bound is checked)
+    //check it is not before the start of the map
+    if(histIt == _histosByLowerBound.begin()){
+      return 0;
+    }
+    //lowerbound actually gives us the iterator one above the nearest element, 
+    //so decrement it.  
+    //This is safe because we already checked we're not at the start!
+    --histIt;
+    
+    if(histo!=histIt->second){
+      return 0;
+    }
+    
+    histo->fill(val, weight);
+    
+    return histo;
+  }
+////////////////////////////////////////////////////////////////////////////////
+  //Template declarations for the compiler.
+  template class BinnedHistogram<double>;
+  template class BinnedHistogram<int>;
+  template class BinnedHistogram<float>;
+  
+}
+////////////////////////////////////////////////////////////////////////////////
+
