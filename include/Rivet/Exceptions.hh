fe1425b9d96d78e8b7eda121d86a27effd8e7467
buckley <none@none>
2008-05-19 12:58:11 +0000

    Added a proper set of Rivet exceptions.

--- include/Rivet/Exceptions.hh
+++ include/Rivet/Exceptions.hh
@@ -1,15 +1,46 @@
-// $Id: $
 #ifndef RIVET_EXCEPTIONS_HH 
-#define RIVET_EXCEPTIONS_HH 1
+#define RIVET_EXCEPTIONS_HH
 
+#include <string>
+#include <exception>
 #include <stdexcept>
 
 namespace Rivet {
 
-  class RivetException : public exception {
+  /// Basic unspecialised Rivet exception.
+  class Exception : public std::exception { 
+  public:
+    Exception() : std::exception() {}
+  };
 
-  }
+  /// @brief Generic runtime Rivet error.
+  class Error : public std::runtime_error, public Exception {
+  public:
+    Error(const std::string& what) : std::runtime_error(what) {} 
+  };
+
+
+  /// Error for e.g. use of invalid bin ranges.
+  class RangeError : public std::out_of_range, public Exception {
+  public:
+    RangeError(const std::string& what) : std::out_of_range(what) {} 
+  };
+
+
+  /// @todo Clarify where this might arise!
+  class LogicError : public std::logic_error, public Exception {
+  public:
+    LogicError(const std::string& what) : std::logic_error(what) {} 
+  };
+
+  /// @brief Errors relating to event/bin weights
+  /// Arises in computing statistical quantities because e.g. the bin
+  /// weight is zero or negative.
+  class WeightError : public Error {
+  public:
+    WeightError(const std::string& what) : Error(what) {} 
+  };
 
 }
 
-#endif // RIVET_EXCEPTIONS_HH
+#endif
