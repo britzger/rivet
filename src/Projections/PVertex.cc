1d195f52816ae09741a94ab1b394e55b23b24693
buckley <none@none>
2008-05-21 15:03:49 +0000

    Improved on the HepMC production vertex mess - untested and might be fragile.

--- src/Projections/PVertex.cc
+++ src/Projections/PVertex.cc
@@ -1,33 +1,28 @@
 // -*- C++ -*-
-
 #include "Rivet/Rivet.hh"
 #include "Rivet/Projections/PVertex.hh"
 #include "HepMC/GenVertex.h"
 #include "HepMC/GenEvent.h"
 
-
 namespace Rivet {
 
-  void PVertex::project(const Event& e) {
 
+  void PVertex::project(const Event& e) {
+    // We'll *try* to do it right, in case any generators are doing the right thing...
     _thePVertex = e.genEvent().signal_process_vertex();
     getLog() << Log::DEBUG << "PVertex ptr from HepMC = " << _thePVertex << endl;
-
-    // Since no signal vertices are filled (nor Fortran, neither C++ MC's)
-    // the decay vertex from first particle (supposed to be beam particle)
-    // is set as Primary Vertex to get right vertex positions. 
-
-    /// @todo Why does this line lead to a segv?
-    //if (_thePVertex) cout << "_thePVertex=0" << endl;
-    //_thePVertex = (*(e.genEvent().particles_begin()))->production_vertex();
-    _thePVertex = (*(e.genEvent().particles_begin()))->end_vertex();
-
-
-    const size_t pVertexParticleSize = _thePVertex->particles_in_size();
-    //if ( !_thePVertex || pVertexParticleSize != 2 )
-    if ( !_thePVertex )
-    if (pVertexParticleSize != 2 )
+    if (!_thePVertex) {
+      // Since no signal vertices are filled in existing Fortran & C++ MC's,
+      // the decay vertex from first particle (supposed to be beam particle)
+      // is set as Primary Vertex to get right vertex positions. 
+      _thePVertex = (*(e.genEvent().particles_begin()))->end_vertex();
+    }
+    assert(_thePVertex);
+    const unsigned int pVertexParticleSize = _thePVertex->particles_in_size();
+    if (pVertexParticleSize != 2 ) {
       throw Error("Wrong number of Primary Vertex particles: " + pVertexParticleSize);
+    }
   }
+
 
 }
