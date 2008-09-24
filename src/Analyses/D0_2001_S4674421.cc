e619055c8f2905d30aebe4746904bd2cb029500a
holsch <none@none>
2008-09-24 22:45:16 +0000

    Fixed a small typo that lead to histo 'd02-x01-y01' being registered twice, once as '/D0_2001_S4674421/d02-x01-y01' and the other time as '/D0_2001_S4674421d02-x01-y01'. But still there seem to be some severe scaling issues.

--- src/Analyses/D0_2001_S4674421.cc
+++ src/Analyses/D0_2001_S4674421.cc
@@ -53,7 +53,7 @@ namespace Rivet {
     // Correct Z pT distribution to Z cross-section
     const double xSecZ = xSecPerEvent * _eventsFilledZ;
 
-    _h_dsigdpt_wz_rat = histogramFactory().divide(getName() + "d02-x01-y01", *_h_dsigdpt_w, *_h_dsigdpt_z);
+    _h_dsigdpt_wz_rat = histogramFactory().divide(getName() + "/d02-x01-y01", *_h_dsigdpt_w, *_h_dsigdpt_z);
     _h_dsigdpt_wz_rat->scale(xSecW/xSecZ * _mwmz * _brzee / _brwenu);
 
     normalize(_h_dsigdpt_w, xSecW);
