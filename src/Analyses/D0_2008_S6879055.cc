758f35fdf52bdc740250358952cc3aaa928c581f
buckley <none@none>
2008-11-18 00:38:49 +0000

    Tidying up... D0 2008 S6... is a bit messy

--- src/Analyses/D0_2008_S6879055.cc
+++ src/Analyses/D0_2008_S6879055.cc
@@ -55,65 +55,65 @@ namespace Rivet {
 
   // Do the analysis 
   void D0_2008_S6879055::analyze(const Event & event) {
-    Log & log = getLog();
-    log << Log::DEBUG << "Starting analyzing" << endl;
+    getLog() << Log::DEBUG << "Starting analyzing" << endl;
     double weight = event.weight();
-    log << Log::DEBUG << "Event weight is " << weight << endl;
+    getLog() << Log::DEBUG << "Event weight is " << weight << endl;
 
     //skip if the event is empty
     const FinalState & fs = applyProjection<FinalState>(event, "FS");
-    if (fs.isEmpty())
+    if (fs.isEmpty()) {
       return;
+    }
 
     //check that the primary vertex is within 60 cm in z from (0,0,0)
-    const PVertex & vertex = applyProjection<PVertex>(event, "PrimaryVertex");
-    log << Log::DEBUG << "Primary vertex is at " << vertex.getPVPosition() << endl;
-    // skip event if the vertex position is not within 60 cm.
-    if (fabs(vertex.getPVPosition().z()) > 60.) {
-      log << Log::DEBUG << "Skipping event " << event.genEvent().event_number()
-        << " because vertex position in z is " << vertex.getPVPosition().z() << endl;
+    const PVertex& vertex = applyProjection<PVertex>(event, "PrimaryVertex");
+    getLog() << Log::DEBUG << "Primary vertex is at " << vertex.getPVPosition()/cm << " cm" << endl;
+    // Skip event if the vertex position is not within 60 cm.
+    if (fabs(vertex.getPVPosition().z())/cm > 60) {
+      getLog() << Log::DEBUG << "Skipping event " << event.genEvent().event_number()
+               << " because vertex position in z is " << vertex.getPVPosition().z()/cm << endl;
       return;
     }
-    //find the Z candidates
+    // Find the Z candidates
     const InvMassFinalState & invmassfs = applyProjection<InvMassFinalState>(event, "ElectronsFromZ");
-    //if there is no Z candidate in the FinalState skip event,
+    // If there is no Z candidate in the FinalState, skip the event
     if (invmassfs.isEmpty()) {
-      log << Log::DEBUG << "Skipping event " << event.genEvent().event_number()
-        << " because no Z candidate is found " << endl;
+      getLog() << Log::DEBUG << "Skipping event " << event.genEvent().event_number()
+               << " because no Z candidate is found " << endl;
       return;
     }
-    //now build the jets on a fs without the electrons from Z
+    // Now build the jets on a fs without the electrons from Z
     const D0ILConeJets & jetpro = applyProjection<D0ILConeJets>(event, "Jets");
 
-    //take the jets with pt > 20
-    const list < FourMomentum > &jets = jetpro.getLorentzJets();
-    list < FourMomentum > jets_aboveptmin;
-    list < FourMomentum >::const_iterator jetbegin = jets.begin();
-    list < FourMomentum >::const_iterator jetend = jets.end();
-    for (list < FourMomentum >::const_iterator ijet = jetbegin; ijet != jetend; ++ijet) {
-      if (ijet->pT() > 20)
-        jets_aboveptmin.push_back(*ijet);
+    // Take the jets with pT > 20
+    /// @todo Purge the evil!
+    const list<FourMomentum>& jets = jetpro.getLorentzJets();
+    list<FourMomentum> jets_aboveptmin;
+    foreach (const FourMomentum& ijet, jets) {
+      if (ijet.pT()/GeV > 20) {
+        jets_aboveptmin.push_back(ijet);
+      }
     }
-    log << Log::DEBUG << "jets above 20 GeV size = " << jets_aboveptmin.size() << endl;
+    getLog() << Log::DEBUG << "Num jets above 20 GeV = " << jets_aboveptmin.size() << endl;
 
-    //check they are isolated form leptons
+    // Check they are isolated from leptons
     const D0JetFromParticleIso & isoJet = applyProjection<D0JetFromParticleIso>(event, "JetIsolation");
     if (isoJet.getIsolatedParticles(1).size() != jets_aboveptmin.size()) {
-      log << Log::DEBUG << "skipping event " << event.genEvent().event_number()
-        << " because Jet Isolated from lepton size " << isoJet.getIsolatedParticles(0).size()
-        << " is different from jet size " << jets_aboveptmin.size() << endl;
+      getLog() << Log::DEBUG << "Skipping event " << event.genEvent().event_number()
+               << " because jet isolated from lepton size " << isoJet.getIsolatedParticles(0).size()
+               << " is different from jet size " << jets_aboveptmin.size() << endl;
       return;
     }
-    //now take only the jets with |eta|<2.5
-    list < FourMomentum > finaljet_list;
-    for (list < FourMomentum >::const_iterator ijet = jets_aboveptmin.begin(); ijet != jets_aboveptmin.end(); ++ijet) {
-      if (fabs(ijet->pseudorapidity()) < 2.5)
-        finaljet_list.push_back(*ijet);
-      //if (fabs(ijet->pseudorapidity()) < 5) finaljet_list.push_back(*ijet);
+    // Now take only the jets with |eta|<2.5
+    list<FourMomentum> finaljet_list;
+    foreach (const FourMomentum& ijet, jets_aboveptmin) {
+      if (fabs(ijet.pseudorapidity()) < 2.5) {
+        finaljet_list.push_back(ijet);
+      }
     }
-    log << Log::DEBUG << "Jets above 20 GeV and with |eta| < 2.5  size = " << finaljet_list.size() << endl;
+    getLog() << Log::DEBUG << "Num jets above 20 GeV and with |eta| < 2.5 = " << finaljet_list.size() << endl;
 
-    //now count....
+    // Now count....
     _events += weight;
     //_crossSectionRatio->fill(0, weight);
     if (finaljet_list.size() >= 1) {
@@ -132,27 +132,20 @@ namespace Rivet {
       _crossSectionRatio->fill(4, weight);
       _crossSectionRatioNormToDataBin1->fill(4, weight);
     }
-
-    // Finished
-    log << Log::DEBUG << "Finished analyzing" << endl;
   }
 
 
 
   // Finalize
   void D0_2008_S6879055::finalize() {
-    Log & log = getLog();
-
-    //now divide by the inclusive result
-    log << Log::DEBUG << "Entries for bin 0 " << _crossSectionRatio->binEntries(0) << endl;
-    //if (_crossSectionRatio->binEntries(0) != 0){
+    // Now divide by the inclusive result
+    getLog() << Log::DEBUG << "Entries for bin 0 " << _crossSectionRatio->binEntries(0) << endl;
     if (_events != 0) {
       _crossSectionRatio->scale((double) 1 / _events);
       _crossSectionRatioNormToDataBin1->scale((double) 1 / _events);
       _crossSectionRatioNormToDataBin1->scale(0.1201 / _crossSectionRatio->binHeight(1));
     }
-    log << Log::DEBUG << "Finished!" << endl;
-
+    getLog() << Log::DEBUG << "Finished!" << endl;
   }
 
 }
