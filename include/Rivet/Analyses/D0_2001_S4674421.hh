9315502452b4bd99fef36028f7181263b212e3e8
fsiegert <none@none>
2009-01-30 13:07:40 +0000

    D0_2001_S4674421.hh: Remove obsolete DPS declaration

--- include/Rivet/Analyses/D0_2001_S4674421.hh
+++ include/Rivet/Analyses/D0_2001_S4674421.hh
@@ -135,7 +135,6 @@ namespace Rivet {
     AIDA::IHistogram1D* _h_dsigdpt_w;
     AIDA::IHistogram1D* _h_dsigdpt_z;
     AIDA::IHistogram1D* _h_dsigdpt_scaled_z;
-    AIDA::IDataPointSet* _dset_dsigpt_wz_rat;
    //@}    
 
     /// Hide copy assignment operator.
