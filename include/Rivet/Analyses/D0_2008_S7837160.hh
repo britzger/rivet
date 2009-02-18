cd93406348e214900062259a525524404c625136
buckley <none@none>
2009-02-18 15:57:48 +0000

    Metadata added for D0_2008_S7837160

--- include/Rivet/Analyses/D0_2008_S7837160.hh
+++ include/Rivet/Analyses/D0_2008_S7837160.hh
@@ -34,20 +34,45 @@ namespace Rivet {
     /// A short description of the analysis.
     string summary() const {
       return "Measurement of W charge asymmetry from D0 Run II";
-    }    
+    }
+    /// A full description of the analysis.
+    string description() const {
+      ostringstream os;
+      os << "Measurement of the electron charge asymmetry in p pbar -> W + X -> e nu_e  + X "
+         << "events at a center of mass energy of 1.96 TeV. The asymmetry is measured as " 
+         << "a function of the electron transverse momentum and pseudorapidity in the "
+         << "interval (-3.2, 3.2)."
+         << "\n\n"
+         << "This data is sensitive to proton parton distribution functions due to the "
+         << "valence asymmetry in the incoming quarks which produce the W. Initial state "
+         << "radiation should also affect the pT distribution.";
+      return os.str();
+    }
     /// Experiment which performed and published this analysis. 
     string experiment() const {
       return "D0";
+    }
+    /// Collider on which the experiment ran.
+    string collider() const {
+      return "Tevatron Run 2";
     }
     /// When published (preprint year according to SPIRES). 
     string year() const {
       return "2008";
     }
+    /// Names & emails of paper/analysis authors.
+    vector<string> authors() const {
+      vector<string> rtn;
+      rtn += "Andy Buckley <andy.buckley@durham.ac.uk>";
+      rtn += "Gavin Hesketh <gavin.hesketh@cern.ch>";
+      return rtn;
+    }
     /// Publication references.
     vector<string> references() const {
       vector<string> ret;
-      //ret.push_back("Phys.Rev.Lett.");
-      ret.push_back("arXiv:0807.3367");
+      ret += "Phys.Rev.Lett.101:211801,2008";
+      ret += "doi:10.1103/PhysRevLett.101.211801";
+      ret += "arXiv:0807.3367v1 [hep-ex]";
       return ret;
     }
     //@}
