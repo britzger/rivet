a8334b79e2bd5bef8231415bf59098e538628362
jmonk <none@none>
2009-02-19 15:28:05 +0000

    Added analysis metadata

--- include/Rivet/Analyses/ZEUS_2001_S4815815.hh
+++ include/Rivet/Analyses/ZEUS_2001_S4815815.hh
@@ -47,10 +47,56 @@ namespace Rivet {
     string experiment() const {
       return "ZEUS";
     }
+    /// Collider on which the experiment ran.
+    string collider() const{
+      return "HERA Run I";
+    }
+    
     /// When published (preprint year according to SPIRES).
     string year() const {
       return "2001";
     }
+    
+    /// Names & emails of paper/analysis authors.
+    vector<string> authors() const {
+      vector<string> rtn;
+      rtn.push_back("Jon Butterworth <jmb@hep.ucl.ac.uk>");
+      return rtn;
+    }
+    
+    /// A full description of the analysis.
+    string description() const {
+      ostringstream os;
+      os<< "ZEUS photoproduction of jets from proton-positron collisions at beam energies of 820~GeV on 27.5~GeV."
+        << " Photoproduction can either be direct, in which case the photon interacts directly with the parton," 
+        << " or resolved, in which case the photon acts as a source of quarks and gluons."
+        << " A photon-proton centre of mass energy of between 134~GeV and 227~GeV is probed," 
+        << " with values of xP, the fractional momentum of the partons inside the proton, predominantly in the region"
+        << " between 0.01 and 0.1. The fractional momentum of the partons from the photon, $x\\gamma$, is in the region 0.1 to 1."
+        << " Jets are reconstructed in the range $-1<|\\eta|<2.4$ using the kT algorithm with an R parameter of 1.0."
+        << " The minimum pT of the leading jet should be greater then 14~GeV, and at least one other jet must have pT>11~GeV.";
+      return os.str();
+    }
+    
+    /// Information about the events needed as input for this analysis.
+    string runInfo() const {
+      ostringstream os;
+      os << "HERA beam conditions: 820~GeV protons colliding with 27.5~GeV positrons."
+         << "Direct and resolved photoproduction of di-jets."
+         << "Leading jet pT > 14~GeV, second jet pT > 11~GeV."
+         << "Jet pseudorapidity $-1 < \\eta < 2.4$.";
+      return os.str();
+    }
+    
+    /// Journal, and preprint references.
+    vector<string> references() const {
+      vector<string> refs;
+      refs.push_back( "Eur.Phys.J.C23:615,2002" );
+      refs.push_back( "DESY 01/220" );
+      refs.push_back("hep-ex/0112029");
+      return refs;
+    }
+    
     //@}
 
     /// @name Analysis methods
