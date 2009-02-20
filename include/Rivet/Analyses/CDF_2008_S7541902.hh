65eef4a3e93141adb0311bbeada7c06af352d2b0
nurse <none@none>
2009-02-20 03:04:15 +0000

    Add metadata and change mT2 cut to mT cut for clarity

--- include/Rivet/Analyses/CDF_2008_S7541902.hh
+++ include/Rivet/Analyses/CDF_2008_S7541902.hh
@@ -33,7 +33,7 @@ namespace Rivet {
     /// Constructor
     CDF_2008_S7541902()
       : _electronETCut(20.0 *GeV), _electronETACut(1.1),
-        _eTmissCut(30.0 *GeV), _mT2Cut(200.0 * GeV * GeV),
+        _eTmissCut(30.0 *GeV), _mTCut(20.0 *GeV),
         _jetEtCutA(20.0 *GeV),  _jetEtCutB(25.0 *GeV), _jetETA(2.0),
         _xpoint(1960.)
     {
@@ -78,22 +78,66 @@ namespace Rivet {
     string summary() const {
       return "Jet pT distributions for 4 jet multiplicity bins as well as the jet multiplicity distribution in W + jets events.";
     }
+    
+    /// A full description of the analysis.
+    string description() const {
+      ostringstream os;
+      os <<  "Measurement of the cross section for W boson production in association with jets in $p\bar{p}$" 
+	 <<  "collisions at $\sqrt{s}=1.96$ TeV. The analysis uses 320 pb$^{-1}$ of data collected with the"
+	 <<  "CDF II detector."
+	 <<  "W bosons are identified in their $e\nu$ decay channel and"
+	 <<  "jets are reconstructed using an $R < 0.4$ cone algorithm."
+	 <<  "For each $W + \geq$ n-jet sample (where n = 1--4) a measurement of"
+	 <<  "d$\sigma({p}\bar{p} \rightarrow W + \geq$ n jet)/d$E_T(n^{th}$-jet) $\times$ BR($W \rightarrow{e}\nu$)"
+	 <<  "is made, where d$E_T(n^{th}$-jet) is the Et of the n$^{th}$-highest Et jet above 20 GeV."
+	 <<  "A measurement of the total cross section," 
+	 <<  "$\sigma(p\bar{p} \rightarrow W + \geq$ n-jet) $\times$ BR($W \rightarrow{e}\nu)$ with"
+	 <<  "$E_T(n^{th}-jet) > 25$ GeV is also made." 
+	 <<  "Both measurements are made for jets with $|\eta| < 2$ and for a limited reigon of"
+	 <<  "the $W \rightarrow{e}\nu$ decay phase space: $|\eta^{e}| < 1.1$, $p_{T}^{e} > 20$ GeV,"
+	 <<  "$p_{T}^{\nu} > 30$ GeV and $M_{T} > 20$ GeV."
+	 <<  "The cross sections are corrected for all detector effects and can be directly compared"
+	 <<  "to particle level $W$ + jet(s) predictions."
+	 <<  "These measurements can be used to test and tune QCD predictions for the number of jets"                             <<   "in and kinematics of $W$ + jets events.";
+      return os.str();
+    }
+  /// Characteristics of events to be processed by this analysis
+    string runInfo() const {
+      ostringstream os;
+      os << "Requires the process ppbar->W->enu,"
+         << "additional hard jets will also have to be included to get a good description." 
+	 << "The LO process in Herwig is set with IPROC=1451";
+      return os.str();
+    }
+   
     /// Experiment which performed and published this analysis.
     string experiment() const {
       return "CDF";
+    }
+    /// Collider on which the experiment ran.
+    string collider() const {
+      return "Tevatron Run 2";
     }
     /// When published (preprint year according to SPIRES).
     string year() const {
       return "2008";
     }
+
+   /// Names & emails of paper/analysis authors.
+    vector<string> authors() const {
+      vector<string> rtn;
+      rtn += "Emily Nurse <nurse@hep.ucl.ac.uk>";
+      return rtn;
+    }
+    
     /// Journal, and preprint references.
     vector<string> references() const {
       vector<string> ret;
       ret.push_back("arXiv:0711.4044 [hep-ex]");
+      ret.push_back("Phys.Rev.D77:011108,2008");
       return ret;
     }
     //@}
-
 
     /// @name Analysis methods
     //@{
@@ -114,7 +158,7 @@ namespace Rivet {
     /// Cut on the missing ET
     double _eTmissCut;
     /// Cut on the transverse mass squared
-    double _mT2Cut;
+    double _mTCut;
     /// Cut on the jet ET for differential cross sections
     double _jetEtCutA;
     /// Cut on the jet ET for jet multiplicity
@@ -132,6 +176,9 @@ namespace Rivet {
     AIDA::IDataPointSet* _histJetMultRatio[4];
     AIDA::IHistogram1D* _histJetMult[4];
     //@}
+
+
+
 
   };
 
