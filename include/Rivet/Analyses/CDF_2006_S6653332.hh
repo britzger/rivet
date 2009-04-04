cfbebef43864388fe32a7dc4238dd444e842fa46
nurse <none@none>
2009-04-04 21:54:30 +0000

    The only result from this paper is the inclusive cross section and ratio. Plot these instead of the differential distributions, and remove lepton ID cuts

--- include/Rivet/Analyses/CDF_2006_S6653332.hh
+++ include/Rivet/Analyses/CDF_2006_S6653332.hh
@@ -6,12 +6,9 @@
 #include "Rivet/Projections/FinalState.hh"
 #include "Rivet/Projections/VetoedFinalState.hh"
 #include "Rivet/Projections/ChargedFinalState.hh"
-#include "Rivet/Projections/TotalVisibleMomentum.hh"
-#include "Rivet/Projections/D0ILConeJets.hh"
+#include "Rivet/Projections/InvMassFinalState.hh"
 #include "Rivet/Projections/FastJets.hh"
 #include "Rivet/Projections/ChargedLeptons.hh"
-#include "Rivet/Projections/PVertex.hh"
-#include "Rivet/Projections/SVertex.hh"
 
 namespace Rivet {
 
@@ -28,9 +25,31 @@ namespace Rivet {
 
     /// @name Constructors etc.
     //@{
-    
     /// Constructor
-    CDF_2006_S6653332();
+    CDF_2006_S6653332()  : _Rjet(0.7), _JetPtCut(20.), _JetEtaCut(1.5),
+			   _sumWeightsWithZ(0.0), _sumWeightsWithZJet(0.0)
+    { 
+      setBeams(PROTON, ANTIPROTON);
+      setNeedsCrossSection(true);
+      const FinalState fs(-3.6, 3.6);
+      addProjection(fs, "FS");
+
+      // Create a final state with any e+e- or mu+mu- pair with 
+      // invariant mass 76 -> 106 GeV and ET > 20 (Z decay products)
+      std::vector<std::pair<long,long> > vids;
+      vids.push_back(make_pair(ELECTRON, POSITRON));
+      vids.push_back(make_pair(MUON, ANTIMUON));
+      FinalState fs2(-3.6, 3.6);
+      InvMassFinalState invfs(fs2, vids, 76*GeV, 106*GeV);
+      addProjection(invfs, "INVFS");
+      // Make a final state without the Z decay products for jet clustering
+      VetoedFinalState vfs(fs);
+      vfs.addVetoOnThisFinalState(invfs);
+      addProjection(vfs, "VFS");
+      addProjection(FastJets(vfs, FastJets::CDFMIDPOINT, 0.7), "Jets");
+
+    }
+
 
     /// Factory method.
     static Analysis* create() { 
@@ -117,101 +136,20 @@ namespace Rivet {
 
   private:
 
-    /// @name Analysis cuts
+
+    double _Rjet;
+    double _JetPtCut;
+    double _JetEtaCut;
+
+    double _sumWeightsWithZ;
+    double _sumWeightsWithZJet;
+
+
     //@{
-    /// Cut on primary vertex z-position (\f$ z(\text{PV}) < 60 \text{cm} \f$)
-    const double _pvzmax;
-
-    /// Cut on missing transverse energy (\f$ \text{MET} < 25 \text{GeV} \f$)
-    const double _metmax;
-
-    /// Cut on scalar sum of transverse energy (\f$ \text{SET} < 150 \text{GeV} \f$)
-    const double _setmax;
-
-    /// Cut on leptons minimum transverse momentum (\f$ p_T(\text{lep.}) > 10 \text{GeV} \f$) 
-    const double _lepPtmin;
-
-    /// Cut on max. pseudorapidity of electrons/calorimeter (\f$ |\eta(e)| < 3.6 \f$) 
-    const double _eletamax;
-
-    /// Cut on max. pseudorapidity of muons/muon chambers (\f$ |\eta(\mu)| < 1.5 \f$) 
-    const double _muetamax;
-
-    /// Cut on min invariant dilepton mass (\f$ m(ll) > 66 \text{GeV} \f$)
-    const double _mllmin;
-
-    /// Cut on max invariant dilepton mass (\f$ m(ll) < 116 \text{GeV} \f$)
-    const double _mllmax;
-
-    /// Cut on trigger lepton min transverse momentum (\f$ p_T > 18 \text{GeV} \f$)
-    const double _triglepPtmin;
-
-    /// Cut on trigger electron max pseudorapidity (\f$ |\eta(e)| < 1.1 \f$)
-    const double _trigeletamax;
-
-    /// Cut on trigger muon max pseudorapidity (\f$ |\eta(\mu)| < 1.0 \f$)
-    const double _trigmuetamax;
-
-    /// Cut on lepton isolation radius (\f$ R_\text{isol} < 0.4 \f$)
-    const double _Rlepisol;
-
-    /// Hadronic calorimeter fraction (fh = 0.8)
-    const double _fh;
-
-    /// Hadronic/el.magn. calo. fraction cut const (fhfemconst = 0.055)
-    const double _fhfemconst;
-    
-    /// Hadronic/el.magn. calo. fraction cut slope (fhfemslope = 0.00045)
-    const double _fhfemslope;
-
-    /// Muon Energy cut case separation (muEsep = 100 GeV)
-    const double _muEsep;
-
-    /// Muon E hadr. isol. threshold (muEhMin = 6 GeV)
-    const double _muEhMin;
-
-    /// Muon E el.mag. isol. threshold (muEemMin = 2 GeV)
-    const double _muEemMin;
-
-    /// Muon had. Energy isol. cut slope (muEhslope = 0.0280)
-    const double _muEhslope;
-
-    /// Muon el.mag. Energy isol. cut slope (muEemslope = 0.0115)
-    const double _muEemslope;
-    
-    /// Jets transverse momentum cut (jetPtmin = 20 GeV)
-    const double _jetPtmin;
-
-    /// Jets pseudorapidity cut (jetetamax = 1.5)
-    const double _jetetamax;
-
-    /// Max. distance (eta,phi) between vertex vis. momentum and jet to be probed (vtxjetRmax= 0.7) 
-    const double _vtxjetRmax;
-
-    /// Tracker geometrical acceptance (\f$ \eta < 2.0 \f$)
-    const double _trketamax;
-
-    /// Impact Parameter resolution (ipres = 34e-3mm), including beam spot
-    const double _ipres;
-
-    /// Cut on Decay Length Significance (dlsmin = 7.5)
-    const double _dlsmin;
-
-    /// Decay Length Significance resolution (assumed to be (dlsres = 34e-3mm))
-    const double _dlsres;
-    //@}
-
-
-    /// Set of vectors defining the jet axes (for detached vertex finding).
-    vector<FourMomentum> _jetaxes;
-
-    
-    /// @name Histogram collections
-    //@{
-    AIDA::IHistogram1D* _histJetsPt;
-    AIDA::IHistogram1D* _histJetsEta;
-    AIDA::IHistogram1D* _histbJetsPt;
-    AIDA::IHistogram1D* _histbJetsEta;
+    /// Histograms
+    AIDA::IHistogram1D* _sigmaBJet;
+    AIDA::IHistogram1D* _ratioBJetToZ;
+    AIDA::IHistogram1D* _ratioBJetToJet;
     //@}
 
   };
