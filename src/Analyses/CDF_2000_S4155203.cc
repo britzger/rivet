5f91ae784e371eec490499097c96a1f9cbffa57f
buckley <none@none>
2009-05-05 16:10:29 +0000

    Fixing CDF 2000 Z pT analysis to remove acceptance cuts (these have been 'corrected' in the data) and to use the ZFinder projection to do almost everything.

--- src/Analyses/CDF_2000_S4155203.cc
+++ src/Analyses/CDF_2000_S4155203.cc
@@ -4,10 +4,7 @@
 #include "Rivet/Rivet.hh"
 #include "Rivet/Tools/Logging.hh"
 #include "Rivet/Analyses/CDF_2000_S4155203.hh"
-#include "Rivet/Projections/ChargedFinalState.hh"
-#include "Rivet/Projections/LossyFinalState.hh"
-#include "Rivet/Projections/IdentifiedFinalState.hh"
-#include "Rivet/Projections/ChargedLeptons.hh"
+#include "Rivet/Projections/ZFinder.hh"
 #include "Rivet/RivetAIDA.hh"
 
 namespace Rivet {
@@ -15,102 +12,32 @@ namespace Rivet {
 
   CDF_2000_S4155203::CDF_2000_S4155203() { 
     setBeams(PROTON, ANTIPROTON);
-    const FinalState fs(-4.2, 4.2, 15*GeV);
-
-    // Get photons  
-    IdentifiedFinalState ifs(fs);
-    ifs.acceptId(PHOTON);
-    addProjection(ifs, "PhotonFS");
-
-    // Leading electrons
-    // LeadingParticlesFinalState lpfs(fs);
-    // lpfs.addParticleId(ELECTRON).addParticleId(POSITRON);
-    // addProjection(lpfs, "LeadingElectrons");  
-    addProjection(ChargedLeptons(fs), "CL");
+    ZFinder zfinder(FinalState(), ELECTRON, 66.0*GeV, 116.0*GeV, 0.2);
+    addProjection(zfinder, "ZFinder");
   }
-
 
 
   // Book histograms
   void CDF_2000_S4155203::init() {
-    _hist_zpt = bookHistogram1D(1, 1, 1, "$p_\\perp$ of Z boson in $\\Pelectron \\Ppositron$ decays",
-                                "$p_\\perp(\\PZ)$ / GeV",
-                                "$\\d{\\sigma}/\\d{p_\\perp(\\PZ)}$"); //< @todo Cross-section units
+    /// @todo Cross-section units in label
+    _hist_zpt =
+      bookHistogram1D(1, 1, 1, "$p_\\perp$ of Z boson in $\\Pelectron \\Ppositron$ decays",
+                      "$p_\\perp(\\PZ)$ / GeV", "$\\d{\\sigma}/\\d{p_\\perp(\\PZ)}$");
   }
 
 
   // Do the analysis
   void CDF_2000_S4155203::analyze(const Event& e) {
-    const double weight = e.weight();
-
-    // Get the leptons
-    const ParticleVector& leptons = applyProjection<ChargedLeptons>(e, "CL").chargedLeptons();
-    ParticleVector electrons;
-    electrons.clear();
-
-    bool has_central_electron = false;
-    foreach (const Particle& l, leptons) {
-      // Only take electrons
-      if (abs(l.pdgId()) != ELECTRON) continue;
-      
-      // Check for electrons in the fiducial regions
-      if (fabs(l.momentum().pseudorapidity()) < 1.1 &&
-          l.momentum().pT()/GeV > 20) {
-        has_central_electron = true;
-        electrons.push_back(l);
-      }
-      else if (fabs(l.momentum().pseudorapidity()) > 1.1 &&
-               fabs(l.momentum().pseudorapidity()) < 4.2 &&
-               l.momentum().pT()/GeV > 15) {
-        electrons.push_back(l);
-      }
-    }
-
-    // One electron needs to be in the central region
-    if (! has_central_electron) {
+    const ZFinder& zfinder = applyProjection<ZFinder>(e, "ZFinder");
+    if (zfinder.particles().size() != 1) {
+      getLog() << Log::DEBUG << "No unique e+e- pair found" << endl;
       vetoEvent(e);
     }
 
-    // We want exactly two electrons with opposite charge
-    getLog() << Log::DEBUG << "Electron multiplicity = " << electrons.size() << endl;
-    if (electrons.size() != 2 || electrons[0].pdgId() != -electrons[1].pdgId() ) {
-      vetoEvent(e);
-    }
-
-    
-    // Add back in photons that could have radiated from the Z decay products
-    FourMomentum e1 = electrons[0].momentum();
-    FourMomentum e2 = electrons[1].momentum();
-    const ParticleVector photons = applyProjection<FinalState>(e, "PhotonFS").particles();
-    const double HALO_RADIUS = 0.2;
-    foreach (const Particle& p, photons) {
-      const double pho_eta = p.momentum().pseudorapidity();
-      const double pho_phi = p.momentum().azimuthalAngle();
-      // NB. Not bothered about double-counting photons lying between the electrons
-      if (deltaR(e1.pseudorapidity(), e1.azimuthalAngle(), pho_eta, pho_phi) < HALO_RADIUS) {
-        e1 += p.momentum();
-        continue;
-      }
-      if (deltaR(e2.pseudorapidity(), e2.azimuthalAngle(), pho_eta, pho_phi) < HALO_RADIUS) {
-        e2 += p.momentum(); 
-      }
-    }
-
-
-    FourVector dilepton = electrons[0].momentum() + electrons[1].momentum();
-    const double mZ = mass(dilepton);
-    const double pTZ = pT(dilepton);
-
-    // Lepton pair should have an invariant mass between 66 and 116
-    if (mZ/GeV < 66.0 || mZ/GeV > 116.0) {
-      vetoEvent(e);
-    }
-
-    getLog() << Log::DEBUG << "Dilepton mass = " << mZ/GeV << " GeV"  << endl;
-    getLog() << Log::DEBUG << "Dilepton pT   = " << pTZ/GeV << " GeV" << endl;
-
-    // Fill the histograms
-    _hist_zpt->fill(pTZ/GeV, weight);
+    FourMomentum pZ = zfinder.particles()[0].momentum();    
+    getLog() << Log::DEBUG << "Dilepton mass = " << pZ.mass()/GeV << " GeV"  << endl;
+    getLog() << Log::DEBUG << "Dilepton pT   = " << pZ.pT()/GeV << " GeV" << endl;
+    _hist_zpt->fill(pZ.pT()/GeV, e.weight());
   }
 
 
