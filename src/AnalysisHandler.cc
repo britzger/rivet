d30aae19066f1ff444fda31e55f5ffb7c3355b8b
buckley <none@none>
2009-05-07 17:19:32 +0000

    Removing HepMC cross-section stuff until the API is decided on, otherwise Rivet 1.1.3 could go out of date very fast...

--- src/AnalysisHandler.cc
+++ src/AnalysisHandler.cc
@@ -55,14 +55,15 @@ namespace Rivet {
     foreach (Analysis* a, _analyses) {
       getLog() << Log::DEBUG << "About to run analysis " << a->name() << endl;
       a->analyze(event);
-      #ifdef HEPMC_HAS_CROSS_SECTION
-      const double xs = HepMC::GenRun::instance().cross_section();
-      if (xs > 0) {
-        getLog() << Log::DEBUG << "Setting cross-section for " << a->name() 
-                 << "=" << xs << " pb" << endl;
-        a->setCrossSection(xs);
-      }
-      #endif
+      /// @todo Introduce the HepMC cross-section stuff when the API is decided.
+      // #ifdef HEPMC_HAS_CROSS_SECTION
+      // const double xs = HepMC::GenRun::instance().cross_section();
+      // if (xs > 0) {
+      //   getLog() << Log::DEBUG << "Setting cross-section for " << a->name() 
+      //            << "=" << xs << " pb" << endl;
+      //   a->setCrossSection(xs);
+      // }
+      // #endif
       getLog() << Log::DEBUG << "Finished running analysis " << a->name() << endl;
     }
   }
