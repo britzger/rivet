2573858abdb909935358b5c31332f5ef2ece4074
fsiegert <none@none>
2009-08-04 15:56:34 +0000

    Bugfix in recently changed histogram division.

--- include/LWH/HistogramFactory.h
+++ include/LWH/HistogramFactory.h
@@ -643,7 +643,7 @@ public:
       x->setErrorMinus(binwidth/2.0);
         
       double yval(0), yerrup(0), yerrdown(0);
-      if ( h2.sum[i] == 0 || h2.sumw[i] == 0.0 ) {
+      if ( h2.binHeight(j) == 0 ) {
         /// @todo Bad way of handling div by zero!
         yval = 0.0;
         yerrup = 0.0;
