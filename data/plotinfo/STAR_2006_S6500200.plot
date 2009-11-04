39a75c74bd2645dae4ed784faf6b35b9d17b705e
hoeth <none@none>
2009-11-04 12:13:53 +0000

    New analysis STAR_2006_S6500200 (pion and proton pT spectra in pp collisions at 200 GeV). It is still unclear if they used a cut in rapidity or pseudorapidity, thus the analysis is declared "UNDER DEVELOPMENT" and "DO NOT USE".

---
+++ data/plotinfo/STAR_2006_S6500200.plot
@@ -0,0 +1,28 @@
+# BEGIN PLOT /STAR_2006_S6500200/d01-x01-y01
+Title=$\pi^+$ transverse momentum
+XLabel=$p_\perp$ [GeV]
+YLabel=$1/(2\pi) \, d^2N/(p_\perp dy dp_\perp)$ [GeV$^{-2}$]
+FullRange=1
+# END PLOT
+
+# BEGIN PLOT /STAR_2006_S6500200/d01-x02-y01
+Title=$\pi^-$ transverse momentum
+XLabel=$p_\perp$ [GeV]
+YLabel=$1/(2\pi) \, d^2N/(p_\perp dy dp_\perp)$ [GeV$^{-2}$]
+FullRange=1
+# END PLOT
+
+# BEGIN PLOT /STAR_2006_S6500200/d01-x03-y01
+Title=Proton transverse momentum
+XLabel=$p_\perp$ [GeV]
+YLabel=$1/(2\pi) \, d^2N/(p_\perp dy dp_\perp)$ [GeV$^{-2}$]
+FullRange=1
+# END PLOT
+
+# BEGIN PLOT /STAR_2006_S6500200/d01-x04-y01
+Title=Anti-proton transverse momentum
+XLabel=$p_\perp$ [GeV]
+YLabel=$1/(2\pi) \, d^2N/(p_\perp dy dp_\perp)$ [GeV$^{-2}$]
+FullRange=1
+# END PLOT
+
---
+++ src/Analyses/STAR_2006_S6500200.cc
@@ -0,0 +1,89 @@
+// -*- C++ -*-
+#include "Rivet/Analysis.hh"
+#include "Rivet/Tools/Logging.hh"
+#include "Rivet/Projections/IdentifiedFinalState.hh"
+#include "Rivet/RivetAIDA.hh"
+
+namespace Rivet {
+
+  /// @brief identified hadron spectra in pp at 200 GeV
+  class STAR_2006_S6500200 : public Analysis {
+  public:
+
+    /// Constructor
+    STAR_2006_S6500200()
+      : Analysis("STAR_2006_S6500200")
+    {
+      setBeams(PROTON, PROTON);
+    }
+
+    /// Book projections and histograms
+    void init() {
+      IdentifiedFinalState pionfs(-3.0, 3.0, 0.3*GeV);
+      IdentifiedFinalState protonfs(-3.0, 3.0, 0.4*GeV);
+      pionfs.acceptIdPair(PIPLUS);
+      protonfs.acceptIdPair(PROTON);
+      addProjection(pionfs, "PIONFS");
+      addProjection(protonfs, "PROTONFS");
+
+      _h_pT_piplus     = bookHistogram1D(1, 1, 1);
+      _h_pT_piminus    = bookHistogram1D(1, 2, 1);
+      _h_pT_proton     = bookHistogram1D(1, 3, 1);
+      _h_pT_antiproton = bookHistogram1D(1, 4, 1);
+    }
+
+
+    /// Do the analysis
+    void analyze(const Event& event) {
+      const double weight = event.weight();
+
+      const IdentifiedFinalState& pionfs = applyProjection<IdentifiedFinalState>(event, "PIONFS");
+      const IdentifiedFinalState& protonfs = applyProjection<IdentifiedFinalState>(event, "PROTONFS");
+      foreach (const Particle& p, pionfs.particles()) {
+        if (fabs(p.momentum().rapidity()) < 0.5) {
+          const double pT = p.momentum().pT() / GeV;
+          if (p.pdgId()>0) {
+            _h_pT_piplus->fill(pT, weight/pT);
+          }
+          else {
+            _h_pT_piminus->fill(pT, weight/pT);
+          }
+        }
+      }
+      foreach (const Particle& p, protonfs.particles()) {
+        if (fabs(p.momentum().rapidity()) < 0.5) {
+          const double pT = p.momentum().pT() / GeV;
+          if (p.pdgId()>0) {
+            _h_pT_proton->fill(pT, weight/pT);
+          }
+          else {
+            _h_pT_antiproton->fill(pT, weight/pT);
+          }
+        }
+      }
+    }
+
+
+    /// Finalize
+    void finalize() {
+      scale(_h_pT_piplus,     1./(2*M_PI*sumOfWeights()));
+      scale(_h_pT_piminus,    1./(2*M_PI*sumOfWeights()));
+      scale(_h_pT_proton,     1./(2*M_PI*sumOfWeights()));
+      scale(_h_pT_antiproton, 1./(2*M_PI*sumOfWeights()));
+    }
+
+  private:
+
+    AIDA::IHistogram1D * _h_pT_piplus;
+    AIDA::IHistogram1D * _h_pT_piminus;
+    AIDA::IHistogram1D * _h_pT_proton;
+    AIDA::IHistogram1D * _h_pT_antiproton;
+
+  };
+
+
+
+  // This global object acts as a hook for the plugin system
+  AnalysisBuilder<STAR_2006_S6500200> plugin_STAR_2006_S6500200;
+
+}
