6aa8dc58bea9e0495a4a6f2b12b16c0d2820b14e
buckley <none@none>
2009-12-09 14:39:31 +0000

    CDF 1988 fixed and VALIDATED

--- src/Analyses/CDF_1988_S1865951.cc
+++ src/Analyses/CDF_1988_S1865951.cc
@@ -1,10 +1,7 @@
 // -*- C++ -*-
 #include "Rivet/Analysis.hh"
 #include "Rivet/RivetAIDA.hh"
-#include "Rivet/Projections/FinalState.hh"
 #include "Rivet/Projections/ChargedFinalState.hh"
-#include "Rivet/Projections/TotalVisibleMomentum.hh"
-#include "Rivet/Projections/Beam.hh"
 #include "Rivet/Projections/TriggerCDFRun0Run1.hh"
 
 namespace Rivet {
@@ -18,22 +15,26 @@ namespace Rivet {
       : Analysis("CDF_1988_S1865951")
     {
       setBeams(PROTON, ANTIPROTON);
+      _sumWTrig = 0;
     }
 
 
     /// @name Analysis methods
     //@{
  
-    /// Book histograms
+    /// Book histograms and set up projections
     void init() {
+      // Set up projections
       addProjection(TriggerCDFRun0Run1(), "Trigger");
       const ChargedFinalState cfs(-1.0, 1.0, 0.4*GeV);
       addProjection(cfs, "CFS");
-      addProjection(TotalVisibleMomentum(cfs), "Mom");
-      addProjection(Beam(), "Beam");
 
-      _hist_pt1800 = bookHistogram1D(1, 1, 1);
-      _hist_pt630 = bookHistogram1D(2, 1, 1);
+      // Book histo
+      if (fuzzyEquals(sqrtS()/GeV, 1800, 1E-3)) {
+        _hist_pt = bookHistogram1D(1, 1, 1);
+      } else if (fuzzyEquals(sqrtS()/GeV, 630, 1E-3)) {
+        _hist_pt = bookHistogram1D(2, 1, 1);
+      }
     }
  
  
@@ -43,39 +44,36 @@ namespace Rivet {
       const bool trigger = applyProjection<TriggerCDFRun0Run1>(event, "Trigger").minBiasDecision();
       if (!trigger) vetoEvent;
       const double weight = event.weight();
+      _sumWTrig += weight;
 
-      const double sqrtS = applyProjection<Beam>(event, "Beam").sqrtS();
-      const FinalState& trackfs = applyProjection<ChargedFinalState>(event, "CFS");
-   
+      const FinalState& trackfs = applyProjection<ChargedFinalState>(event, "CFS");   
       foreach (Particle p, trackfs.particles()) {
         const double pt = p.momentum().pT();
         // Effective weight for d3sig/dp3 = weight / ( Delta eta * 2pi * pt ), with Delta(eta) = 2
-        const double eff_weight = weight/(2*TWOPI*pt);
-        if (fuzzyEquals(sqrtS, 630/GeV)) {
-          _hist_pt630->fill(pt, eff_weight);
-        } else if (fuzzyEquals(sqrtS, 1800/GeV)) {
-          _hist_pt1800->fill(pt, eff_weight);
-        }
-     
+        const double eff_weight = weight/(2*2*TWOPI*pt);
+        _hist_pt->fill(pt, eff_weight);
       }
     }
  
  
     /// Scale histos
     void finalize() {
-      /// @todo Total cross section hard-coded, needs a way to pass variable from generator
-      scale(_hist_pt630, 32.6/sumOfWeights());
-      scale(_hist_pt1800, 38.5/sumOfWeights());
+      scale(_hist_pt, crossSectionPerEvent()/millibarn);
     }
 
     //@}
 
+
   private:
+
+    /// @name Counters
+    //@{
+    double _sumWTrig;
+    //@}
  
     /// @name Histos
     //@{
-    AIDA::IHistogram1D* _hist_pt630;
-    AIDA::IHistogram1D* _hist_pt1800;
+    AIDA::IHistogram1D* _hist_pt;
     //@}
 
   };
