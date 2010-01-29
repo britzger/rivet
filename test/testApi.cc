4adee319d0c3032279a6c5add728e6c5610693b1
fsiegert <none@none>
2010-01-29 21:44:22 +0000

    Move test subdirectory to top level, such that libRivet is available for checks. Add test program which makes use of some API calls to the Rivet library.

---
+++ test/testApi.cc
@@ -0,0 +1,34 @@
+#include "Rivet/AnalysisHandler.hh"
+#include "HepMC/GenEvent.h"
+#include "HepMC/IO_GenEvent.h"
+
+using namespace std;
+
+int main() {
+  string fname = "out";
+  Rivet::AnalysisHandler rivet(fname, "", Rivet::AIDAML);
+
+  // specify the analyses to be used
+  rivet.addAnalysis("D0_2008_S7554427");
+  vector<string> moreanalyses(1, "D0_2007_S7075677");
+  rivet.addAnalyses(moreanalyses);
+
+  rivet.init(); // obsolete, but allowed for compatibility
+
+  std::istream* file = new std::fstream("testApi.hepmc", std::ios::in);
+  HepMC::IO_GenEvent hepmcio(*file);
+  HepMC::GenEvent evt;
+  double sum_of_weights = 0.0;
+  while (hepmcio.fill_next_event(&evt)) {
+    rivet.analyze(evt);
+    sum_of_weights+=evt.weights()[0];
+  }
+  delete file;
+
+  rivet.setCrossSection(1.0);
+  rivet.setSumOfWeights(sum_of_weights); // not necessary, but allowed
+  rivet.finalize();
+  rivet.commitData();
+
+  return 0;
+}
