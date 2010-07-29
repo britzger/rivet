34868c95e31f47ed59cb5a31b54a484ba132aefa
buckley <none@none>
2010-07-29 23:37:47 +0000

    Improving test script

--- test/testCmdLine.sh
+++ test/testCmdLine.sh
@@ -1,6 +1,11 @@
 #! /usr/bin/env bash
 
+if [[ -z "$PYTHON_BUILD_DIR" ]]; then
+    echo "\$PYTHON_BUILD_DIR must be defined" 1>&2
+    exit 1
+fi
 export PYTHONPATH=$(ls -d $PYTHON_BUILD_DIR/lib.*):$PYTHONPATH
+
 
 function _clean() {
     rm -f fifo.hepmc
@@ -21,18 +26,19 @@ function _check() {
     fi
 }
 
+
 _setup
 
-rivet -a D0_2008_S7554427 testApi.hepmc file2.hepmc > log
+rivet -a D0_2008_S7554427 testApi.hepmc file2.hepmc > log || exit $?
 grep -q "20 events" log
 _check
 
-cat testApi.hepmc | rivet -a D0_2008_S7554427 > log
+cat testApi.hepmc | rivet -a D0_2008_S7554427 > log || exit $?
 grep -q "10 events" log
 _check
 
 cat testApi.hepmc > fifo.hepmc &
-rivet -a D0_2008_S7554427 fifo.hepmc > log
+rivet -a D0_2008_S7554427 fifo.hepmc > log || exit $?
 grep -q "10 events" log
 _check
 _clean
