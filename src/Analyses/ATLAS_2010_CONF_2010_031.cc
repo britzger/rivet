1293124e1c683fe302605123b3817d593e762c8d
hoeth <none@none>
2010-12-28 02:26:00 +0000

    Merged two real data plots into the ATLAS_2010_CONF_2010_031 analysis and changed the Nch cut down to 1. That's just for the plots in the MCnet review, and soon we will have the real ATLAS analysis with the real data here. Soon.

--- src/Analyses/ATLAS_2010_CONF_2010_031.cc
+++ src/Analyses/ATLAS_2010_CONF_2010_031.cc
@@ -23,16 +23,16 @@ namespace Rivet {
       ChargedFinalState cfs(-2.5, 2.5, 0.5*GeV);
       addProjection(cfs, "CFS");
 
-      if (sqrtS()/GeV >= 890 && sqrtS()/GeV <= 910) {
-        _h_dNch_deta = bookHistogram1D(2, 1, 1);
-        _h_dNevt_dNch = bookHistogram1D(2, 2, 1);
-        _h_dNch_dpT = bookHistogram1D(2, 3, 1);
-        _p_meanpT_Nch = bookProfile1D(2, 4, 1);
-      }
+//      if (sqrtS()/GeV >= 890 && sqrtS()/GeV <= 910) {
+//        _h_dNch_deta = bookHistogram1D(2, 1, 1);
+//        _h_dNevt_dNch = bookHistogram1D(2, 2, 1);
+//        _h_dNch_dpT = bookHistogram1D(2, 3, 1);
+//        _p_meanpT_Nch = bookProfile1D(2, 4, 1);
+//      }
       if (sqrtS()/GeV >= 6990 && sqrtS()/GeV <= 7010) {
-        _h_dNch_deta = bookHistogram1D(3, 1, 1);
+//        _h_dNch_deta = bookHistogram1D(3, 1, 1);
         _h_dNevt_dNch = bookHistogram1D(3, 2, 1);
-        _h_dNch_dpT = bookHistogram1D(3, 3, 1);
+//        _h_dNch_dpT = bookHistogram1D(3, 3, 1);
         _p_meanpT_Nch = bookProfile1D(3, 4, 1);
       }
     }
@@ -42,7 +42,7 @@ namespace Rivet {
       const double weight = event.weight();
 
       const ChargedFinalState& charged = applyProjection<ChargedFinalState>(event, "CFS");
-      if (charged.size() < 6) {
+      if (charged.size() < 1) {
         vetoEvent;
       }
       _Nevt_after_cuts += weight;
@@ -50,17 +50,17 @@ namespace Rivet {
       _h_dNevt_dNch->fill(charged.size(), weight);
       foreach (const Particle& p, charged.particles()) {
         double pT = p.momentum().pT()/GeV;
-        _h_dNch_deta->fill(p.momentum().eta(), weight);
-        _h_dNch_dpT->fill(pT, weight/pT);
+//        _h_dNch_deta->fill(p.momentum().eta(), weight);
+//        _h_dNch_dpT->fill(pT, weight/pT);
         _p_meanpT_Nch->fill(charged.size(), pT, weight);
       }
     }
 
 
     void finalize() {
-      double deta = 5.0;
-      scale(_h_dNch_deta, 1.0/_Nevt_after_cuts);
-      scale(_h_dNch_dpT, 1.0/_Nevt_after_cuts/TWOPI/deta);
+//      double deta = 5.0;
+//      scale(_h_dNch_deta, 1.0/_Nevt_after_cuts);
+//      scale(_h_dNch_dpT, 1.0/_Nevt_after_cuts/TWOPI/deta);
       scale(_h_dNevt_dNch, 1.0/_Nevt_after_cuts);
     }
 
