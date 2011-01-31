00ad180a3d04e9ce585d54a45d50a23672a74a07
buckley <none@none>
2011-01-31 14:43:31 +0000

    Don't sum photons around muons in the D0 2010 Z pT analysis.

--- src/Analyses/D0_2010_S8671338.cc
+++ src/Analyses/D0_2010_S8671338.cc
@@ -8,66 +8,62 @@
 
 namespace Rivet {
 
+
   /// @brief Measurement of Z(->muon muon) pT differential cross-section
   /// @author Flavia Dias
-  class D0_2010_S8671338 : public Analysis { 
-
+  class D0_2010_S8671338 : public Analysis {
   public:
 
     /// @name Construction
     //@{
+
     /// Constructor
     D0_2010_S8671338() : Analysis("D0_2010_S8671338")
     {
       setBeams(PROTON, ANTIPROTON);
+      setNeedsCrossSection(true);
     }
-    
+
     //@}
+
 
     ///@name Analysis methods
     //@{
 
-    ///Add Projections, Book histograms
+    /// Add projections and book histograms
     void init() {
-      ZFinder zfinder(-1.7, 1.7, 15.0*GeV, MUON, 65.0*GeV, 115.0*GeV, 0.2);
+      ZFinder zfinder(-1.7, 1.7, 15.0*GeV, MUON, 65.0*GeV, 115.0*GeV, 0.0);
       addProjection(zfinder, "ZFinder");
-  
+
       _h_Z_pT_cross_section = bookHistogram1D(1, 1, 1);
     }
+
 
     // Do the analysis
     void analyze(const Event& e) {
       const double weight = e.weight();
-      
       const ZFinder& zfinder = applyProjection<ZFinder>(e, "ZFinder");
-      if (zfinder.particles().size()==1) {
-     
-        const FourMomentum Zmom = zfinder.particles()[0].momentum();
-	// In Z pT
-        _h_Z_pT_cross_section->fill(Zmom.pT(), weight);      
-
+      if (zfinder.particles().size() == 1) {
+        _h_Z_pT_cross_section->fill(zfinder.particles()[0].momentum().pT()/GeV, weight);
       }
-
     }
 
 
- /// Finalize
+    /// Finalize
     void finalize() {
       normalize(_h_Z_pT_cross_section, 1.0);
     }
+
     //@}
 
 
   private:
 
-    /// @name Histograms
-    //@{
+    /// @name Histogram
     AIDA::IHistogram1D * _h_Z_pT_cross_section;
-    //@}
-
-
 
   };
+
 
   // This global object acts as a hook for the plugin system
   AnalysisBuilder<D0_2010_S8671338> plugin_D0_2010_S8671338;
