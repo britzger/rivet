ab2219705541951dbc1f468ea91caa8c3f6e0e14
fsiegert <none@none>
2011-08-21 11:26:16 +0000

    Add a couple more bookkeeping numbers to MC_XS.

--- src/Analyses/MC_XS.cc
+++ src/Analyses/MC_XS.cc
@@ -32,12 +32,18 @@ namespace Rivet {
     /// Book histograms and initialise projections before the run
     void init() {
       _h_XS = bookDataPointSet("XS", 1, 0.0, 1.0);
+      _h_N = bookHistogram1D("N", 1, 0.0, 1.0);
+      _h_pmXS = bookHistogram1D("pmXS", 2, -1.0, 1.0);
+      _h_pmN = bookHistogram1D("pmN", 2, -1.0, 1.0);
       _mc_xs=_mc_error=0.;
     }
 
 
     /// Perform the per-event analysis
     void analyze(const Event& event) {
+      _h_N->fill(0.5,1.);
+      _h_pmXS->fill(0.5*(event.weight()>0?1.:-1),abs(event.weight()));
+      _h_pmN->fill(0.5*(event.weight()>0?1.:-1),1.);
 #ifdef HEPMC_HAS_CROSS_SECTION
       _mc_xs    = event.genEvent().cross_section()->cross_section();
       _mc_error = event.genEvent().cross_section()->cross_section_error();
@@ -47,6 +53,7 @@ namespace Rivet {
 
     /// Normalise histograms etc., after the run
     void finalize() {
+      scale(_h_pmXS, crossSection()/sumOfWeights());
 #ifndef HEPMC_HAS_CROSS_SECTION
       _mc_xs=crossSection();
       _mc_error=0.0;
@@ -65,6 +72,9 @@ namespace Rivet {
     /// @name Histograms
     //@{
     AIDA::IDataPointSet * _h_XS;
+    AIDA::IHistogram1D *  _h_N;
+    AIDA::IHistogram1D *  _h_pmXS;
+    AIDA::IHistogram1D *  _h_pmN;
     double _mc_xs, _mc_error;
     //@}
 
