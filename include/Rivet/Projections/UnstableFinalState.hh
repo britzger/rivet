76cd63542a386fb3cb1e43616ffd58bd7b63d0c5
buckley <none@none>
2011-08-23 13:46:47 +0000

    Add some more Doxygen description to the UnstableFinalState

--- include/Rivet/Projections/UnstableFinalState.hh
+++ include/Rivet/Projections/UnstableFinalState.hh
@@ -10,11 +10,23 @@ namespace Rivet {
 
 
   /// @brief Project out all physical-but-decayed particles in an event.
+  ///
+  /// The particles returned by the UFS are unique unstable particles, such as
+  /// hadrons which are decayed by the generator. If, for example, you set Ks
+  /// and Lambda particles stable in the generator, they will not be returned by
+  /// the UFS. Also, you should be aware that all unstable particles in a decay
+  /// chain are returned: if you are looking for something like the number of B
+  /// hadrons in an event and there is a decay chain from e.g. B** -> B, you
+  /// will count both B mesons unless you are careful to check for
+  /// ancestor/descendent relations between the particles. Duplicate particles
+  /// in the event record, i.e. those which differ only in bookkeeping details
+  /// or photon emissions, are stripped from the returned particles collection.
   class UnstableFinalState : public FinalState {
   public:
 
     /// @name Standard constructors and destructors.
     //@{
+
     /// The default constructor. May specify the minimum and maximum
     /// pseudorapidity \f$ \eta \f$ and the min \f$ p_T \f$ (in GeV).
     UnstableFinalState(double mineta = -MAXRAPIDITY,
@@ -33,7 +45,12 @@ namespace Rivet {
     virtual const Projection* clone() const {
       return new UnstableFinalState(*this);
     }
+
     //@}
+
+
+    /// @name Accessors
+    //@{
 
     /// Access the projected final-state particles.
     virtual const ParticleVector& particles() const { return _theParticles; }
@@ -43,6 +60,8 @@ namespace Rivet {
 
     /// @deprecated Is this final state empty?
     virtual bool isEmpty() const { return _theParticles.empty(); }
+
+    //@}
 
 
   protected:
