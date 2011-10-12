98559c34170606f84d599c06416b7e332c6026af
buckley <none@none>
2011-10-12 15:17:15 +0000

    Adding a separate math function test program

--- test/testMatVec.cc
+++ test/testMatVec.cc
@@ -7,9 +7,9 @@
 #include "Rivet/Math/Matrices.hh"
 
 using namespace std;
+using namespace Rivet;
 
 int main() {
-  using namespace Rivet;
 
   FourVector a(1,0,0,0);
   cout << a << ": interval = " << a.invariant() << endl;
@@ -139,28 +139,6 @@ int main() {
   cout << (ltX * ltY).boost() << endl;
   cout << (ltY * ltX).boost() << endl;
   cout << (ltX * ltX.inverse()).boost() << endl;
-
-  ////////////
-
-  // Angle tests
-  assert(fuzzyEquals(angle(FourMomentum(1,0,0,1), FourMomentum(1,0,0,1))/M_PI, 0.0));
-  assert(fuzzyEquals(angle(FourMomentum(1,0,0,1), FourMomentum(1,0,1,0))/M_PI, 0.5));
-  assert(fuzzyEquals(angle(FourMomentum(1,0,0,1), FourMomentum(1,0,0,-1))/M_PI, 1.0));
-  // Test with vectors of different magnitude
-  assert(fuzzyEquals(angle(FourMomentum(3,0,0,3), FourMomentum(1,0,0,1))/M_PI, 0.0));
-  assert(fuzzyEquals(angle(FourMomentum(5,0,0,5), FourMomentum(1,0,1,0))/M_PI, 0.5));
-  assert(fuzzyEquals(angle(FourMomentum(7,0,0,7), FourMomentum(1,0,0,-1))/M_PI, 1.0));
-
-  ////////////
-
-  linspace(0, 10, 50);
-  logspace(0.000001, 1.0, 50);
-
-  assert(isZero(1e-15));
-  assert(fuzzyEquals(1e-15, 0.0));
-  assert(fuzzyEquals(2.0, 2.0));
-  assert(!fuzzyEquals(1, 2.0));
-  assert(fuzzyGtrEquals(1.0, 1.0 - 1e-15));
 
   return EXIT_SUCCESS;
 }
