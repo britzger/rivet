53da6d6e3cb7cdf3922395d6a504093cb87f0e2b
hoeth <none@none>
2011-12-06 13:41:07 +0000

    Fix whitespaces in ATLAS_2011_S8983313 and ATLAS_2011_S9225137

--- src/Analyses/ATLAS_2011_S9225137.cc
+++ src/Analyses/ATLAS_2011_S9225137.cc
@@ -66,7 +66,7 @@ namespace Rivet {
 
       /// Jet finder
       addProjection(FastJets(vfs, FastJets::ANTIKT, 0.4),
-		    "AntiKtJets04");
+                    "AntiKtJets04");
 
       // all tracks (to do deltaR with leptons)
       addProjection(ChargedFinalState(-3.0,3.0),"cfs");
@@ -94,16 +94,16 @@ namespace Rivet {
 
       // apply electron veto region
       ParticleVector veto_e
-	= applyProjection<IdentifiedFinalState>(event, "veto_elecs").particles();
+        = applyProjection<IdentifiedFinalState>(event, "veto_elecs").particles();
       if ( ! veto_e.empty() ) {
-	MSG_DEBUG("electrons in veto region");
-	vetoEvent;
+        MSG_DEBUG("electrons in veto region");
+        vetoEvent;
       }
 
       // get the jet candidates
       Jets cand_jets;
       foreach (const Jet& jet,
-	       applyProjection<FastJets>(event, "AntiKtJets04").jetsByPt(20.0*GeV) ) {
+               applyProjection<FastJets>(event, "AntiKtJets04").jetsByPt(20.0*GeV) ) {
         if ( fabs( jet.momentum().eta() ) < 4.9 ) {
           cand_jets.push_back(jet);
         }
@@ -111,101 +111,101 @@ namespace Rivet {
 
       // candidate muons
       ParticleVector cand_mu;
-      ParticleVector chg_tracks = 
-	applyProjection<ChargedFinalState>(event, "cfs").particles();
+      ParticleVector chg_tracks =
+        applyProjection<ChargedFinalState>(event, "cfs").particles();
       foreach ( const Particle & mu,
-		applyProjection<IdentifiedFinalState>(event, "muons").particlesByPt() ) {
-	double pTinCone = -mu.momentum().pT();
-	foreach ( const Particle & track, chg_tracks ) {
-	  if ( deltaR(mu.momentum(),track.momentum()) <= 0.2 )
-	    pTinCone += track.momentum().pT();
-	}
-	if ( pTinCone < 1.8*GeV )
-	  cand_mu.push_back(mu);
+                applyProjection<IdentifiedFinalState>(event, "muons").particlesByPt() ) {
+        double pTinCone = -mu.momentum().pT();
+        foreach ( const Particle & track, chg_tracks ) {
+          if ( deltaR(mu.momentum(),track.momentum()) <= 0.2 )
+            pTinCone += track.momentum().pT();
+        }
+        if ( pTinCone < 1.8*GeV )
+          cand_mu.push_back(mu);
       }
 
       // candidate electrons
 
-      ParticleVector cand_e  = 
-	applyProjection<IdentifiedFinalState>(event, "elecs").particlesByPt();
+      ParticleVector cand_e  =
+        applyProjection<IdentifiedFinalState>(event, "elecs").particlesByPt();
 
-      // resolve jet/lepton ambiguity 
+      // resolve jet/lepton ambiguity
       Jets cand_jets_2;
       foreach ( const Jet& jet, cand_jets ) {
-	// candidates above eta=2.8 are jets
-	if ( fabs( jet.momentum().eta() ) >= 2.8 )
-	  cand_jets_2.push_back( jet );
-	// otherwise more the R=0.2 from an electrons
-	else {
-	  bool away_from_e = true;
-	  foreach ( const Particle & e, cand_e ) {
-	    if ( deltaR(e.momentum(),jet.momentum()) <= 0.2 ) {
-	      away_from_e = false;
-	      break;
-	    }
-	  }
-	  if ( away_from_e )
-	    cand_jets_2.push_back( jet );
-	}
+        // candidates above eta=2.8 are jets
+        if ( fabs( jet.momentum().eta() ) >= 2.8 )
+          cand_jets_2.push_back( jet );
+        // otherwise more the R=0.2 from an electrons
+        else {
+          bool away_from_e = true;
+          foreach ( const Particle & e, cand_e ) {
+            if ( deltaR(e.momentum(),jet.momentum()) <= 0.2 ) {
+              away_from_e = false;
+              break;
+            }
+          }
+          if ( away_from_e )
+            cand_jets_2.push_back( jet );
+        }
       }
 
       // only keep electrons more than R=0.4 from jets
       ParticleVector recon_e;
       foreach ( const Particle & e, cand_e ) {
-	bool away = true;
-	foreach ( const Jet& jet, cand_jets_2 ) {
-	  if ( deltaR(e.momentum(),jet.momentum()) < 0.4 ) {
-	    away = false;
-	    break;
-	  }
-	}
-	if ( away )
-	  recon_e.push_back( e );
+        bool away = true;
+        foreach ( const Jet& jet, cand_jets_2 ) {
+          if ( deltaR(e.momentum(),jet.momentum()) < 0.4 ) {
+            away = false;
+            break;
+          }
+        }
+        if ( away )
+          recon_e.push_back( e );
       }
 
       // only keep muons more than R=0.4 from jets
       ParticleVector recon_mu;
       foreach ( const Particle & mu, cand_mu ) {
-	bool away = true;
-	foreach ( const Jet& jet, cand_jets_2 ) {
-	  if ( deltaR(mu.momentum(),jet.momentum()) < 0.4 ) {
-	    away = false;
-	    break;
-	  }
-	}
-	if ( away )
-	  recon_mu.push_back( mu );
+        bool away = true;
+        foreach ( const Jet& jet, cand_jets_2 ) {
+          if ( deltaR(mu.momentum(),jet.momentum()) < 0.4 ) {
+            away = false;
+            break;
+          }
+        }
+        if ( away )
+          recon_mu.push_back( mu );
       }
 
       // pTmiss
-      ParticleVector vfs_particles = 
-	applyProjection<VisibleFinalState>(event, "vfs").particles();
+      ParticleVector vfs_particles =
+        applyProjection<VisibleFinalState>(event, "vfs").particles();
       FourMomentum pTmiss;
       foreach ( const Particle & p, vfs_particles ) {
-	pTmiss -= p.momentum();
+        pTmiss -= p.momentum();
       }
       double eTmiss = pTmiss.pT();
 
       // final jet filter
       Jets recon_jets;
       foreach ( const Jet& jet, cand_jets_2 ) {
-	if ( fabs( jet.momentum().eta() ) <= 2.8 )
-	  recon_jets.push_back( jet );
+        if ( fabs( jet.momentum().eta() ) <= 2.8 )
+          recon_jets.push_back( jet );
       }
 
       // now only use recon_jets, recon_mu, recon_e
 
       // reject events with electrons and muons
       if ( ! ( recon_mu.empty() && recon_e.empty() ) ) {
-	MSG_DEBUG("Charged leptons left after selection");
-	vetoEvent;
+        MSG_DEBUG("Charged leptons left after selection");
+        vetoEvent;
       }
 
       // calculate H_T
       double HT=0;
       foreach ( const Jet& jet, recon_jets ) {
-	if ( jet.momentum().pT() > 40 * GeV )
-	  HT += jet.momentum().pT() ;
+        if ( jet.momentum().pT() > 40 * GeV )
+          HT += jet.momentum().pT() ;
       }
 
       // number of jets and deltaR
@@ -214,19 +214,19 @@ namespace Rivet {
       bool pass80DeltaR=true;
       unsigned int njet80=0;
       for (unsigned int ix=0;ix<recon_jets.size();++ix) {
-	if(recon_jets[ix].momentum().pT()>80.*GeV) ++njet80;
-	if(recon_jets[ix].momentum().pT()>55.*GeV) ++njet55;
+        if(recon_jets[ix].momentum().pT()>80.*GeV) ++njet80;
+        if(recon_jets[ix].momentum().pT()>55.*GeV) ++njet55;
 
-	for (unsigned int iy=ix+1;iy<recon_jets.size();++iy) {
-	  if(recon_jets[ix].momentum().pT()>55.*GeV &&
-	     recon_jets[iy].momentum().pT()>55.*GeV &&
-	     deltaR(recon_jets[ix],recon_jets[ix]) <0.6 ) 
-	    pass55DeltaR = false;
-	  if(recon_jets[ix].momentum().pT()>80.*GeV &&
-	     recon_jets[iy].momentum().pT()>80.*GeV &&
-	     deltaR(recon_jets[ix],recon_jets[ix]) <0.6 ) 
-	    pass80DeltaR = false;
-	}
+        for (unsigned int iy=ix+1;iy<recon_jets.size();++iy) {
+          if(recon_jets[ix].momentum().pT()>55.*GeV &&
+             recon_jets[iy].momentum().pT()>55.*GeV &&
+             deltaR(recon_jets[ix],recon_jets[ix]) <0.6 )
+            pass55DeltaR = false;
+          if(recon_jets[ix].momentum().pT()>80.*GeV &&
+             recon_jets[iy].momentum().pT()>80.*GeV &&
+             deltaR(recon_jets[ix],recon_jets[ix]) <0.6 )
+            pass80DeltaR = false;
+        }
       }
 
       // require at least four jets with et > 55
@@ -238,39 +238,39 @@ namespace Rivet {
       if(njet80==5) _etmissHTB->fill(etht,weight);
 
       if(etht>1.5&&etht<2. ) {
-	if(njet55>3) _njet55A->fill(njet55,weight);
-	if(njet80>3) _njet80A->fill(njet80,weight);
+        if(njet55>3) _njet55A->fill(njet55,weight);
+        if(njet80>3) _njet80A->fill(njet80,weight);
       }
       if(etht>2. &&etht<3. ) {
-	if(njet55>3) _njet55B->fill(njet55,weight);
-	if(njet80>3) _njet80B->fill(njet80,weight);
+        if(njet55>3) _njet55B->fill(njet55,weight);
+        if(njet80>3) _njet80B->fill(njet80,weight);
       }
 
       // apply E_T/sqrt(H_T) cut
       if(etht<=3.5*GeV) {
-	MSG_DEBUG("Fails ET/sqrt(HT) cut ");
-	vetoEvent;
+        MSG_DEBUG("Fails ET/sqrt(HT) cut ");
+        vetoEvent;
       }
 
       // check passes at least one delta5/ njet number cut
       if(!(pass55DeltaR && njet55 >= 7) &&
-	 !(pass80DeltaR && njet80 >= 6) ) {
-	MSG_DEBUG("Fails DeltaR cut or jet number cuts");
-	vetoEvent;
+         !(pass80DeltaR && njet80 >= 6) ) {
+        MSG_DEBUG("Fails DeltaR cut or jet number cuts");
+        vetoEvent;
       }
 
       // 7j55
       if(njet55>=7&&pass55DeltaR)
-	_count_7j55->fill( 0.5, weight) ;
+        _count_7j55->fill( 0.5, weight) ;
       // 8j55
       if(njet55>=8&&pass55DeltaR)
-	_count_8j55->fill( 0.5, weight) ;
+        _count_8j55->fill( 0.5, weight) ;
       // 6j80
       if(njet80>=6&&pass80DeltaR)
-	_count_6j80->fill( 0.5, weight) ;
+        _count_6j80->fill( 0.5, weight) ;
       // 7j80
       if(njet80>=7&&pass80DeltaR)
-	_count_7j80->fill( 0.5, weight) ;
+        _count_7j80->fill( 0.5, weight) ;
 
     }
 
