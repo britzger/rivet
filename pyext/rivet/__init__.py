8028ab90f446db88fcfe0e91d43ec59231e7f84a
Dave Mallows <dave.mallows@gmail.com>
2013-05-30 20:10:39 +0100

    Replaced SWIG with Cython.

--- pyext/rivet/__init__.py
+++ pyext/rivet/__init__.py
@@ -1,20 +1,3 @@
-"Python interface to the Rivet MC validation system"
-
-# ## Try to bootstrap the Python path for rivet loading
-# import os, sys
-# PROGPATH = sys.argv[0]
-# PROGNAME = os.path.basename(PROGPATH)
-# import commands
-# try:
-#     modname = sys.modules[__name__].__file__
-#     binpath = os.path.dirname(modname)
-#     rivetconfigpath = os.path.join(binpath, "rivet-config")
-#     rivetpypath = commands.getoutput(rivetconfigpath + " --pythonpath")
-#     sys.path.append(rivetpypath)
-# except:
-#     pass
-
-## Change dlopen status to GLOBAL for Rivet lib
 import sys
 try:
     import ctypes
@@ -26,19 +9,6 @@ except:
     del dl
 del sys
 
-## Import SWIG-generated wrapper core
-from rivet.rivetwrap import *
-
-## Import plot info helper
+from rivet.core import *
 from rivet.plotinfo import *
-
-## Import submodules into the visible namespace
 import spiresbib, util
-
-## Try to use Psyco optimiser if available
-# TODO: or PyPy, Cython, Weave?
-try:
-    import psyco
-    psyco.full()
-except Exception, e:
-    pass
