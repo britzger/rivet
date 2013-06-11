8d413a891ecd99019624385908f28b3680594138
Andy Buckley <andy@insectnation.org>
2013-06-11 15:50:29 +0200

    Adding PrimaryHadrons projection.

---
+++ include/Rivet/Projections/PrimaryHadrons.hh
@@ -0,0 +1,48 @@
+// -*- C++ -*-
+#ifndef RIVET_PrimaryHadrons_HH
+#define RIVET_PrimaryHadrons_HH
+
+#include "Rivet/Projections/FinalState.hh"
+#include "Rivet/Projections/UnstableFinalState.hh"
+#include "Rivet/Particle.hh"
+#include "Rivet/Event.hh"
+
+namespace Rivet {
+
+
+  /// @brief Project out the first hadrons (and taus) from hadronisation.
+  class PrimaryHadrons : public FinalState {
+  public:
+
+    /// @name Constructors and destructors.
+    //@{
+
+    /// Constructor with specification of the minimum and maximum pseudorapidity
+    /// \f$ \eta \f$ and the min \f$ p_T \f$ (in GeV).
+    PrimaryHadrons(double mineta = -MAXRAPIDITY,
+                   double maxeta =  MAXRAPIDITY,
+                   double minpt  =  0.0*GeV) {
+      setName("PrimaryHadrons");
+      addProjection(UnstableFinalState(mineta, maxeta, minpt), "UFS");
+    }
+
+
+    /// Clone on the heap.
+    virtual const Projection* clone() const {
+      return new PrimaryHadrons(*this);
+    }
+
+    //@}
+
+  protected:
+
+    /// Apply the projection to the event.
+    virtual void project(const Event& e);
+
+  };
+
+
+}
+
+
+#endif
