36a9f9025128e41cd7673484b0c2f7d6958d54fb
Andy Buckley <andy@insectnation.org>
2013-06-11 15:36:21 +0200

    Adding particles_in/out functions on GenParticle to RivetHepMC.

--- include/Rivet/RivetHepMC.hh
+++ include/Rivet/RivetHepMC.hh
@@ -2,11 +2,12 @@
 #ifndef RIVET_RivetHepMC_HH
 #define RIVET_RivetHepMC_HH
 
+#include "Rivet/RivetSTL.hh"
+#include "Rivet/Exceptions.hh"
 #include "HepMC/GenEvent.h"
 #include "HepMC/GenParticle.h"
 #include "HepMC/GenVertex.h"
 #include "HepMC/IO_GenEvent.h"
-#include "Rivet/RivetSTL.hh"
 
 namespace Rivet {
 
@@ -14,6 +15,9 @@ namespace Rivet {
   using HepMC::GenEvent;
   using HepMC::GenParticle;
   using HepMC::GenVertex;
+
+
+  /// @todo Add functions from mcutils
 
 
   inline std::vector<GenParticle*> particles(const GenEvent& ge) {
@@ -36,6 +40,7 @@ namespace Rivet {
   //  }
 
 
+
   inline std::vector<GenVertex*> vertices(const GenEvent& ge) {
     std::vector<GenVertex*> rtn;
     for (GenEvent::vertex_const_iterator vi = ge.vertices_begin(); vi != ge.vertices_end(); ++vi) {
@@ -56,12 +61,17 @@ namespace Rivet {
   //  }
 
 
-  inline const std::pair<GenVertex::particles_in_const_iterator, GenVertex::particles_in_const_iterator> particles_in(const GenVertex* gv) {
+  ////////////////////////////////////
+
+
+  inline std::pair<GenVertex::particles_in_const_iterator, GenVertex::particles_in_const_iterator>
+  particles_in(const GenVertex* gv) {
     return make_pair(gv->particles_in_const_begin(), gv->particles_in_const_end());
   }
 
 
-  inline const std::pair<GenVertex::particles_out_const_iterator, GenVertex::particles_out_const_iterator> particles_out(const GenVertex* gv) {
+  inline std::pair<GenVertex::particles_out_const_iterator, GenVertex::particles_out_const_iterator>
+  particles_out(const GenVertex* gv) {
     return make_pair(gv->particles_out_const_begin(), gv->particles_out_const_end());
   }
 
@@ -76,6 +86,25 @@ namespace Rivet {
   //  inline std::pair<GenVertex::particle_iterator, GenVertex::particle_iterator> particles(GenVertex* gv, HepMC::IteratorRange range=HepMC::relatives) {
   //    return make_pair(gv->particles_begin(range), gv->particles_end(range));
   //  }
+
+
+  //////////////////////////
+
+
+  /// Get the direct parents or all-ancestors of GenParticle @a gp
+  inline std::vector<GenParticle*> particles_in(GenParticle* gp, HepMC::IteratorRange range=HepMC::ancestors) {
+    if (range != HepMC::parents && range != HepMC::ancestors)
+      throw UserError("Requested particles_in(GenParticle*) with a non-'in' iterator range");
+    return (gp->production_vertex()) ? particles(gp->production_vertex(), range) : std::vector<GenParticle*>();
+  }
+
+
+  /// Get the direct children or all-descendents of GenParticle @a gp
+  inline std::vector<GenParticle*> particles_out(GenParticle* gp, HepMC::IteratorRange range=HepMC::descendants) {
+    if (range != HepMC::children && range != HepMC::descendants)
+      throw UserError("Requested particles_out(GenParticle*) with a non-'out' iterator range");
+    return (gp->end_vertex()) ? particles(gp->end_vertex(), range) : std::vector<GenParticle*>();
+  }
 
 
 }
