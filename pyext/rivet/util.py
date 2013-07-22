a985de607a16c9be03d5455417f89ad84618dd32
Andy Buckley <andy@insectnation.org>
2013-07-22 20:32:02 +0200

    Tweak process name changing and Python path bootstrapping

--- pyext/rivet/util.py
+++ pyext/rivet/util.py
@@ -1,5 +1,6 @@
 "Python utility functions for use by Rivet scripts (and anyone else who wants to)"
 
+
 def check_python_version(req_version=(2,4,0)):
     "Enforce the Rivet scripts' minimal Python version requirement"
     import sys
@@ -7,14 +8,23 @@ def check_python_version(req_version=(2,4,0)):
         sys.stderr.write( "Python version >= %s is required... exiting\n" % ".".join(req_version) )
         sys.exit(1)
 
+
 def set_process_name(name):
     "Try to rename the process on Linux so it doesn't appear as 'python <scriptpath>'"
     try:
-        import ctypes
-        libc = ctypes.cdll.LoadLibrary("libc.so.6")
-        libc.prctl(15, name, 0, 0, 0)
-    except Exception:
-        pass
+        ## Try to use this: https://code.google.com/p/py-setproctitle/
+        import setproctitle
+        setproctitle.setproctitle(name)
+    except:
+        try:
+            ## Fall back to a by-hand thing that doesn't work for me...
+            import ctypes
+            libc = ctypes.cdll.LoadLibrary("libc.so.6")
+            libc.prctl(15, name, 0, 0, 0)
+        except:
+            ## And then give up ;-)
+            pass
+
 
 def import_ET():
     "Try to import the ElementTree XML parser, which has many historical import signatures"
