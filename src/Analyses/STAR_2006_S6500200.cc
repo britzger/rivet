471b6a6a2773c78c0f8c2fcc7e0e541a974b206f
Andy Buckley <andy@insectnation.org>
2013-09-22 22:24:30 +0200

    Last problem analysis converted!!!!!!

--- src/Analyses/STAR_2006_S6500200.cc
+++ src/Analyses/STAR_2006_S6500200.cc
@@ -31,15 +31,17 @@ namespace Rivet {
       addProjection(pionfs, "PionFS");
       addProjection(protonfs, "ProtonFS");
 
-      _h_pT_piplus     = bookHisto1D(1, 1, 1);
-      _h_pT_piminus    = bookHisto1D(1, 2, 1);
+      _h_pT_piplus     = bookHisto1D(1, 1, 1); // full range pion binning
+      _h_pT_piminus    = bookHisto1D(1, 2, 1); // full range pion binning
+      _tmp_pT_piplus   = bookHisto1D("TMP/pT_piplus", refData(2, 3, 1)); // pi histo compatible with more restricted proton binning
+      _tmp_pT_piminus  = bookHisto1D("TMP/pT_piminus", refData(2, 4, 1)); // pi histo compatible with more restricted proton binning
       _h_pT_proton     = bookHisto1D(1, 3, 1);
       _h_pT_antiproton = bookHisto1D(1, 4, 1);
 
-      _h_piminus_piplus = bookScatter2D(2, 1, 1);
-      _h_antipr_pr      = bookScatter2D(2, 2, 1);
-      _h_pr_piplus      = bookScatter2D(2, 3, 1);
-      _h_antipr_piminus = bookScatter2D(2, 4, 1);
+      _s_piminus_piplus = bookScatter2D(2, 1, 1);
+      _s_antipr_pr      = bookScatter2D(2, 2, 1);
+      _s_pr_piplus      = bookScatter2D(2, 3, 1);
+      _s_antipr_piminus = bookScatter2D(2, 4, 1);
     }
 
 
@@ -47,7 +49,7 @@ namespace Rivet {
     void analyze(const Event& event) {
       const ChargedFinalState& bbc1 = applyProjection<ChargedFinalState>(event, "BBC1");
       const ChargedFinalState& bbc2 = applyProjection<ChargedFinalState>(event, "BBC2");
-      if (bbc1.size()<1 || bbc2.size()<1) {
+      if (bbc1.size() < 1 || bbc2.size() < 1) {
         MSG_DEBUG("Failed beam-beam-counter trigger");
         vetoEvent;
       }
@@ -57,26 +59,19 @@ namespace Rivet {
       const IdentifiedFinalState& pionfs = applyProjection<IdentifiedFinalState>(event, "PionFS");
       foreach (const Particle& p, pionfs.particles()) {
         if (fabs(p.momentum().rapidity()) < 0.5) {
+          /// @todo Use a binned counter to avoid this bin width cancellation hack
           const double pT = p.momentum().pT() / GeV;
-          if (p.pdgId()>0) {
-            _h_pT_piplus->fill(pT, weight/pT);
-          }
-          else {
-            _h_pT_piminus->fill(pT, weight/pT);
-          }
+          ((p.pdgId() > 0) ? _h_pT_piplus : _h_pT_piminus)->fill(pT, weight/pT);
+          ((p.pdgId() > 0) ? _tmp_pT_piplus : _tmp_pT_piminus)->fill(pT, weight/pT);
         }
       }
 
       const IdentifiedFinalState& protonfs = applyProjection<IdentifiedFinalState>(event, "ProtonFS");
       foreach (const Particle& p, protonfs.particles()) {
         if (fabs(p.momentum().rapidity()) < 0.5) {
+          /// @todo Use a binned counter to avoid this bin width cancellation hack
           const double pT = p.momentum().pT() / GeV;
-          if (p.pdgId()>0) {
-            _h_pT_proton->fill(pT, weight/pT);
-          }
-          else {
-            _h_pT_antiproton->fill(pT, weight/pT);
-          }
+          ((p.pdgId() > 0) ? _h_pT_proton : _h_pT_antiproton)->fill(pT, weight/pT);
         }
       }
       _sumWeightSelected += event.weight();
@@ -85,30 +80,23 @@ namespace Rivet {
 
     /// Finalize
     void finalize() {
-      divide(_h_pT_piminus, _h_pT_piplus, _h_piminus_piplus);
-      divide(_h_pT_antiproton, _h_pT_proton, _h_antipr_pr);
-      divide(_h_pT_proton, _h_pT_piplus, _h_pr_piplus);
-      divide(_h_pT_antiproton, _h_pT_piminus, _h_antipr_piminus);
-      scale(_h_pT_piplus,     1./(2*M_PI*_sumWeightSelected));
-      scale(_h_pT_piminus,    1./(2*M_PI*_sumWeightSelected));
-      scale(_h_pT_proton,     1./(2*M_PI*_sumWeightSelected));
-      scale(_h_pT_antiproton, 1./(2*M_PI*_sumWeightSelected));
+      divide(_h_pT_piminus, _h_pT_piplus, _s_piminus_piplus);
+      divide(_h_pT_antiproton, _h_pT_proton, _s_antipr_pr);
+      divide(_h_pT_proton, _tmp_pT_piplus, _s_pr_piplus);
+      divide(_h_pT_antiproton, _tmp_pT_piminus, _s_antipr_piminus);
+      scale(_h_pT_piplus,     1/(2*M_PI*_sumWeightSelected));
+      scale(_h_pT_piminus,    1/(2*M_PI*_sumWeightSelected));
+      scale(_h_pT_proton,     1/(2*M_PI*_sumWeightSelected));
+      scale(_h_pT_antiproton, 1/(2*M_PI*_sumWeightSelected));
     }
 
 
   private:
 
     double _sumWeightSelected;
-
-    Histo1DPtr _h_pT_piplus;
-    Histo1DPtr _h_pT_piminus;
-    Histo1DPtr _h_pT_proton;
-    Histo1DPtr _h_pT_antiproton;
-
-    Scatter2DPtr _h_piminus_piplus;
-    Scatter2DPtr _h_antipr_pr;
-    Scatter2DPtr _h_pr_piplus;
-    Scatter2DPtr _h_antipr_piminus;
+    Histo1DPtr _h_pT_piplus, _h_pT_piminus, _h_pT_proton, _h_pT_antiproton;
+    Histo1DPtr _tmp_pT_piplus, _tmp_pT_piminus;
+    Scatter2DPtr _s_piminus_piplus, _s_antipr_pr, _s_pr_piplus, _s_antipr_piminus;
 
   };
 
