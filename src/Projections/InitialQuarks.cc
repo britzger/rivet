188b6428f9cb61f0038e2d4ae7be7a3a3a325244
Peter Richardson <Peter.Richardson@durham.ac.uk>
2014-06-05 08:59:38 +0100

    fix to InitialQuarks projection to include bottom and intermediate photons
    --HG--
    branch : release-2-0

--- src/Projections/InitialQuarks.cc
+++ src/Projections/InitialQuarks.cc
@@ -20,13 +20,13 @@ namespace Rivet {
       const GenVertex* pv = p->production_vertex();
       const GenVertex* dv = p->end_vertex();
       const PdgId pid = abs(p->pdg_id());
-      bool passed = inRange((long)pid, 1, 5);
+      bool passed = inRange((long)pid, 1, 6);
       if (passed) {
         if (pv != 0) {
           foreach (const GenParticle* pp, particles_in(pv)) {
             // Only accept if parent is electron or Z0
             const PdgId pid = abs(pp->pdg_id());
-            passed = (pid == PID::ELECTRON || abs(pp->pdg_id()) == PID::ZBOSON);
+            passed = (pid == PID::ELECTRON || abs(pp->pdg_id()) == PID::ZBOSON || abs(pp->pdg_id()) == PID::GAMMA);
           }
         } else {
           passed = false;
