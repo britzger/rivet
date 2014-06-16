50da25239a270a0a69215842781727ef798ee20d
Peter Richardson <Peter.Richardson@durham.ac.uk>
2014-06-16 09:52:24 +0100

    changed norm to scale including sumOfWeights in ATLAS_2011_I954993
    --HG--
    branch : release-2-0

--- src/Analyses/ATLAS_2011_I954993.cc
+++ src/Analyses/ATLAS_2011_I954993.cc
@@ -93,7 +93,7 @@ namespace Rivet {
 
     /// Finalize
     void finalize() {
-      normalize(_h_fiducial, crossSection()/femtobarn);
+      scale(_h_fiducial, crossSection()/femtobarn/sumOfWeights());
     }
 
     //@}
