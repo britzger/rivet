134b2a9e8769c40f19992247912ed157d81177f2
Andy Buckley <andy@insectnation.org>
2014-11-26 12:25:09 +0000

    Adding DEPRECATED macro to throw compiler deprecation warnings when using deprecated features.
    --HG--
    branch : release-2-2-x

--- include/Rivet/Config/RivetCommon.hh
+++ include/Rivet/Config/RivetCommon.hh
@@ -3,11 +3,14 @@
 
 // Convenience build-setup header for Rivet internal use
 
+
 // Automatic build info from autoconf
 #include "Rivet/Config/RivetConfig.hh"
 #include "Rivet/Config/BuildOptions.hh"
 
-// Macro to help with overzealous compiler warnings
+
+/// Macro to help with overzealous compiler warnings
+/// @note It's easier and better to just not give an arg name to args which won't be used, when possible.
 #ifdef UNUSED
 #elif defined(__GNUC__)
 # define UNUSED(x) UNUSED_ ## x __attribute__((unused))
@@ -16,6 +19,24 @@
 #else
 # define UNUSED(x) x
 #endif
+
+
+/// Macro to help mark code as deprecated to produce compiler warnings
+#if __GNUC__ && __cplusplus && RIVET_NO_DEPRECATION_WARNINGS == 0
+#define GCC_VERSION (__GNUC__ * 10000 + __GNUC_MINOR__ * 100 + __GNUC_PATCHLEVEL__)
+#if GCC_VERSION >= 40500
+  #if __cplusplus > 201103L
+  #define DEPRECATED(x) [[deprecated(x)]]
+  #else
+  #define DEPRECATED(x) __attribute__((deprecated(x)))
+  #endif
+#else
+  #define DEPRECATED(x) __attribute__((deprecated))
+#endif
+#else
+  #define DEPRECATED(x)
+#endif
+
 
 #include "Rivet/Exceptions.hh"
 
