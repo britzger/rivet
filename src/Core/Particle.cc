75bd6a412b90217d2e10db0fbfb6d92c0a1fc5c5
Andy Buckley <andy@insectnation.org>
2015-02-24 22:42:21 +0000

    Add Particle::fromHadron and Particle::fromPromptTau, and add a boolean 'prompt' argument to Particle::fromTau.
    --HG--
    branch : release-2-2-x

--- src/Core/Particle.cc
+++ src/Core/Particle.cc
@@ -19,18 +19,6 @@ namespace Rivet {
   }
 
 
-  bool Particle::fromDecay() const {
-    /// @todo Shouldn't a const vertex be being returned? Ah, HepMC...
-    GenVertex* prodVtx = genParticle()->production_vertex();
-    if (prodVtx == NULL) return false;
-    foreach (const GenParticle* ancestor, particles(prodVtx, HepMC::ancestors)) {
-      const PdgId pid = ancestor->pdg_id();
-      if (ancestor->status() == 2 && (PID::isHadron(pid) || abs(pid) == PID::TAU)) return true;
-    }
-    return false;
-  }
-
-
   bool Particle::fromBottom() const {
     /// @todo Shouldn't a const vertex be being returned? Ah, HepMC...
     GenVertex* prodVtx = genParticle()->production_vertex();
@@ -55,8 +43,22 @@ namespace Rivet {
   }
 
 
-  bool Particle::fromTau() const {
+
+  bool Particle::fromHadron() const {
     /// @todo Shouldn't a const vertex be being returned? Ah, HepMC...
+    GenVertex* prodVtx = genParticle()->production_vertex();
+    if (prodVtx == NULL) return false;
+    foreach (const GenParticle* ancestor, particles(prodVtx, HepMC::ancestors)) {
+      const PdgId pid = ancestor->pdg_id();
+      if (ancestor->status() == 2 && PID::isHadron(pid)) return true;
+    }
+    return false;
+  }
+
+
+  bool Particle::fromTau(bool prompt_taus_only) const {
+    /// @todo Shouldn't a const vertex be being returned? Ah, HepMC...
+    if (prompt_taus_only && fromHadron()) return false;
     GenVertex* prodVtx = genParticle()->production_vertex();
     if (prodVtx == NULL) return false;
     foreach (const GenParticle* ancestor, particles(prodVtx, HepMC::ancestors)) {
@@ -65,6 +67,18 @@ namespace Rivet {
     }
     return false;
   }
+
+
+  // bool Particle::fromDecay() const {
+  //   /// @todo Shouldn't a const vertex be being returned? Ah, HepMC...
+  //   GenVertex* prodVtx = genParticle()->production_vertex();
+  //   if (prodVtx == NULL) return false;
+  //   foreach (const GenParticle* ancestor, particles(prodVtx, HepMC::ancestors)) {
+  //     const PdgId pid = ancestor->pdg_id();
+  //     if (ancestor->status() == 2 && (PID::isHadron(pid) || abs(pid) == PID::TAU)) return true;
+  //   }
+  //   return false;
+  // }
 
 
 }
