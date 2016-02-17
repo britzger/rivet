6555024a5e677878c619328f9702aed914ff7873
Andy Buckley <andy@insectnation.org>
2016-02-17 23:37:42 +0000

    Oops, add missing 'self' args to AOPath methods
    --HG--
    branch : release-2-4-x

--- pyext/rivet/aopaths.py
+++ pyext/rivet/aopaths.py
@@ -18,9 +18,9 @@ class AOPath(object):
     TODO: move to YODA?
     """
     import re
-    re_aopath = re.compile(r"^(/[^\[\]\@]+)(\[\w+\])?(#\d+)?$")
+    re_aopath = re.compile(r"^(/[^\[\]\@\#]+)(\[\w+\])?(#\d+)?$")
 
-    def __init__(path):
+    def __init__(self, path):
         import os
         self.origpath = path
         m = re_aopath.match(path)
@@ -33,54 +33,54 @@ class AOPath(object):
         self._varid = m.group(2).lstrip("[").ristrip("]") if m.group(2) else None
         self._binid = int(m.group(3).lstrip("#")) if m.group(3) else None
 
-    def basepath(keepref=False):
+    def basepath(self, keepref=False):
         "Main 'Unix-like' part of the AO path, optionally including a /REF prefix"
         return self._basepath if keepref else self._basepath.lstrip("/REF").rstrip("/")
 
-    def varpath(keepref=False, defaultvarid=None):
+    def varpath(self, keepref=False, defaultvarid=None):
         "The basepath, plus any bracketed variation identifier"
         p = self.basepath(keepref)
         if self.varid(defaultvarid) is not None:
             p += "[%s]" % str(self.varid(defaultvarid))
         return p
 
-    def binpath(keepref=False, defaultbinid=None, defaultvarid=None):
+    def binpath(self, keepref=False, defaultbinid=None, defaultvarid=None):
         "The varpath, plus any #-prefixed bin number identifier"
         p = self.varpath(keepref, defaultvarid)
         if self.binid(defaultbinid) is not None:
             p += "#%d" % self.binid(defaultbinid)
         return p
 
-    def basepathparts(keepref=False):
+    def basepathparts(self, keepref=False):
         "List of basepath components, split by forward slashes"
         return self.basepath(keepref).strip("/").split("/")
 
     # TODO: basepathhead, basepathtail
 
-    def dirname(keepref=False):
+    def dirname(self, keepref=False):
         "The non-final (i.e. dir-like) part of the basepath"
         return os.path.dirname(self.basepath(keepref))
 
-    def dirnameparts(keepref=False):
+    def dirnameparts(self, keepref=False):
         "List of dirname components, split by forward slashes"
         return self.dirname(keepref).strip("/").split("/")
 
-    def basename():
+    def basename(self):
         "The final (i.e. file-like) part of the basepath"
         return self._basename
 
-    def varid(default=None):
+    def varid(self, default=None):
         "The variation identifier (without brackets) if there is one, otherwise None"
         return self._varid if self._varid is not None else default
 
-    def binid(default=None):
+    def binid(self, default=None):
         "The bin identifier (without #) if there is one, otherwise None"
         return self._binid if self._binid is not None else default
 
-    def isref():
+    def isref(self):
         "Is there a /REF prefix in the original path?"
         return self._isref
 
-    def istmp():
+    def istmp(self):
         "Do any basepath components start with an underscore, used to hide them from plotting?"
         return isTmpPath(self.basepath())
