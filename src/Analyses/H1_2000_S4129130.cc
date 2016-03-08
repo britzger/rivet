b9e3aabbf4037d557ed7b482b8cadc87bbaf1ddb
Andy Buckley <andy@insectnation.org>
2016-03-08 21:15:26 +0000

    Use conventional method ordering

--- src/Analyses/H1_2000_S4129130.cc
+++ src/Analyses/H1_2000_S4129130.cc
@@ -24,6 +24,50 @@ namespace Rivet {
     /// @name Analysis methods
     //@{
 
+    void init() {
+      // Projections
+      addProjection(DISLepton(), "Lepton");
+      addProjection(DISKinematics(), "Kinematics");
+      addProjection(FinalState(), "FS");
+
+      // Histos
+      Histo1DPtr h;
+
+      // Histograms and weight vectors for low Q^2 a
+      for (size_t ix = 0; ix < 17; ++ix) {
+        h = bookHisto1D(ix+1, 1, 1);
+        _histETLowQa.push_back(h);
+        _weightETLowQa.push_back(0.);
+      }
+
+      // Histograms and weight vectors for high Q^2 a
+      for (size_t ix = 0; ix < 7; ++ix) {
+        h = bookHisto1D(ix+18, 1, 1);
+        _histETHighQa.push_back(h);
+        _weightETHighQa.push_back(0.);
+      }
+
+      // Histograms and weight vectors for low Q^2 b
+      for (size_t ix = 0; ix < 5; ++ix) {
+        h = bookHisto1D(ix+25, 1, 1);
+        _histETLowQb.push_back(h);
+        _weightETLowQb.push_back(0.);
+      }
+
+      // Histograms and weight vectors for high Q^2 b
+      for (size_t ix = 0; ix < 3; ++ix) {
+        h = bookHisto1D(30+ix, 1, 1);
+        _histETHighQb.push_back(h);
+        _weightETHighQb.push_back(0.0);
+      }
+
+      // Histograms for the averages
+      _histAverETCentral = bookProfile1D(33,  1, 1);
+      _histAverETFrag = bookProfile1D(34,  1, 1);
+    }
+
+
+    /// Analyze each event
     void analyze(const Event& event) {
 
       // DIS kinematics
@@ -172,49 +216,6 @@ namespace Rivet {
     }
 
 
-    void init() {
-      // Projections
-      addProjection(DISLepton(), "Lepton");
-      addProjection(DISKinematics(), "Kinematics");
-      addProjection(FinalState(), "FS");
-
-      // Histos
-      Histo1DPtr h;
-
-      // Histograms and weight vectors for low Q^2 a
-      for (size_t ix = 0; ix < 17; ++ix) {
-        h = bookHisto1D(ix+1, 1, 1);
-        _histETLowQa.push_back(h);
-        _weightETLowQa.push_back(0.);
-      }
-
-      // Histograms and weight vectors for high Q^2 a
-      for (size_t ix = 0; ix < 7; ++ix) {
-        h = bookHisto1D(ix+18, 1, 1);
-        _histETHighQa.push_back(h);
-        _weightETHighQa.push_back(0.);
-      }
-
-      // Histograms and weight vectors for low Q^2 b
-      for (size_t ix = 0; ix < 5; ++ix) {
-        h = bookHisto1D(ix+25, 1, 1);
-        _histETLowQb.push_back(h);
-        _weightETLowQb.push_back(0.);
-      }
-
-      // Histograms and weight vectors for high Q^2 b
-      for (size_t ix = 0; ix < 3; ++ix) {
-        h = bookHisto1D(30+ix, 1, 1);
-        _histETHighQb.push_back(h);
-        _weightETHighQb.push_back(0.0);
-      }
-
-      // Histograms for the averages
-      _histAverETCentral = bookProfile1D(33,  1, 1);
-      _histAverETFrag = bookProfile1D(34,  1, 1);
-    }
-
-
     // Finalize
     void finalize() {
       // Normalization of the Et distributions
@@ -224,7 +225,6 @@ namespace Rivet {
       for (size_t ix = 0; ix <  5; ++ix) if (_weightETLowQb[ix]  != 0) scale(_histETLowQb[ix],  1/_weightETLowQb[ix]);
       for (size_t ix = 0; ix <  3; ++ix) if (_weightETHighQb[ix] != 0) scale(_histETHighQb[ix], 1/_weightETHighQb[ix]);
     }
-
 
     //@}
 
