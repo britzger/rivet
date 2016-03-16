d5fae9c2346a07cff2513011a15fe709d0a1694c
Andy Buckley <andy@insectnation.org>
2016-03-16 18:29:01 +0000

    Do copying/filtering

--- include/Rivet/Projections/SmearedJets.hh
+++ include/Rivet/Projections/SmearedJets.hh
@@ -28,7 +28,6 @@ namespace Rivet {
     /// @name Constructors etc.
     //@{
 
-
     // SmearedJets(const JetAlg& ja,
     // boost::function<double(const Jet&)>& jetEffFn, boost::function<void(Jet&)>& jetSmearFn=jet_smear_identity)
     // boost::function<double(const Jet&)>& bTagEffFn=fn1, boost::function<double(const Jet&)>& bTagMistagFn=fn0,
@@ -38,14 +37,10 @@ namespace Rivet {
     // _cTagEffFn(cTagEffFn), _cTagMistagFn(cTagMistagFn)
 
 
-    // typedef double (*J2DFN)(Jet&);
-    // typedef void (*J2VFN)(Jet&);
-
-
     /// Constructor with jet efficiency function arg
     template <typename J2DFN>
     SmearedJets(const JetAlg& ja, J2DFN jetEffFn)
-      : _jetEffFn(jetEffFn) //, _jetSmearFn(JET_SMEAR_IDENTITY)
+      : _jetEffFn(jetEffFn)
     {
       setName("SmearedJets");
       addProjection(ja, "TruthJets");
@@ -71,7 +66,7 @@ namespace Rivet {
 
 
     int compare(const Projection& p) const {
-      /// @todo Maybe do the comparison via void* pointer comparisons, extracted before the function<> wrapping
+      /// @todo Maybe do the comparison via void* pointer comparisons, extracted before the function<> wrapping?
       //const SmearedJets& other = dynamic_cast<const SmearedJets&>(p);
       //return cmp(_jetEffFn, other._jetEffFn) || cmp(_jetSmearFn, other._jetSmearFn);
       return UNDEFINED;
@@ -91,16 +86,15 @@ namespace Rivet {
 
 
     void project(const Event& e) {
-      _recojets = applyProjection<JetAlg>(e, "TruthJets").jetsByPt();
-      // Filtering
-      /// @todo Use erase-remove_if?
-      if (_jetEffFn) {
-        foreach (Jet& j, _recojets) {
-          const double jeff = _jetEffFn(j);
-          MSG_INFO(j.pT()/GeV << " " << jeff);
-          if (jeff == 1) continue; //< don't remove; no need to roll expensive dice
-          if (jeff == 0 || jeff > rand01()) MSG_INFO("REMOVE JET!!!"); ///< @todo Actually remove it...
-        }
+
+      // Copying and filtering
+      const Jets& truthjets = applyProjection<JetAlg>(e, "TruthJets").jetsByPt();
+      _recojets.clear(); _recojets.reserve(truthjets.size());
+      foreach (const Jet& j, truthjets) {
+        const double jeff = (_jetEffFn) ? _jetEffFn(j) : 1;
+        MSG_DEBUG("Efficiency of jet " << j.mom() << " = " << 100*jeff << "%");
+        if (jeff == 0) continue; //< remove; no need to roll expensive dice
+        if (jeff == 1 || jeff < rand01()) _recojets.push_back(j);
       }
       // Smearing
       if (_jetSmearFn) {
