effcefa1e60767d62aabb3137ce3e2c2fb7e4893
Andy Buckley <andy@insectnation.org>
2016-03-17 20:48:12 +0000

    Add some tagging logic and a few more tweaks

--- include/Rivet/Projections/SmearedJets.hh
+++ include/Rivet/Projections/SmearedJets.hh
@@ -17,7 +17,8 @@ namespace Rivet {
   // double P4_FN1(const FourMomentum& p) { return 1; }
   // Particle PARTICLE_SMEAR_IDENTITY(const Particle& p) { return p; }
 
-  double JET_EFF_PERFECT(const Jet& p) { return 1; }
+  double JET_EFF_ZERO(const Jet& p) { return 0; }
+  double JET_EFF_ONE(const Jet& p) { return 1; }
   double JET_BTAG_PERFECT(const Jet& j) { return j.bTagged() ? 1 : 0; }
   double JET_CTAG_PERFECT(const Jet& j) { return j.cTagged() ? 1 : 0; }
   Jet JET_SMEAR_IDENTITY(const Jet& j) { return j; }
@@ -31,11 +32,11 @@ namespace Rivet {
     /// @name Constructors etc.
     //@{
 
-    /// @brief Constructor with smearing function args
+    /// @brief Constructor with efficiency and smearing function args
     /// The jet reconstruction efficiency is mandatory; the smearing and tagging functions are optional
-    template <typename J2DFN, typename J2VFN>
+    template <typename J2DFN, typename J2JFN>
     SmearedJets(const JetAlg& ja, J2DFN jetEffFn,
-                J2VFN jetSmearFn=JET_SMEAR_IDENTITY,
+                J2JFN jetSmearFn=JET_SMEAR_IDENTITY,
                 J2DFN bTagEffFn=JET_BTAG_PERFECT, J2DFN cTagEffFn=JET_CTAG_PERFECT)
       : _jetEffFn(jetEffFn), _bTagEffFn(cTagEffFn), _cTagEffFn(cTagEffFn), _jetSmearFn(jetSmearFn)
     {
@@ -44,7 +45,7 @@ namespace Rivet {
     }
 
 
-    /// @brief Constructor with smearing function args
+    /// @brief Constructor with all-mandatory efficiency function args and no smearing
     /// The jet reconstruction efficiency is mandatory; the smearing and tagging functions are optional
     template <typename J2DFN>
     SmearedJets(const JetAlg& ja, J2DFN jetEffFn, J2DFN bTagEffFn, J2DFN cTagEffFn)
@@ -91,14 +92,25 @@ namespace Rivet {
       foreach (const Jet& j, truthjets) {
         const double jeff = (_jetEffFn) ? _jetEffFn(j) : 1;
         MSG_DEBUG("Efficiency of jet " << j.mom() << " = " << 100*jeff << "%");
-        if (jeff == 0) continue; //< remove; no need to roll expensive dice
-        if (jeff == 1 || jeff < rand01()) _recojets.push_back(j);
-      }
-      // Smearing
-      if (_jetSmearFn) {
-        foreach (Jet& j, _recojets) {
-          _jetSmearFn(j); //< smear the jet momentum
+        if (jeff == 0) continue; //< no need to roll expensive dice
+        if (jeff == 1 || jeff < rand01()) {
+          /// @todo Modify constituent particle vectors for consistency
+          /// @todo Set a null PseudoJet if the Jet is smeared?
+          _recojets.push_back(_jetSmearFn ? _jetSmearFn(j) : j); //< smearing
         }
+      }
+      // Tagging efficiencies
+      foreach (Jet& j, _recojets) {
+        const double beff = (_bTagEffFn) ? _bTagEffFn(j) : 1;
+        const bool btag = beff == 1 || (beff != 0 && beff < rand01());
+        /// @todo Remove b-tags if needed, and add a dummy one if needed
+        //if (!btag && j.bTagged()) erase(remove_if(...)...);
+        //if (btag && !j.bTagged()) j.tags().push_back(Particle(...));
+        const double ceff = (_cTagEffFn) ? _cTagEffFn(j) : 1;
+        const bool ctag = ceff == 1 || (ceff != 0 && ceff < rand01());
+        /// @todo Remove c-tags if needed, and add a dummy one if needed
+        //if (!ctag && j.cTagged()) erase(remove_if(...)...);
+        //if (ctag && !j.cTagged()) j.tags().push_back(Particle(...));
       }
     }
 
@@ -118,7 +130,7 @@ namespace Rivet {
 
     Jets _recojets;
     boost::function<double(const Jet&)> _jetEffFn, _bTagEffFn, _cTagEffFn;
-    boost::function<void(Jet&)> _jetSmearFn;
+    boost::function<Jet(const Jet&)> _jetSmearFn;
 
   };
 
