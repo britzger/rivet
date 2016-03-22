32722492f1d95dbfc48a2d07db123d3cdefcede2
Andy Buckley <andy@insectnation.org>
2016-03-22 22:25:45 +0000

    Comments

--- include/Rivet/Projections/SmearedJets.hh
+++ include/Rivet/Projections/SmearedJets.hh
@@ -20,21 +20,28 @@ namespace Rivet {
   // double P4_FN1(const FourMomentum& p) { return 1; }
   // Particle PARTICLE_SMEAR_IDENTITY(const Particle& p) { return p; }
 
+  /// Return a uniformly sampled random number between 0 and 1
   /// @todo Move to (math?)utils
   double rand01() { return rand() / (double)RAND_MAX; }
 
+  /// Return a constant 0 given a Jet as argument
   double JET_EFF_ZERO(const Jet& p) { return 0; }
+  /// Return a constant 1 given a Jet as argument
   double JET_EFF_ONE(const Jet& p) { return 1; }
 
+  /// Return 1 if the given Jet contains a b, otherwise 0
   double JET_BTAG_PERFECT(const Jet& j) { return j.bTagged() ? 1 : 0; }
+  /// Return the ATLAS Run 1c jet flavour tagging efficiency for the given Jet
   double JET_BTAG_ATLAS_RUN1C(const Jet& j) {
     if (j.bTagged()) return 0.80*tanh(0.003*j.pT()/GeV)*(30/(1+0.086*j.pT()/GeV));
     if (j.cTagged()) return 0.20*tanh(0.02*j.pT()/GeV)*(1/(1+0.0034*j.pT()/GeV));
     return 0.002 + 7.3e-6*j.pT()/GeV;
   }
 
+  /// Return 1 if the given Jet contains a c, otherwise 0
   double JET_CTAG_PERFECT(const Jet& j) { return j.cTagged() ? 1 : 0; }
 
+  /// Take a jet and return an unmodified copy
   /// @todo Modify constituent particle vectors for consistency
   /// @todo Set a null PseudoJet if the Jet is smeared?
   Jet JET_SMEAR_IDENTITY(const Jet& j) { return j; }
