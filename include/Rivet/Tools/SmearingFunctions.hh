e001858b22ee63f5b3ef5b241106ac27f21d77da
Andy Buckley <andy@insectnation.org>
2016-04-06 15:39:37 +0100

    More smearing & efficiency functions

--- include/Rivet/Tools/SmearingFunctions.hh
+++ include/Rivet/Tools/SmearingFunctions.hh
@@ -32,6 +32,10 @@ namespace Rivet {
 
   inline Particle PARTICLE_SMEAR_IDENTITY(const Particle& p) { return p; }
 
+
+  ////////////////////////
+
+
   inline double ELECTRON_TRKEFF_ATLAS_RUN1(const Particle& e) {
     if (e.abseta() > 2.5) return 0;
     if (e.pT() < 0.1*GeV) return 0;
@@ -73,8 +77,9 @@ namespace Rivet {
     const int i_pt = binIndex(e.pT()/GeV, edges_pt, true);
     const int i = i_eta*edges_pt.size() + i_pt;
 
+    // Calculate absolute resolution in GeV
     const double c1 = sqr(e2s[i]), c2 = sqr(es[i]), c3 = sqr(cs[i]);
-    const double resolution = sqrt(c1*e.E2() + c2*e.E() + c3);
+    const double resolution = sqrt(c1*e.E2() + c2*e.E() + c3) * GeV;
 
     /// @todo Extract to a smear_energy helper function
     /// @todo Also make smear_direction and smear_pt functions, and jet versions that also update/smear constituents
@@ -82,6 +87,114 @@ namespace Rivet {
     const double smeared_E = max(d(gen), e.mass()); //< can't let the energy go below the mass!
     return Particle(e.pid(), FourMomentum::mkEtaPhiME(e.eta(), e.phi(), e.mass(), smeared_E));
   }
+
+
+  ///////////////////
+
+
+  inline double MUON_TRKEFF_ATLAS_RUN1(const Particle& m) {
+    if (m.abseta() > 2.5) return 0;
+    if (m.pT() < 0.1*GeV) return 0;
+    if (m.abseta() < 1.5) {
+      return (m.pT() < 1*GeV) ? 0.75 : 0.99;
+    } else {
+      return (m.pT() < 1*GeV) ? 0.70 : 0.98;
+    }
+  }
+
+  inline double MUON_EFF_ATLAS_RUN1(const Particle& m) {
+    if (m.abseta() > 2.7) return 0;
+    if (m.pT() < 10*GeV) return 0;
+    return (m.abseta() < 1.5) ? 0.95 : 0.85;
+  }
+
+  inline Particle MUON_SMEAR_ATLAS_RUN1(const Particle& m) {
+    /// @todo Need to isolate random generators to a single thread
+    static random_device rd;
+    static mt19937 gen(rd());
+
+    static const vector<double> edges_eta = {0, 1.5, 2.5};
+    static const vector<double> edges_pt = {0, 0.1, 1.0, 10., 200.};
+    static const vector<double> res = {0., 0.03, 0.02, 0.03, 0.05,
+                                       0., 0.04, 0.03, 0.04, 0.05};
+
+    const int i_eta = binIndex(m.abseta(), edges_eta, true);
+    const int i_pt = binIndex(m.pT()/GeV, edges_pt, true);
+    const int i = i_eta*edges_pt.size() + i_pt;
+
+    const double resolution = res[i];
+
+    // Smear by a Gaussian centered on the current pT, with width given by the resolution
+    /// @todo Extract to a smear_pt helper function
+    normal_distribution<> d(m.pT(), resolution*m.pT());
+    const double smeared_pt = max(d(gen), 0.);
+    return Particle(m.pid(), FourMomentum::mkEtaPhiMPt(m.eta(), m.phi(), m.mass(), smeared_pt));
+  }
+
+
+  //////////////////////////
+
+
+  inline double TAU_EFF_ATLAS_RUN1(const Particle& t) {
+    if (t.abseta() > 2.5) return 0;
+    if (t.pT() < 1*GeV) return 0;
+    /// @todo If leptonic return 0
+    return 0.45;
+  }
+
+
+  /// From ATL-PHYS-PUB-2015-045 medium working point
+  inline double TAU_EFF_ATLAS_RUN2(const Particle& t) {
+    if (t.abseta() > 2.5) return 0;
+    if (t.pT() < 1*GeV) return 0;
+    /// @todo If leptonic return 0
+    ///< @todo Make nProng-specific
+    // 1 prong efficiency / mistag = 0.7 / 0.02
+    // >= 2 prong efficiency / mistag = 0.6 / 0.01
+    const vector<Particle> children = t.children();
+    /// @todo Require prongs in tracker acceptance, too?
+    const size_t nprongs = count_if(children.begin(), children.end(),
+                                    [](const Particle& p){ return p.isHadron() && p.charge3() != 0; });
+    if (nprongs == 0) return 0;
+    if (nprongs == 1) return (t.abspid() == PID::TAU) ? 0.7 : 0.02;
+    return (t.abspid() == PID::TAU) ? 0.6 : 0.01;
+  }
+
+
+  inline Particle TAU_SMEAR_ATLAS_RUN1(const Particle& t) {
+    /// @todo Need to isolate random generators to a single thread
+    static random_device rd;
+    static mt19937 gen(rd());
+
+    // Const fractional resolution for now
+    static const double resolution = 0.03;
+
+    // Smear by a Gaussian centered on 1 with width given by the (fractional) resolution
+    /// @todo Is this the best way to smear? Should we preserve the energy, or pT, or direction?
+    normal_distribution<> d(1., resolution);
+    const double fsmear = max(d(gen), 0.);
+    return Particle(t.pid(), FourMomentum::mkXYZM(t.px()*fsmear, t.py()*fsmear, t.pz()*fsmear, t.mass()));
+  }
+
+
+  ///////////////////////
+
+
+  inline Jet JET_SMEAR_ATLAS_RUN1(const Jet& j) {
+    /// @todo Need to isolate random generators to a single thread
+    static random_device rd;
+    static mt19937 gen(rd());
+
+    // Const fractional resolution for now
+    static const double resolution = 0.03;
+
+    // Smear by a Gaussian centered on 1 with width given by the (fractional) resolution
+    /// @todo Is this the best way to smear? Should we preserve the energy, or pT, or direction?
+    normal_distribution<> d(1., resolution);
+    const double fsmear = max(d(gen), 0.);
+    return Jet(FourMomentum::mkXYZM(j.px()*fsmear, j.py()*fsmear, j.pz()*fsmear, j.mass()));
+  }
+
 
   //@}
 
