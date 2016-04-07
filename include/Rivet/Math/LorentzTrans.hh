37c9c3e9ffdaaf685ca9d6cc850b6b3c10ba6724
Andy Buckley <andy@insectnation.org>
2016-04-07 15:55:01 +0100

    Interface and documentation improvements

--- include/Rivet/Math/LorentzTrans.hh
+++ include/Rivet/Math/LorentzTrans.hh
@@ -1,27 +1,44 @@
 #ifndef RIVET_MATH_LORENTZTRANS
 #define RIVET_MATH_LORENTZTRANS
 
-#include <iostream>
-
 #include "Rivet/Math/MathHeader.hh"
 #include "Rivet/Math/MathUtils.hh"
 #include "Rivet/Math/MatrixN.hh"
 #include "Rivet/Math/Matrix3.hh"
 #include "Rivet/Math/Vector4.hh"
+#include <iostream>
 
 namespace Rivet {
 
 
-  inline double lorentzGamma(const double beta) {
-    return 1.0 / sqrt(1 - beta*beta);
+  /// @name Simple Lorentz factor conversions
+  //@{
+
+  /// Calculate the gamma factor from beta
+  inline double lorentzBeta2Gamma(double beta) {
+    return 1.0 / sqrt(1 - sqr(beta));
   }
+
+  /// Calculate beta from the gamma factor
+  inline double lorentzGamma2Beta(double gamma) {
+    return sqrt(1 - sqr(1/gamma));
+  }
+
+  //@}
 
 
   /// @brief Object implementing Lorentz transform calculations and boosts.
+  ///
+  /// @note These boosts are defined actively, i.e. as modifications of vectors
+  /// rather than frame transformations. So the boost vector is the opposite of
+  /// what you might expect.
+  ///
+  /// @todo Review the active/passive convention choice. Seems counterintuitive now...
   class LorentzTransform {
-    friend string toString(const LorentzTransform& lt);
-
   public:
+
+    // friend string toString(const LorentzTransform& lt); ///< @todo Ick!
+
     LorentzTransform() {
       _boostMatrix = Matrix<4>::mkIdentity();
     }
@@ -30,14 +47,15 @@ namespace Rivet {
       setBoost(boost);
     }
 
-    LorentzTransform(const double betaX, const double betaY, const double betaZ) {
+    LorentzTransform(double betaX, double betaY, double betaZ) {
       setBoost(betaX, betaY, betaZ);
     }
 
+    /// Set the \f$ \vec\beta \f$ vector for an active Lorentz boost
     LorentzTransform& setBoost(const Vector3& boost) {
       assert(boost.mod2() < 1);
       const double beta = boost.mod();
-      const double gamma = lorentzGamma(beta);
+      const double gamma = lorentzBeta2Gamma(beta);
       _boostMatrix = Matrix<4>::mkIdentity();
       _boostMatrix.set(0, 0, gamma);
       _boostMatrix.set(1, 1, gamma);
@@ -48,15 +66,12 @@ namespace Rivet {
       return *this;
     }
 
-  //   LorentzTransform& setBoost(const Vector3& boostdirection, const double beta) {
-  //     const Vector3 boost = boostdirection.unit() * beta;
-  //     return setBoost(boost);
-  //   }
-
-    LorentzTransform& setBoost(const double betaX, const double betaY, const double betaZ) {
+    /// Set the \f$ \vec\beta \f$ vector for an active Lorentz boost, as 3-components
+    LorentzTransform& setBoost(double betaX, double betaY, double betaZ) {
       return setBoost(Vector3(betaX, betaY, betaZ));
     }
 
+    /// Get the \f$ \vec\beta \f$ vector for an active Lorentz boost
     Vector3 boost() const {
       FourMomentum boost(_boostMatrix.getColumn(0));
       //cout << "!!!" << boost << endl;
@@ -66,34 +81,38 @@ namespace Rivet {
       return boost.p3().unit() * beta;
     }
 
+    /// Get the \f$ \beta \f$ factor
     double beta() const {
       return boost().mod();
     }
 
+    /// Get the \f$ \gamma \f$ factor
     double gamma() const {
-      return lorentzGamma(beta());
+      return lorentzBeta2Gamma(beta());
     }
 
     LorentzTransform rotate(const Vector3& from, const Vector3& to) const {
       return rotate(Matrix3(from, to));
     }
 
-    LorentzTransform rotate(const Vector3& axis, const double angle) const {
+    LorentzTransform rotate(const Vector3& axis, double angle) const {
       return rotate(Matrix3(axis, angle));
     }
 
     LorentzTransform rotate(const Matrix3& rot) const {
       LorentzTransform lt = *this;
-      const Matrix4 rot4 = mkMatrix4(rot);
+      const Matrix4 rot4 = _mkMatrix4(rot);
       const Matrix4 newlt = rot4 * _boostMatrix * rot4.inverse();
       lt._boostMatrix = newlt;
       return lt;
     }
 
+    /// Apply this transformation to the given 4-vector
     FourVector transform(const FourVector& v4) const {
       return multiply(_boostMatrix, v4);
     }
 
+    /// Calculate the inverse transform
     LorentzTransform inverse() const {
       LorentzTransform rtn;
       rtn._boostMatrix = _boostMatrix.inverse();
@@ -108,27 +127,32 @@ namespace Rivet {
       return rtn;
     }
 
+    /// Return the matrix form
     Matrix4 toMatrix() const {
       return _boostMatrix;
     }
 
-
+    /// Operator combination of two LTs
     LorentzTransform operator*(const LorentzTransform& lt) const {
       return combine(lt);
     }
 
-    LorentzTransform preMult(const Matrix3 & m3) {
-      _boostMatrix = multiply(mkMatrix4(m3),_boostMatrix);
+    /// Pre-multiply m3 by this LT
+    LorentzTransform preMult(const Matrix3& m3) {
+      _boostMatrix = multiply(_mkMatrix4(m3),_boostMatrix);
       return *this;
     }
 
-    LorentzTransform postMult(const Matrix3 & m3) {
-      _boostMatrix *= mkMatrix4(m3);
+    /// Post-multiply m3 by this LT
+    LorentzTransform postMult(const Matrix3& m3) {
+      _boostMatrix *= _mkMatrix4(m3);
       return *this;
     }
+
 
   private:
-    Matrix4 mkMatrix4(const Matrix3& m3) const {
+
+    Matrix4 _mkMatrix4(const Matrix3& m3) const {
       Matrix4 m4 = Matrix4::mkIdentity();
       for (size_t i = 0; i < 3; ++i) {
         for (size_t j = 0; j < 3; ++j) {
@@ -138,8 +162,6 @@ namespace Rivet {
       return m4;
     }
 
-
-  private:
     Matrix4 _boostMatrix;
 
   };
@@ -163,7 +185,7 @@ namespace Rivet {
 
 
   inline string toString(const LorentzTransform& lt) {
-    return toString(lt._boostMatrix);
+    return toString(lt.toMatrix());
   }
 
   inline ostream& operator<<(std::ostream& out, const LorentzTransform& lt) {
