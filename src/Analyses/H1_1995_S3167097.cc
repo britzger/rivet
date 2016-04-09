8521d7dffefb80b594cc5531142f26e842cdb3ef
David Grellscheid <david.grellscheid@durham.ac.uk>
2016-04-09 12:03:00 +0100

    Use make_shared where possible

--- src/Analyses/H1_1995_S3167097.cc
+++ src/Analyses/H1_1995_S3167097.cc
@@ -92,9 +92,9 @@ namespace Rivet {
       for (size_t ibin = 0; ibin < 9; ++ibin)
         scale(_hEtFlow[ibin], 0.5/_sumw[ibin]);
       /// @todo Improve this!
-      addAnalysisObject(Scatter2DPtr( new Scatter2D(_tmphAvEt/_tmphN, histoPath("21")) ));
-      addAnalysisObject(Scatter2DPtr( new Scatter2D(_tmphAvX/_tmphN,  histoPath("22")) ));
-      addAnalysisObject(Scatter2DPtr( new Scatter2D(_tmphAvQ2/_tmphN, histoPath("23")) ));
+      addAnalysisObject(make_shared<Scatter2D>(_tmphAvEt/_tmphN, histoPath("21")) );
+      addAnalysisObject(make_shared<Scatter2D>(_tmphAvX/_tmphN,  histoPath("22")) );
+      addAnalysisObject(make_shared<Scatter2D>(_tmphAvQ2/_tmphN, histoPath("23")) );
     }
 
     //@}
