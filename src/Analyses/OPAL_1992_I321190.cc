21ed8c168c3c138ca7afe46ca71eb85852640fb4
Peter Richardson <Peter.Richardson@durham.ac.uk>
2017-03-08 11:59:47 +0000

    added analyeses for charged multiplicity at Z pole
    --HG--
    branch : release-2-5-x

--- src/Analyses/OPAL_1992_I321190.cc
+++ src/Analyses/OPAL_1992_I321190.cc
@@ -5,44 +5,40 @@
 namespace Rivet {
 
 
-  /// @brief ALEPH LEP1 charged multiplicity in hadronic Z decay
-  /// @author Andy Buckley
-  class ALEPH_1991_S2435284 : public Analysis {
+  /// @brief OPAL LEP1 charged multiplicity, code basically a copy of the ALEPH one
+  /// @author Peter Richardson
+  class OPAL_1992_I321190 : public Analysis {
   public:
 
-    /// Constructor.
-    ALEPH_1991_S2435284()
-      : Analysis("ALEPH_1991_S2435284")
-    {
-    }
+    /// Constructor
+    DEFAULT_RIVET_ANALYSIS_CTOR(OPAL_1992_I321190);
 
 
     /// @name Analysis methods
     //@{
 
-    /// Book projections and histogram
+    /// Book histograms and initialise projections before the run
     void init() {
       const ChargedFinalState cfs;
       declare(cfs, "CFS");
 
       _histChTot = bookHisto1D(1, 1, 1);
-      _histAver  = bookHisto1D(2, 1, 1);
+      _histAver  = bookProfile1D(5, 1, 1);
     }
 
 
-    /// Do the analysis
+    /// Perform the per-event analysis
     void analyze(const Event& event) {
       const FinalState& cfs = apply<FinalState>(event, "CFS");
       MSG_DEBUG("Total charged multiplicity = " << cfs.size());
       _histChTot->fill(cfs.size(), event.weight());
-      _histAver->fill(_histAver->bin(0).xMid(),cfs.size()*event.weight());
+      _histAver->fill(sqrtS(),cfs.size(),event.weight());
     }
 
 
-    /// Normalize the histogram
+    /// Normalise histograms etc., after the run
     void finalize() {
-      scale(_histChTot, 2.0/sumOfWeights()); // same as in ALEPH 1996
-      scale(_histAver , 1./sumOfWeights());
+      scale(_histChTot, 200.0/sumOfWeights()); // bin width (2) and %age (100)
     }
 
     //@}
@@ -53,14 +49,14 @@ namespace Rivet {
     /// @name Histograms
     //@{
     Histo1DPtr _histChTot;
-    Histo1DPtr _histAver;
+    Profile1DPtr _histAver;
     //@}
 
   };
 
 
-
   // The hook for the plugin system
-  DECLARE_RIVET_PLUGIN(ALEPH_1991_S2435284);
+  DECLARE_RIVET_PLUGIN(OPAL_1992_I321190);
+
 
 }
