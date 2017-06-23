84667a7077cc51cc6c5eda7c80e537afe6b136df
Andy Buckley <andy@insectnation.org>
2017-06-23 10:29:23 +0100

    Split rand01 implementation into Tools/Random.hh/cc ... hopefully makes the OMP static map of RNGs safe!

---
+++ include/Rivet/Tools/Random.hh
@@ -0,0 +1,19 @@
+// -*- C++ -*-
+#ifndef RIVET_Random_HH
+#define RIVET_Random_HH
+
+// #include <random>
+// #if defined(_OPENMP)
+// #include "omp.h"
+// #endif
+
+namespace Rivet {
+
+
+  /// Return a uniformly sampled random number between 0 and 1
+  double rand01();
+
+
+}
+
+#endif
--- include/Rivet/Tools/SmearingFunctions.hh
+++ include/Rivet/Tools/SmearingFunctions.hh
@@ -4,47 +4,13 @@
 
 #include "Rivet/Particle.hh"
 #include "Rivet/Jet.hh"
-#include <random>
-#if defined(_OPENMP)
-#include "omp.h"
-#endif
+#include "Rivet/Tools/Random.hh"
 
 namespace Rivet {
 
 
   /// @name Random number and filtering utils
   //@{
-
-  /// Return a uniformly sampled random number between 0 and 1
-  /// @todo Move to (math?)utils
-  /// @todo Need to isolate random generators to a single thread
-  inline double rand01() {
-    // return rand() / (double)RAND_MAX;
-    //
-    // static random_device rd;
-    // static mt19937 gen(rd());
-    // return generate_canonical<double, 10>(gen);
-    //
-    // static mt19937 gen(12345);
-    // return generate_canonical<double, 10>(gen);
-    //
-    #if defined(_OPENMP)
-    static map<int,mt19937> gens;
-    const int nthread = omp_get_thread_num();
-    if (gens.find(nthread) == gens.end()) {
-      seed_seq seq{1,2,3,4,5};
-      vector<uint32_t> seeds(nthread+1);
-      seq.generate(seeds.begin(), seeds.end());
-      gens[nthread] = mt19937(seeds[nthread]);
-      // cout << "Thread " << nthread+1 << ", seed=" << seeds[nthread] << " (" << gens.size() << " RNGs)" << endl;
-    }
-    mt19937& g = gens[nthread];
-    #else
-    static mt19937 g(12345);
-    #endif
-    const double r = generate_canonical<double, 10>(g);
-    return r;
-  }
 
   /// Return true if Particle @a p is chosen to survive a random efficiency selection
   template <typename FN>
