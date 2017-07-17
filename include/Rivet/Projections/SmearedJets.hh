0292af849d2d8c901d592a2866c4a4f176837463
Andy Buckley <andy@insectnation.org>
2017-07-17 16:31:15 +0100

    Tidy ctor delegation, comments, etc.

--- include/Rivet/Projections/SmearedJets.hh
+++ include/Rivet/Projections/SmearedJets.hh
@@ -12,20 +12,20 @@
 namespace Rivet {
 
 
-  // Recursive variadic template arg decoding
-  namespace {
-    template<typename T>
-    vector<JetEffSmearFn>& toEffSmearFns(vector<JetEffSmearFn>& v, const T& t) {
-      v.push_back(JetEffSmearFn(t));
-      return v;
-    }
-    template<typename T, typename... ARGS>
-    vector<JetEffSmearFn>& toEffSmearFns(vector<JetEffSmearFn>& v, const T& first, ARGS... args) {
-      v.push_back(JetEffSmearFn(first));
-      toEffSmearFns(v, args...);
-      return v;
-    }
-  }
+  // // Recursive variadic template arg decoding
+  // namespace {
+  //   template<typename T>
+  //   vector<JetEffSmearFn>& toEffSmearFns(vector<JetEffSmearFn>& v, const T& t) {
+  //     v.push_back(JetEffSmearFn(t));
+  //     return v;
+  //   }
+  //   template<typename T, typename... ARGS>
+  //   vector<JetEffSmearFn>& toEffSmearFns(vector<JetEffSmearFn>& v, const T& first, ARGS... args) {
+  //     v.push_back(JetEffSmearFn(first));
+  //     toEffSmearFns(v, args...);
+  //     return v;
+  //   }
+  // }
 
 
 
@@ -36,35 +36,53 @@ namespace Rivet {
     /// @name Constructors etc.
     //@{
 
-    /// @brief Constructor with efficiency and smearing function args
-    /// The jet eff/smearing is mandatory; the tagging functions are optional
-    SmearedJets(const JetAlg& ja,
-                const JetSmearFn& smearFn)
-      : SmearedJets(ja, vector<JetEffSmearFn>{smearFn}, JET_BTAG_PERFECT, JET_CTAG_PERFECT)
-    {    }
-
-
-    /// @brief Constructor with efficiency and smearing function args
-    /// The jet reconstruction efficiency is mandatory; the smearing and tagging functions are optional
+    /// @brief Constructor with a reco efficiency and optional tagging efficiencies
     SmearedJets(const JetAlg& ja,
                 const JetSmearFn& smearFn,
-                const JetEffFn& bTagEffFn)
-      : SmearedJets(ja, vector<JetEffSmearFn>{smearFn}, bTagEffFn, JET_CTAG_PERFECT)
-    {    }
-
-
-    /// @brief Constructor with efficiency and smearing function args
-    /// The jet reconstruction efficiency is mandatory; the smearing and tagging functions are optional
-    SmearedJets(const JetAlg& ja,
-                const JetSmearFn& smearFn,
-                const JetEffFn& bTagEffFn,
-                const JetEffFn& cTagEffFn)
+                const JetEffFn& bTagEffFn=JET_BTAG_PERFECT,
+                const JetEffFn& cTagEffFn=JET_CTAG_PERFECT)
       : SmearedJets(ja, vector<JetEffSmearFn>{smearFn}, bTagEffFn, cTagEffFn)
     {    }
 
 
-    /// @brief Constructor with efficiency and smearing function args
-    /// The jet reconstruction efficiency is mandatory; the smearing and tagging functions are optional
+    /// @brief Constructor with tagging efficiencies, plus an ordered init-list of efficiency and smearing functions
+    SmearedJets(const JetAlg& ja,
+                const JetEffFn& bTagEffFn=JET_BTAG_PERFECT,
+                const JetEffFn& cTagEffFn=JET_CTAG_PERFECT,
+                const initializer_list<JetEffSmearFn>& effSmearFns={})
+      : SmearedJets(ja, vector<JetEffSmearFn>{effSmearFns}, bTagEffFn, cTagEffFn)
+    {    }
+
+    /// @brief Constructor with tagging efficiencies, plus an ordered vector of efficiency and smearing functions
+    SmearedJets(const JetAlg& ja,
+                const JetEffFn& bTagEffFn=JET_BTAG_PERFECT,
+                const JetEffFn& cTagEffFn=JET_CTAG_PERFECT,
+                const vector<JetEffSmearFn>& effSmearFns={})
+      : SmearedJets(ja, effSmearFns, bTagEffFn, cTagEffFn)
+    {    }
+
+
+    /// @brief Constructor with an ordered init-list of efficiency and smearing functions, plus optional tagging efficiencies
+    SmearedJets(const JetAlg& ja,
+                const initializer_list<JetEffSmearFn>& effSmearFns,
+                const JetEffFn& bTagEffFn=JET_BTAG_PERFECT,
+                const JetEffFn& cTagEffFn=JET_CTAG_PERFECT)
+      : SmearedJets(ja, vector<JetEffSmearFn>{effSmearFns}, bTagEffFn, cTagEffFn)
+    {    }
+
+    /// @brief Constructor with an ordered vector of efficiency and smearing functions, plus optional tagging efficiencies
+    SmearedJets(const JetAlg& ja,
+                const vector<JetEffSmearFn>& effSmearFns,
+                const JetEffFn& bTagEffFn=JET_BTAG_PERFECT,
+                const JetEffFn& cTagEffFn=JET_CTAG_PERFECT)
+      : _detFns(effSmearFns), _bTagEffFn(bTagEffFn), _cTagEffFn(cTagEffFn)
+    {
+      setName("SmearedJets");
+      addProjection(ja, "TruthJets");
+    }
+
+
+    /// @brief Constructor with trailing efficiency arg
     /// @deprecated Use the version with pair-smearing list as 2nd argument
     SmearedJets(const JetAlg& ja,
                 const JetSmearFn& smearFn,
@@ -75,25 +93,9 @@ namespace Rivet {
     {    }
 
 
-    /// @brief Constructor with efficiency and smearing function args
-    /// The jet reconstruction efficiency is mandatory; the smearing and tagging functions are optional
-    SmearedJets(const JetAlg& ja,
-                const vector<JetEffSmearFn>& effSmearFns,
-                const JetEffFn& bTagEffFn, const JetEffFn& cTagEffFn)
-      : _detFns(effSmearFns), _bTagEffFn(bTagEffFn), _cTagEffFn(cTagEffFn)
-    {
-      setName("SmearedJets");
-      addProjection(ja, "TruthJets");
-    }
-
-
-    /// @brief Constructor with efficiency and smearing function args
-    /// The jet reconstruction efficiency is mandatory; the smearing and tagging functions are optional
-    SmearedJets(const JetAlg& ja,
-                const initializer_list<JetEffSmearFn>& effSmearFns,
-                const JetEffFn& bTagEffFn, const JetEffFn& cTagEffFn)
-      : SmearedJets(ja, vector<JetEffSmearFn>{effSmearFns}, bTagEffFn, cTagEffFn)
-    {    }
+    /// @todo How to include tagging effs?
+    /// @todo Variadic eff/smear fn list?
+    /// @todo Add a trailing Cut arg cf. SmearedParticles? -- wrap into an eff function
 
 
     /// Clone on the heap.
