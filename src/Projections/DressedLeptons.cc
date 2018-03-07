fda3656c434b4109ee683cadbd4480b389ac61b5
Jon Butterworth <j.butterworth@cern.ch>
2018-03-07 06:38:38 +0000

    make the error message a bit more helpful
    --HG--
    branch : release-2-6-x

--- src/Projections/DressedLeptons.cc
+++ src/Projections/DressedLeptons.cc
@@ -21,7 +21,7 @@ namespace Rivet {
   }
 
   void DressedLepton::addPhoton(const Particle& p, bool momsum) {
-    if (p.pid() != PID::PHOTON) throw Error("Clustering a non-photon on to a DressedLepton");
+    if (p.pid() != PID::PHOTON) throw Error("Clustering a non-photon on to a DressedLepton:"+to_string(p.pid()));
     addConstituent(p, momsum);
   }
 
