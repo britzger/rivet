a02dfc309eb38f9bee111e3ecdd6daed80508da7
Jon Butterworth <j.butterworth@cern.ch>
2018-04-04 16:36:59 +0100

    another fix for pointer behavior of constituents
    --HG--
    branch : release-2-6-x

--- analyses/pluginCMS/CMS_2017_I1610623.cc
+++ analyses/pluginCMS/CMS_2017_I1610623.cc
@@ -99,9 +99,9 @@ namespace Rivet {
 
       if (wfinder_mu.bosons().size() == 1) {
 
-        const FourMomentum& lepton0 = wfinder_mu.constituentLeptons()[0].momentum();
-        const FourMomentum& neutrino = wfinder_mu.constituentNeutrinos()[0].momentum();
-        double WmT = sqrt( 2 * lepton0.pT() * neutrino.pT() * (1 - cos(deltaPhi(lepton0, neutrino))) );
+        const FourMomentum lepton0 = wfinder_mu.constituentLepton().momentum();
+        const FourMomentum neutrino = wfinder_mu.constituentNeutrino().momentum();
+        double WmT = wfinder_mu.mT();
 
         if (WmT < 50.0*GeV) vetoEvent;
 
