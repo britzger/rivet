f19f5b4d3b2e2f3e77984d190414d4117ca265ce
Andy Buckley <andy@insectnation.org>
2018-05-23 17:43:57 +0100

    foreach removal
    --HG--
    branch : release-2-6-x

--- bin/rivet-nopy.cc
+++ bin/rivet-nopy.cc
@@ -10,7 +10,7 @@ int main(int argc, char** argv) {
     return 1;
   }
 
-  foreach (const string& a, Rivet::AnalysisLoader::analysisNames())
+  for (const string& a : Rivet::AnalysisLoader::analysisNames())
     cout << a << endl;
 
   Rivet::AnalysisHandler ah;
