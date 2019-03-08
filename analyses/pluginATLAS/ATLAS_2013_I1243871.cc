414e829cb479289783026e9287b6545e27d57f91
Leif Lönnblad <leif.lonnblad@thep.lu.se>
2019-03-08 14:14:17 +0100

    I don't know what happened to this analysis on the default branch, but I couldn't make it compile, so I simply copied it from the release-2-7-x branch.
    --HG--
    branch : jmonk-hepmc3

--- analyses/pluginATLAS/ATLAS_2013_I1243871.cc
+++ analyses/pluginATLAS/ATLAS_2013_I1243871.cc
@@ -1,10 +1,12 @@
 // -*- C++ -*-
 #include "Rivet/Analysis.hh"
+#include "Rivet/Tools/Logging.hh"
 #include "Rivet/Projections/FinalState.hh"
 #include "Rivet/Projections/IdentifiedFinalState.hh"
 #include "Rivet/Projections/VetoedFinalState.hh"
-#include "Rivet/Projections/HeavyHadrons.hh"
 #include "Rivet/Projections/FastJets.hh"
+#include "Rivet/Tools/ParticleIdUtils.hh"
+#include "Rivet/Particle.hh"
 
 namespace Rivet {
 
@@ -34,10 +36,6 @@ namespace Rivet {
       muon_fs.acceptIdPair(PID::MUON);
       declare(muon_fs, "MUON_FS");
 
-      /// Get b-hadrons for tagging
-      HeavyHadrons hh(Cuts::pT > 5*GeV);
-      declare(hh, "HF_HADRONS");
-
       // Final state used as input for jet-finding.
       // We include everything except the muons and neutrinos
       VetoedFinalState jet_input(fs);
@@ -64,48 +62,84 @@ namespace Rivet {
       const double weight = event.weight();
 
       /// Get the various sets of final state particles
-      const Particles& elecs = apply<IdentifiedFinalState>(event, "ELEC_FS").particlesByPt();
-      const Particles& muons = apply<IdentifiedFinalState>(event, "MUON_FS").particlesByPt();
+      const ParticleVector& elecFS = apply<IdentifiedFinalState>(event, "ELEC_FS").particlesByPt();
+      const ParticleVector& muonFS = apply<IdentifiedFinalState>(event, "MUON_FS").particlesByPt();
 
       // Get all jets with pT > 7 GeV (ATLAS standard jet collection)
-      const Jets& allJets = apply<FastJets>(event, "JETS").jetsByPt(7*GeV);
+      /// @todo Why rewrite the jets collection as a vector of pointers?
+      const Jets& jets = apply<FastJets>(event, "JETS").jetsByPt(7*GeV);
+      vector<const Jet*> allJets;
+      for (const Jet& j : jets) allJets.push_back(&j);
 
       // Keep any jets that pass the pt cut
-      const Jets pt_jets = filter_select(allJets, Cuts::pt > 25*GeV && Cuts::abseta < 2.5);
+      vector<const Jet*> pt_jets;
+      foreach (const Jet* j, allJets) {
+        /// @todo Use direct kinematics access
+        const double pt = j->momentum().pT();
+        const double eta = j->momentum().eta();
+        if (pt > 25*GeV && fabs(eta) < 2.5) pt_jets.push_back(j);
+      }
 
       // Remove jets too close to an electron
-      const Jets good_jets = filter_discard(pt_jets, [&](const Jet& j){
-          return any(elecs, deltaRLess(j, 0.2));
-        });
+      vector<const Jet*> good_jets;
+      foreach (const Jet* j, pt_jets) {
+        bool isElectron = 0;
+        foreach (const Particle& e, elecFS) {
+          const double elec_jet_dR = deltaR(e.momentum(), j->momentum());
+          if (elec_jet_dR < 0.2) { isElectron = true; break; }
+        }
+        if (!isElectron) good_jets.push_back(j);
+      }
 
       // Classify the event type
-      const size_t nElec = elecs.size();
-      const size_t nMuon = muons.size();
+      const size_t nElec = elecFS.size();
+      const size_t nMuon = muonFS.size();
       bool isSemilepton = false, isDilepton = false;
       if (nElec == 1 && nMuon == 0) {
         isSemilepton = true;
       } else if (nElec == 0 && nMuon == 1) {
         isSemilepton = true;
       } else if (nElec == 2 && nMuon == 0) {
-        if (charge(elecs[0]) != charge(elecs[1])) isDilepton = true;
+        if (charge(elecFS[0]) != charge(elecFS[1])) isDilepton = true;
       } else if (nElec == 1 && nMuon == 1) {
-        if (charge(elecs[0]) != charge(muons[0])) isDilepton = true;
+        if (charge(elecFS[0]) != charge(muonFS[0])) isDilepton = true;
       } else if (nElec == 0 && nMuon == 2) {
-        if (charge(muons[0]) != charge(muons[1])) isDilepton = true;
+        if (charge(muonFS[0]) != charge(muonFS[1])) isDilepton = true;
       }
       const bool isGoodEvent = (isSemilepton && good_jets.size() >= 4) || (isDilepton && good_jets.size() >= 2);
       if (!isGoodEvent) vetoEvent;
 
-      // Weakly-decaying b-hadrons for tagging
-      const Particles& b_hadrons = apply<HeavyHadrons>(event, "HF_HADRONS").bHadrons();
+      // Select b-hadrons
+      /// @todo Use built-in identification on Particle, avoid HepMC
+      vector<ConstGenParticlePtr> b_hadrons;
+      vector<ConstGenParticlePtr> allParticles = HepMCUtils::particles(event.genEvent());
+      for (size_t i = 0; i < allParticles.size(); i++) {
+        ConstGenParticlePtr p = allParticles.at(i);
+        if ( !(PID::isHadron( p->pdg_id() ) && PID::hasBottom( p->pdg_id() )) ) continue;
+        if (p->momentum().perp() < 5*GeV) continue;
+        b_hadrons.push_back(p);
+      }
 
-      // Select b-jets as those containing a b-hadron and not overlapped with any other jet
-      Jets b_jets = filter_select(good_jets, [&](const Jet& j){
-          if (all(b_hadrons, deltaRGtr(j, 0.3))) return false;
-          for (const Jet& k : allJets)
-            if (inRange(deltaR(j,k), 0.01, 0.8)) return false;
-          return true;
-        });
+      // Select b-jets as those containing a b-hadron
+      /// @todo Use built-in dR < 0.3 Jet tagging, avoid HepMC
+      vector<const Jet*> b_jets;
+      foreach (const Jet* j, good_jets) {
+        bool isbJet = false;
+        foreach (ConstGenParticlePtr b, b_hadrons) {
+          /// @todo Use direct momentum accessor / delta functions
+          const FourMomentum hadron = b->momentum();
+          const double hadron_jet_dR = deltaR(j->momentum(), hadron);
+          if (hadron_jet_dR < 0.3) { isbJet = true; break; }
+        }
+        // Check if it is overlapped to any other jet
+        bool isOverlapped = false;
+        foreach (const Jet* k, allJets) {
+          if (j == k) continue;
+          double dRjj = deltaR(j->momentum(), k->momentum());
+          if (dRjj < 0.8) { isOverlapped = true; break; }
+        }
+        if (isbJet && !isOverlapped) b_jets.push_back(j);
+      }
       MSG_DEBUG(b_jets.size() << " b-jets selected");
 
 
@@ -113,54 +147,66 @@ namespace Rivet {
       /// @todo Use built-in b-tagging (dR < 0.3 defn), avoid HepMC
       const double nominalW = 80.4*GeV;
       double deltaM = 500*GeV;
-      const Jet* light1 = nullptr; const Jet* light2 = nullptr; // NB: const Jets, not const pointers!
-      for (const Jet& i : good_jets) {
-        const bool isbJet1 = any(b_hadrons, deltaRLess(i, 0.3));
+      const Jet* light1 = NULL; const Jet* light2 = NULL; // NB: const Jets, not const pointers!
+      foreach (const Jet* i, good_jets) {
+        bool isbJet1 = false;
+        for (ConstGenParticlePtr b : b_hadrons) {
+          /// @todo Use direct momentum accessor / delta functions
+          const FourMomentum hadron = b->momentum();
+          const double hadron_jet_dR = deltaR(i->momentum(), hadron);
+          if (hadron_jet_dR < 0.3) { isbJet1 = true; break; }
+        }
         if (isbJet1) continue;
-        for (const Jet& j : good_jets) {
-          const bool isbJet2 = any(b_hadrons, deltaRLess(j, 0.3));
+        foreach (const Jet* j, good_jets) {
+          bool isbJet2 = false;
+          foreach (ConstGenParticlePtr b, b_hadrons) {
+            FourMomentum hadron = b->momentum();
+            double hadron_jet_dR = deltaR(j->momentum(), hadron);
+            if (hadron_jet_dR < 0.3) { isbJet2 = true; break; }
+          }
           if (isbJet2) continue;
-          const double invMass = (i.momentum()+j.momentum()).mass();
-          const double dM = fabs(invMass - nominalW);
-          if (dM < deltaM) {
-            deltaM = dM;
-            light1 = &i;
-            light2 = &j;
+          double invMass = (i->momentum()+j->momentum()).mass();
+          if (fabs(invMass-nominalW) < deltaM){
+            deltaM = fabs(invMass - nominalW);
+            light1 = i;
+            light2 = j;
           }
         }
       }
 
       // Check that both jets are not overlapped, and populate the light jets list
-      Jets light_jets;
-      const bool hasGoodLight = light1 != nullptr && light2 != nullptr && light1 != light2;
+      vector<const Jet*> light_jets;
+      const bool hasGoodLight = light1 != NULL && light2 != NULL && light1 != light2;
       if (hasGoodLight) {
         bool isOverlap1 = false, isOverlap2 = false;
-        for (const Jet& j : allJets) {
-          if (light1 == &j) continue;
-          const double dR1j = deltaR(*light1, j);
+        foreach (const Jet* j, allJets) {
+          if (light1 == j) continue;
+          const double dR1j = deltaR(light1->momentum(), j->momentum());
           if (dR1j < 0.8) { isOverlap1 = true; break; }
         }
-        for (const Jet& j : allJets) {
-          if (light2 == &j) continue;
-          const double dR2j = deltaR(*light2, j);
+        foreach (const Jet* j, allJets) {
+          if (light2 == j) continue;
+          const double dR2j = deltaR(light2->momentum(), j->momentum());
           if (dR2j < 0.8) { isOverlap2 = true; break; }
         }
         if (!isOverlap1 && !isOverlap2) {
-          light_jets = {*light1, *light2};
+          light_jets.push_back(light1);
+          light_jets.push_back(light2);
         }
       }
       MSG_DEBUG(light_jets.size() << " light jets selected");
 
 
       // Calculate the jet shapes
+      /// @todo Use C++11 vector/array initialization
       const double binWidth = 0.04; // -> 10 bins from 0.0-0.4
-      const vector<double> ptEdges = {{ 30, 40, 50, 70, 100, 150 }};
+      vector<double> ptEdges; ptEdges += {{ 30, 40, 50, 70, 100, 150 }};
 
       // b-jet shapes
       MSG_DEBUG("Filling b-jet shapes");
-      for (const Jet& bJet : b_jets) {
+      foreach (const Jet* bJet, b_jets) {
         // Work out jet pT bin and skip this jet if out of range
-        const double jetPt = bJet.pT();
+        const double jetPt = bJet->momentum().pT();
         MSG_DEBUG("Jet pT = " << jetPt/GeV << " GeV");
         if (!inRange(jetPt/GeV, 30., 150.)) continue;
         /// @todo Use YODA bin index lookup tools
@@ -169,8 +215,8 @@ namespace Rivet {
 
         // Calculate jet shape
         vector<double> rings(10, 0);
-        for (const Particle& p : bJet.particles()) {
-          const double dR = deltaR(bJet, p);
+        foreach (const Particle& p, bJet->particles()) {
+          const double dR = deltaR(bJet->momentum(), p.momentum());
           const size_t idR = (size_t) floor(dR/binWidth);
           for (size_t i = idR; i < 10; ++i) rings[i] += p.pT();
         }
@@ -188,9 +234,9 @@ namespace Rivet {
 
       // Light jet shapes
       MSG_DEBUG("Filling light jet shapes");
-      for (const Jet& lJet : light_jets) {
+      foreach (const Jet* lJet, light_jets) {
         // Work out jet pT bin and skip this jet if out of range
-        const double jetPt = lJet.pT();
+        const double jetPt = lJet->momentum().pT();
         MSG_DEBUG("Jet pT = " << jetPt/GeV << " GeV");
         if (!inRange(jetPt/GeV, 30., 150.)) continue;
         /// @todo Use YODA bin index lookup tools
@@ -199,8 +245,8 @@ namespace Rivet {
 
         // Calculate jet shape
         vector<double> rings(10, 0);
-        for (const Particle& p : lJet.particles()) {
-          const double dR = deltaR(lJet, p);
+        foreach (const Particle& p, lJet->particles()) {
+          const double dR = deltaR(lJet->momentum(), p.momentum());
           const size_t idR = (size_t) floor(dR/binWidth);
           for (size_t i = idR; i < 10; ++i) rings[i] += p.pT();
         }
