453cfcab87d5d3ce0f7b157ce9b52d252471a8ca
Christian Gutschow <chris.g@cern.ch>
2019-05-21 13:24:23 +0100

    MC_TTBAR: improvements to fiducial selection for different tt decays
    --HG--
    branch : release-2-7-x

--- analyses/pluginMC/MC_TTBAR.cc
+++ analyses/pluginMC/MC_TTBAR.cc
@@ -57,9 +57,16 @@ namespace Rivet {
       _h["ljet_1_pT"] = bookHisto1D(pre + "jetl_1_pT", logspace(50, 20.0, 400.0));
       _h["ljet_2_pT"] = bookHisto1D(pre + "jetl_2_pT", logspace(50, 20.0, 300.0));
       //
-      _h["W_mass"] = bookHisto1D(pre + "W_mass", 75, 30, 180);
-      _h["t_mass"] = bookHisto1D(pre + "t_mass", 150, 130, 430);
-      _h["t_mass_W_cut"] = bookHisto1D(pre + "t_mass_W_cut", 150, 130, 430);
+      if (_mode != 2)  _h["tt_mass"]   = bookHisto1D(pre + "tt_mass", 200, 300.0, 700.0);
+      //
+      if (_mode < 2) { // these rely on a hadronic W being part of the ttbar decay
+        _h["W_mass"]        = bookHisto1D(pre + "W_mass", 75, 30, 180);
+        _h["t_mass"]        = bookHisto1D(pre + "t_mass", 150, 130, 430);
+        _h["t_mass_W_cut"]  = bookHisto1D(pre + "t_mass_W_cut", 150, 130, 430);
+        _h["jetb_1_W_dR"]   = bookHisto1D(pre + "jetb_1_W_dR", 20, 0.0, 7.0);
+        _h["jetb_1_W_deta"] = bookHisto1D(pre + "jetb_1_W_deta", 20, 0.0, 7.0);
+        _h["jetb_1_W_dphi"] = bookHisto1D(pre + "jetb_1_W_dphi", 20, 0.0, M_PI);
+      }
       //
       _h["jetb_1_jetb_2_dR"]   = bookHisto1D(pre + "jetb_1_jetb_2_dR", 20, 0.0, 7.0);
       _h["jetb_1_jetb_2_deta"] = bookHisto1D(pre + "jetb_1_jetb_2_deta", 20, 0.0, 7.0);
@@ -70,10 +77,7 @@ namespace Rivet {
       _h["jetl_1_jetl_2_dR"]   = bookHisto1D(pre + "jetl_1_jetl_2_dR", 20, 0.0, 7.0);
       _h["jetl_1_jetl_2_deta"] = bookHisto1D(pre + "jetl_1_jetl_2_deta", 20, 0.0, 7.0);
       _h["jetl_1_jetl_2_dphi"] = bookHisto1D(pre + "jetl_1_jetl_2_dphi", 20, 0.0, M_PI);
-      _h["jetb_1_W_dR"]        = bookHisto1D(pre + "jetb_1_W_dR", 20, 0.0, 7.0);
-      _h["jetb_1_W_deta"]      = bookHisto1D(pre + "jetb_1_W_deta", 20, 0.0, 7.0);
-      _h["jetb_1_W_dphi"]      = bookHisto1D(pre + "jetb_1_W_dphi", 20, 0.0, M_PI);
-      if (_mode > 0) {
+      if (_mode > 0) { // these rely on at least one leptonic decay mode
         _h["jetb_1_l_dR"]        = bookHisto1D(pre + "jetb_1_l_dR", 20, 0.0, 7.0);
         _h["jetb_1_l_deta"]      = bookHisto1D(pre + "jetb_1_l_deta", 20, 0.0, 7.0);
         _h["jetb_1_l_dphi"]      = bookHisto1D(pre + "jetb_1_l_dphi", 20, 0.0, M_PI);
@@ -113,44 +117,35 @@ namespace Rivet {
       // Use a missing ET cut to bias toward events with a hard neutrino from
       // the leptonically decaying W. This helps to reduce pure QCD backgrounds.
       // not applied in all-hadronic mode
-      const MissingMomentum& met = apply<MissingMomentum>(event, "MissingET");
-      MSG_DEBUG("Vector ET = " << met.vectorEt().mod() << " GeV");
-      if (_mode > 0 && met.vectorEt().mod() < 30*GeV) {
+      const Vector3& met = apply<MissingMomentum>(event, "MissingET").vectorMissingPt();
+      MSG_DEBUG("Vector pT = " << met.mod() << " GeV");
+      if (_mode > 0 && met.mod() < 30*GeV) {
         MSG_DEBUG("Event failed missing ET cut");
         vetoEvent;
       }
 
-      // Use the "Jets" projection to check that there are at least 4 jets of
-      // any pT. Getting the jets sorted by pT ensures that the first jet is the
-      // hardest, and so on. We apply no pT cut here only because we want to
-      // plot all jet pTs to help optimise our jet pT cut.
+      // Use the "Jets" projection to check how many jets with pT > 30 GeV there are
+      // remove jets overlapping with any lepton (dR < 0.3)
+      // cut on jet multiplicity depending on ttbar decay mode
       const FastJets& jetpro = apply<FastJets>(event, "Jets");
-      const Jets alljets = jetpro.jetsByPt();
-      if (alljets.size() < 4) {
-        MSG_DEBUG("Event failed jet multiplicity cut");
-        vetoEvent;
-      }
+      const Jets jets = discardIfAnyDeltaRLess(jetpro.jetsByPt(30*GeV), lfs.chargedLeptons(), 0.3);
 
-      // Update passed-cuts counter and fill all-jets histograms
-      _h["jet_1_pT"]->fill(alljets[0].pT()/GeV, weight);
-      _h["jet_2_pT"]->fill(alljets[1].pT()/GeV, weight);
-      _h["jet_3_pT"]->fill(alljets[2].pT()/GeV, weight);
-      _h["jet_4_pT"]->fill(alljets[3].pT()/GeV, weight);
+      if (     _mode == 0 && jets.size() < 6)  vetoEvent; // all-hadronic
+      else if (_mode == 1 && jets.size() < 4)  vetoEvent; // single lepton
+      else if (_mode == 2 && jets.size() < 2)  vetoEvent; // dilepton
+      else if (_mode == 3 && nLeps == 1 && jets.size() < 4)  vetoEvent; // non-allhadronic
+      else if (_mode == 3 && nLeps == 2 && jets.size() < 2)  vetoEvent;
+      MSG_DEBUG("Event failed jet multiplicity cut");
 
-      // Insist that the hardest 4 jets pass pT hardness cuts. If we don't find
-      // at least 4 such jets, we abandon this event.
-      const Jets jets = jetpro.jetsByPt(30*GeV);
+      // Fill histograms for inclusive jet kinematics 
       _h["njets"]->fill(jets.size(), weight);
+      if (jets.size() > 0)  _h["jet_1_pT"]->fill(jets[0].pT()/GeV, weight);
+      if (jets.size() > 1)  _h["jet_2_pT"]->fill(jets[1].pT()/GeV, weight);
+      if (jets.size() > 2)  _h["jet_3_pT"]->fill(jets[2].pT()/GeV, weight);
+      if (jets.size() > 3)  _h["jet_4_pT"]->fill(jets[3].pT()/GeV, weight);
       double ht = 0.0;
       for (const Jet& j : jets) { ht += j.pT(); }
       _h["jet_HT"]->fill(ht/GeV, weight);
-      if (jets.size() < 4 ||
-          jets[0].pT() < 60*GeV ||
-          jets[1].pT() < 50*GeV ||
-          jets[3].pT() < 30*GeV) {
-        MSG_DEBUG("Event failed jet cuts");
-        vetoEvent;
-      }
 
       // Sort the jets into b-jets and light jets. We expect one hard b-jet from
       // each top decay, so our 4 hardest jets should include two b-jets. The
@@ -158,23 +153,8 @@ namespace Rivet {
       // b-tagging, in a generator-independent way.
       Jets bjets, ljets;
       for (const Jet& jet : jets) {
-        // // Don't count jets that overlap with the hard leptons
-        bool isolated = true;
-        for (const Particle& lepton : lfs.chargedLeptons()) {
-          if (deltaR(jet.momentum(), lepton.momentum()) < 0.3) {
-            isolated = false;
-            break;
-          }
-        }
-        if (!isolated) {
-          MSG_DEBUG("Jet failed lepton isolation cut");
-          break;
-        }
-        if (jet.bTagged()) {
-          bjets.push_back(jet);
-        } else {
-          ljets.push_back(jet);
-        }
+        if (jet.bTagged())  bjets += jet;
+        else                ljets += jet;
       }
       MSG_DEBUG("Number of b-jets = " << bjets.size());
       MSG_DEBUG("Number of l-jets = " << ljets.size());
@@ -182,85 +162,126 @@ namespace Rivet {
         MSG_DEBUG("Event failed post-lepton-isolation b-tagging cut");
         vetoEvent;
       }
-      if (ljets.size() < 2) {
-        MSG_DEBUG("Event failed since not enough light jets remaining after lepton-isolation");
-        vetoEvent;
-      }
+      if (_mode == 0 && ljets.size() < 4)  vetoEvent;
+      else if (_mode == 1 && ljets.size() < 2)  vetoEvent;
+      else if (_mode == 3 && nLeps == 1 && ljets.size() < 2)  vetoEvent;
 
       // Plot the pTs of the identified jets.
       _h["bjet_1_pT"]->fill(bjets[0].pT(), weight);
       _h["bjet_2_pT"]->fill(bjets[1].pT(), weight);
-      _h["ljet_1_pT"]->fill(ljets[0].pT(), weight);
-      _h["ljet_2_pT"]->fill(ljets[1].pT(), weight);
+      // need to check size to cater for dileptonic mode
+      if (ljets.size() > 0)  _h["ljet_1_pT"]->fill(ljets[0].pT(), weight);
+      if (ljets.size() > 1)  _h["ljet_2_pT"]->fill(ljets[1].pT(), weight);
 
-      // Construct the hadronically decaying W momentum 4-vector from pairs of
-      // non-b-tagged jets. The pair which best matches the W mass is used. We start
-      // with an always terrible 4-vector estimate which should always be "beaten" by
-      // a real jet pair.
-      FourMomentum W(10*(sqrtS()>0.?sqrtS():14000.), 0, 0, 0);
-      for (size_t i = 0; i < ljets.size()-1; ++i) {
-        for (size_t j = i + 1; j < ljets.size(); ++j) {
-          const FourMomentum Wcand = ljets[i].momentum() + ljets[j].momentum();
-          MSG_TRACE(i << "," << j << ": candidate W mass = " << Wcand.mass()/GeV
-                    << " GeV, vs. incumbent candidate with " << W.mass()/GeV << " GeV");
-          if (fabs(Wcand.mass() - 80.4*GeV) < fabs(W.mass() - 80.4*GeV)) {
-            W = Wcand;
+
+      // Try to reconstruct ttbar pair (doesn't really work in the dileptonic mode)
+      FourMomentum ttpair = bjets[0].mom() + bjets[1].mom();
+      if (_mode == 0) {
+        ttpair += ljets[0].mom() + ljets[1].mom() + ljets[2].mom() + ljets[3].mom();
+      }
+      else if (nLeps < 2) {
+        ttpair += ljets[0].mom() + ljets[1].mom();
+        const FourMomentum lep = lfs.chargedLeptons()[0].mom();
+        double pz = findZcomponent(lep, met);
+        FourMomentum neutrino(sqrt(sqr(met.x()) + sqr(met.y()) + sqr(pz)), met.x(), met.y(), pz);
+        ttpair += lep + neutrino;
+      }
+      if (nLeps < 2)  _h["tt_mass"]->fill(ttpair.mass()/GeV, weight);
+
+      if (_mode < 2) {
+        // Construct the hadronically decaying W momentum 4-vector from pairs of
+        // non-b-tagged jets. The pair which best matches the W mass is used. We start
+        // with an always terrible 4-vector estimate which should always be "beaten" by
+        // a real jet pair.
+        FourMomentum W(10*(sqrtS()>0.?sqrtS():14000.), 0, 0, 0);
+        for (size_t i = 0; i < ljets.size()-1; ++i) {
+          for (size_t j = i + 1; j < ljets.size(); ++j) {
+            const FourMomentum Wcand = ljets[i].momentum() + ljets[j].momentum();
+            MSG_TRACE(i << "," << j << ": candidate W mass = " << Wcand.mass()/GeV
+                      << " GeV, vs. incumbent candidate with " << W.mass()/GeV << " GeV");
+            if (fabs(Wcand.mass() - 80.4*GeV) < fabs(W.mass() - 80.4*GeV)) {
+              W = Wcand;
+            }
           }
         }
-      }
-      MSG_DEBUG("Candidate W mass = " << W.mass() << " GeV");
+        MSG_DEBUG("Candidate W mass = " << W.mass() << " GeV");
 
-      // There are two b-jets with which this can be combined to make the
-      // hadronically decaying top, one of which is correct and the other is
-      // not... but we have no way to identify which is which, so we construct
-      // both possible top momenta and fill the histograms with both.
-      const FourMomentum t1 = W + bjets[0].momentum();
-      const FourMomentum t2 = W + bjets[1].momentum();
-      _h["W_mass"]->fill(W.mass(), weight);
-      _h["t_mass"]->fill(t1.mass(), weight);
-      _h["t_mass"]->fill(t2.mass(), weight);
+        // There are two b-jets with which this can be combined to make the
+        // hadronically decaying top, one of which is correct and the other is
+        // not... but we have no way to identify which is which, so we construct
+        // both possible top momenta and fill the histograms with both.
+        const FourMomentum t1 = W + bjets[0].momentum();
+        const FourMomentum t2 = W + bjets[1].momentum();
+        _h["W_mass"]->fill(W.mass(), weight);
+        _h["t_mass"]->fill(t1.mass(), weight);
+        _h["t_mass"]->fill(t2.mass(), weight);
 
-      // Placing a cut on the well-known W mass helps to reduce backgrounds
-      if (inRange(W.mass()/GeV, 75.0, 85.0)) {
+        // Placing a cut on the well-known W mass helps to reduce backgrounds
+        // only done for all-hadronic and semileptonic mode (since W is hadronic)
+        if (!inRange(W.mass()/GeV, 75.0, 85.0))  vetoEvent;
         MSG_DEBUG("W found with mass " << W.mass()/GeV << " GeV");
+
         _h["t_mass_W_cut"]->fill(t1.mass(), weight);
         _h["t_mass_W_cut"]->fill(t2.mass(), weight);
-
-        _h["jetb_1_jetb_2_dR"]->fill(deltaR(bjets[0].momentum(), bjets[1].momentum()),weight);
-        _h["jetb_1_jetb_2_deta"]->fill(fabs(bjets[0].eta()-bjets[1].eta()),weight);
-        _h["jetb_1_jetb_2_dphi"]->fill(deltaPhi(bjets[0].momentum(),bjets[1].momentum()),weight);
-
-        _h["jetb_1_jetl_1_dR"]->fill(deltaR(bjets[0].momentum(), ljets[0].momentum()),weight);
-        _h["jetb_1_jetl_1_deta"]->fill(fabs(bjets[0].eta()-ljets[0].eta()),weight);
-        _h["jetb_1_jetl_1_dphi"]->fill(deltaPhi(bjets[0].momentum(),ljets[0].momentum()),weight);
-
-        _h["jetl_1_jetl_2_dR"]->fill(deltaR(ljets[0].momentum(), ljets[1].momentum()),weight);
-        _h["jetl_1_jetl_2_deta"]->fill(fabs(ljets[0].eta()-ljets[1].eta()),weight);
-        _h["jetl_1_jetl_2_dphi"]->fill(deltaPhi(ljets[0].momentum(),ljets[1].momentum()),weight);
 
         _h["jetb_1_W_dR"]->fill(deltaR(bjets[0].momentum(), W),weight);
         _h["jetb_1_W_deta"]->fill(fabs(bjets[0].eta()-W.eta()),weight);
         _h["jetb_1_W_dphi"]->fill(deltaPhi(bjets[0].momentum(),W),weight);
+      }
 
-        if (_mode > 0) {
-          FourMomentum l=lfs.chargedLeptons()[0].momentum();
-          _h["jetb_1_l_dR"]->fill(deltaR(bjets[0].momentum(), l),weight);
-          _h["jetb_1_l_deta"]->fill(fabs(bjets[0].eta()-l.eta()),weight);
-          _h["jetb_1_l_dphi"]->fill(deltaPhi(bjets[0].momentum(),l),weight);
-          _h["jetb_1_l_mass"]->fill(FourMomentum(bjets[0].momentum()+l).mass(), weight);
+      _h["jetb_1_jetb_2_dR"]->fill(deltaR(bjets[0].momentum(), bjets[1].momentum()),weight);
+      _h["jetb_1_jetb_2_deta"]->fill(fabs(bjets[0].eta()-bjets[1].eta()),weight);
+      _h["jetb_1_jetb_2_dphi"]->fill(deltaPhi(bjets[0].momentum(),bjets[1].momentum()),weight);
 
-          if (nLeps > 1) {
-            FourMomentum l=lfs.chargedLeptons()[1].momentum();
-            _h["jetb_1_l2_dR"]->fill(deltaR(bjets[0].momentum(), l),weight);
-            _h["jetb_1_l2_deta"]->fill(fabs(bjets[0].eta()-l.eta()),weight);
-            _h["jetb_1_l2_dphi"]->fill(deltaPhi(bjets[0].momentum(),l),weight);
-            _h["jetb_1_l2_mass"]->fill(FourMomentum(bjets[0].momentum()+l).mass(), weight);
-          }
+      if (ljets.size() > 0) {
+        _h["jetb_1_jetl_1_dR"]->fill(deltaR(bjets[0].momentum(), ljets[0].momentum()),weight);
+        _h["jetb_1_jetl_1_deta"]->fill(fabs(bjets[0].eta()-ljets[0].eta()),weight);
+        _h["jetb_1_jetl_1_dphi"]->fill(deltaPhi(bjets[0].momentum(),ljets[0].momentum()),weight);
+        if (ljets.size() > 1) {
+          _h["jetl_1_jetl_2_dR"]->fill(deltaR(ljets[0].momentum(), ljets[1].momentum()),weight);
+          _h["jetl_1_jetl_2_deta"]->fill(fabs(ljets[0].eta()-ljets[1].eta()),weight);
+          _h["jetl_1_jetl_2_dphi"]->fill(deltaPhi(ljets[0].momentum(),ljets[1].momentum()),weight);
+        }
+      }
+
+      // lepton-centric plots
+      if (_mode > 0) {
+        FourMomentum l=lfs.chargedLeptons()[0].momentum();
+        _h["jetb_1_l_dR"]->fill(deltaR(bjets[0].momentum(), l),weight);
+        _h["jetb_1_l_deta"]->fill(fabs(bjets[0].eta()-l.eta()),weight);
+        _h["jetb_1_l_dphi"]->fill(deltaPhi(bjets[0].momentum(),l),weight);
+        _h["jetb_1_l_mass"]->fill(FourMomentum(bjets[0].momentum()+l).mass(), weight);
+
+        if (nLeps > 1) {
+          FourMomentum l=lfs.chargedLeptons()[1].momentum();
+          _h["jetb_1_l2_dR"]->fill(deltaR(bjets[0].momentum(), l),weight);
+          _h["jetb_1_l2_deta"]->fill(fabs(bjets[0].eta()-l.eta()),weight);
+          _h["jetb_1_l2_dphi"]->fill(deltaPhi(bjets[0].momentum(),l),weight);
+          _h["jetb_1_l2_mass"]->fill(FourMomentum(bjets[0].momentum()+l).mass(), weight);
         }
       }
 
     }
 
+    double findZcomponent(const FourMomentum& lepton, const Vector3& met) const {
+      // estimate z-component of momentum given lepton 4-vector and MET 3-vector
+      double pz_estimate;
+      double m_W = 80.399*GeV;
+      double k = (( sqr( m_W ) - sqr( lepton.mass() ) ) / 2 ) + (lepton.px() * met.x() + lepton.py() * met.y());
+      double a = sqr ( lepton.E() )- sqr ( lepton.pz() );
+      double b = -2*k*lepton.pz();
+      double c = sqr( lepton.E() ) * sqr( met.perp() ) - sqr( k );
+      double discriminant = sqr(b) - 4 * a * c;
+      double quad[2] = { (- b - sqrt(discriminant)) / (2 * a), (- b + sqrt(discriminant)) / (2 * a) }; //two possible quadratic solns
+      if (discriminant < 0)  pz_estimate = - b / (2 * a); //if the discriminant is negative
+      else { //if the discriminant is greater than or equal to zero, take the soln with smallest absolute value
+        double absquad[2];
+        for (int n=0; n<2; ++n)  absquad[n] = fabs(quad[n]);
+        if (absquad[0] < absquad[1])  pz_estimate = quad[0];
+        else                          pz_estimate = quad[1];
+      }
+      return pz_estimate;
+    }
 
     void finalize() {
       const double sf = crossSection() / sumOfWeights();
