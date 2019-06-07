7e4680096b1536cb601cd7d216edf52a4e6f48df
Leif Lönnblad <leif.lonnblad@thep.lu.se>
2019-06-07 22:39:38 +0200

    Just fixing some missing includes inanalyses
    --HG--
    branch : release-2-7-x

--- analyses/pluginLEP/OPAL_1997_I421977.cc
+++ analyses/pluginLEP/OPAL_1997_I421977.cc
@@ -1,7 +1,8 @@
 // -*- C++ -*-
 #include "Rivet/Analysis.hh"
-#include "Rivet/Projections/FinalState.hh"
-#include "Rivet/Projections/FastJets.hh"
+#include "Rivet/Projections/Beam.hh"
+#include "Rivet/Projections/ChargedFinalState.hh"
+#include "Rivet/Projections/UnstableParticles.hh"
 
 namespace Rivet {
 
