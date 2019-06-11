704fbb22e7b404eadece8f0a7c7cd9164d480445
Christian Gutschow <chris.g@cern.ch>
2019-06-11 10:05:39 +0100

    HEPData sync for ATLAS_2016_I1468168
    --HG--
    branch : hdsync-feature

--- pyext/rivet/hepdatapatches/ATLAS_2016_I1468168.py
+++ pyext/rivet/hepdatapatches/ATLAS_2016_I1468168.py
@@ -1,6 +1,10 @@
 
 def patch(path, ao):
-    if path == '/REF/ATLAS_2016_I1468168/d02-x01-y01':
+    needs_patching = [ 
+      '/REF/ATLAS_2016_I1468168/d01-x01-y01', 
+      '/REF/ATLAS_2016_I1468168/d02-x01-y01'
+    ]
+    if path in needs_patching:
       for p in ao.points:
           p.xErrs = (0.5, 0.5)
     return ao
