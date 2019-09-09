279ed445c3dc82ee228cc2f72c949c2747c83f34
Leif Lönnblad <leif.lonnblad@thep.lu.se>
2019-09-09 09:22:48 +0200

    Fixed include file.
    --HG--
    branch : release-3-0-x

--- analyses/pluginSLAC/TPC_1988_I262143.cc
+++ analyses/pluginSLAC/TPC_1988_I262143.cc
@@ -1,6 +1,6 @@
 // -*- C++ -*-
 #include "Rivet/Analysis.hh"
-#include "Rivet/Projections/FinalState.hh"
+#include "Rivet/Projections/ChargedFinalState.hh"
 #include "Rivet/Projections/FastJets.hh"
 
 namespace Rivet {
