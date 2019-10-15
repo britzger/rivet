f0710538059aef273808e3188a4863c9e5a9053d
Christian Gutschow <chris.g@cern.ch>
2019-10-15 23:26:41 +0100

    suppress warnings from InitialQuarks
    --HG--
    branch : release-3-0-x

--- analyses/pluginLEP/DELPHI_1997_I428178.cc
+++ analyses/pluginLEP/DELPHI_1997_I428178.cc
@@ -1,6 +1,8 @@
 // -*- C++ -*-
 #include "Rivet/Analysis.hh"
 #include "Rivet/Projections/ChargedFinalState.hh"
+
+#define I_KNOW_THE_INITIAL_QUARKS_PROJECTION_IS_DODGY_BUT_NEED_TO_USE_IT
 #include "Rivet/Projections/InitialQuarks.hh"
 
 namespace Rivet {
