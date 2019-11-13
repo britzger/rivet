bf733b2e2bbe99f58f325cadd1fb57c7e8348442
Andy Buckley <andy@insectnation.org>
2019-11-13 16:57:55 +0000

    Cosmetic typo fixes, and adding release test info for CMS forward energy flow.

--- analyses/pluginCMS/CMS_2017_I1598460.cc
+++ analyses/pluginCMS/CMS_2017_I1598460.cc
@@ -2,8 +2,6 @@
 #include "Rivet/Analysis.hh"
 #include "Rivet/Projections/FinalState.hh"
 #include "Rivet/Projections/FastJets.hh"
-#include <iostream>
-/// @todo Include more projections as required, e.g. ChargedFinalState, FastJets, ZFinder...
 
 namespace Rivet {
 
@@ -12,9 +10,7 @@ namespace Rivet {
   public:
 
     /// Constructor
-    CMS_2017_I1598460()
-      : Analysis("CMS_2017_I1598460")
-    {    }
+    RIVET_DEFAULT_CTOR(CMS_2017_I1598460);
 
 
     /// @name Analysis methods
@@ -24,65 +20,54 @@ namespace Rivet {
     void init() {
 
       const FinalState fs;
-      declare(fs,"FinalState");
+      declare(fs, "FinalState");
       FastJets fj07(fs, FastJets::ANTIKT, 0.7);
-      declare(fj07,"Jets");
+      declare(fj07, "Jets");
       /// @todo Book histograms here, e.g.:
-      for (int i=0; i<6; i++) 
-	{Histo1DPtr tmp; _h_ybys.push_back(book(tmp, i+1,1,1));}
+      for (int i = 0; i < 6; i++) {
+        Histo1DPtr tmp; _h_ybys.push_back(book(tmp, i+1,1,1));
+      }
     }
 
 
     /// Perform the per-event analysis
     void analyze(const Event& event) {
       const FastJets& fj = apply<FastJets>(event,"Jets");
-      const Jets& jets = fj.jetsByPt(Cuts::pt>50.*GeV && Cuts::absrap < 5.);
+      const Jets& jets = fj.jetsByPt(Cuts::pt > 50*GeV && Cuts::absrap < 5);
+
       // Require two jets
-      if (jets.size() < 2)
-        return;
+      if (jets.size() < 2) vetoEvent;
+
       // Veto events if one of two leading jets |y|>3.0, otherwise
       // the subleading jets can become the leading jets through jet selection
-      if (jets[0].absrap() > 3. || jets[1].absrap() > 3.)
-        return;
+      if (jets[0].absrap() > 3 || jets[1].absrap() > 3) vetoEvent;
 
       double ystar = 0.5 * std::abs(jets[0].rap() - jets[1].rap());
       double yboost = 0.5 * std::abs(jets[0].rap() + jets[1].rap());
       double ptavg = 0.5 * (jets[0].pT() + jets[1].pT());
 
+      // Compute index of histogram to be filled: yb0ys0 --> 0 yb0ys1 --> 1 ...
       size_t i = (size_t)yboost;
       size_t j = (size_t)ystar;
-      // index of histogram to be filled
-      // yb0ys0 --> 0 yb0ys1 --> 1 ...
       size_t idx = j + 3*i - i*(i-1)/2;
 
-      _h_ybys[idx]->fill(ptavg);
+      _h_ybys[idx]->fill(ptavg/GeV);
     }
+
 
     /// Normalise histograms etc., after the run
     void finalize() {
-
-      /// @todo Normalise, scale and otherwise manipulate histograms here
-      for (size_t k = 0; k < _h_ybys.size(); ++k) {
-        scale(_h_ybys[k], crossSection()/sumOfWeights());
-      }
+      scale(_h_ybys, crossSection()/sumOfWeights());
     }
 
     //@}
 
 
-  private:
-
-    //@{
-    std::vector<Histo1DPtr> _h_ybys;
-    //@}
-
+    vector<Histo1DPtr> _h_ybys;
 
   };
 
 
-
-  // The hook for the plugin system
   DECLARE_RIVET_PLUGIN(CMS_2017_I1598460);
-
 
 }
